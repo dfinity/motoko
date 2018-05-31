b0f22a4e9a7403d3e56d95b5a153a7ff5e2292d4
Andreas Rossberg <rossberg@mpi-sws.org>
2018-05-31 14:05:25 +0200

    AST

---
+++ src/syntax.ml
@@ -0,0 +1,123 @@
+(* Variables *)
+
+type var = string Source.phrase
+
+
+(* Types *)
+
+type mut = mut' Source.phrase
+and mut' = ConstMut | VarMut
+
+type actor = actor' Source.phrase
+and actor' = Object | Actor
+
+type typ = typ' Source.phrase
+and typ' =
+  | VarT of var * typ list                     (* constructor *)
+  | PrimT of Types.prim                        (* primitive *)
+  | ObjT of actor * typ_field list             (* object *)
+  | ArrayT of mut * typ                        (* array *)
+  | OptT of typ                                (* option *)
+  | TupT of typ list                           (* tuple *)
+  | FuncT of typ_bind list * typ * typ         (* function *)
+  | AsyncT of typ                              (* future *)
+  | LikeT of typ                               (* expansion *)
+(*
+  | AnyT                                       (* top *)
+  | UnionT of type * typ                       (* union *)
+  | AtomT of string                            (* atom *)
+*)
+
+and typ_field = typ_field' Source.phrase
+and typ_field' = {var : var; typ : typ; mut : mut}
+
+and typ_bind = typ_bind' Source.phrase
+and typ_bind' = {var : var; bound : typ}
+
+
+(* Patterns *)
+
+type pat = pat' Source.phrase
+and pat' =
+  | WildP                                      (* wildcard *)
+  | VarP of var                                (* variable *)
+  | TupP of pat list                           (* tuple *)
+  | AnnotP of pat * typ                        (* type annotation *)
+(*
+  | LitP of Types.lit                          (* literal *)
+  | ObjP of pat_field list                     (* object *)
+  | AsP of pat * pat                           (* conjunctive *)
+  | OrP of pat * pat                           (* disjunctive *)
+
+and pat_field = pat_field' Source.phrase
+and pat_field' = {var : var; pat : pat}
+*)
+
+
+(* Expressions *)
+
+type priv = priv' Source.phrase
+and priv' = Public | Private
+
+type exp = exp' Source.phrase
+and exp' =
+  | VarE of var                                (* variable *)
+  | LitE of Types.lit                          (* literal *)
+  | UnE of Types.unop * exp                    (* unary operator *)
+  | BinE of Types.binop * exp                  (* binary operator *)
+  | TupE of exp list                           (* tuple *)
+  | ProjE of exp * Types.nat                   (* tuple projection *)
+  | ObjE of actor * var option * exp_field list  (* object *)
+  | DotE of exp * var                          (* object projection *)
+  | AssignE of exp * exp                       (* assignment *)
+  | ArrayE of exp list                         (* array *)
+  | IdxE of exp * exp                          (* array indexing *)
+  | CallE of exp * exp                         (* function call *)
+  | BlockE of exp list                         (* block *)
+  | NotE of exp                                (* negation *)
+  | AndE of exp * exp                          (* conjunction *)
+  | OrE of exp * exp                           (* disjunction *)
+  | IfE of exp * exp * exp                     (* conditional *)
+  | SwitchE of exp * case list                 (* switch *)
+  | WhileE of exp * exp                        (* while-do loop *)
+  | LoopE of exp * exp option                  (* do-while loop *)
+  | ForE of pat * exp * exp                    (* iteration *)
+  | LabelE of var * exp                        (* label *)
+  | BreakE of var * exp list                   (* break *)
+  | ContE of var                               (* continue *)
+  | RetE of exp list                           (* return *)
+  | AsyncE of exp                              (* async *)
+  | AwaitE of exp                              (* await *)
+  | AssertE of exp                             (* assertion *)
+  | IsE of exp * typ                           (* instance-of *)
+  | AnnotE of exp * typ                        (* type annotation *)
+  | DecE of dec                                (* declaration *)
+(*
+  | ThrowE of exp list                         (* throw exception *)
+  | TryE of exp * case list                    (* catch eexception *)
+  | FinalE of exp * exp                        (* finally *)
+  | AtomE of string                            (* atom *)
+*)
+
+and exp_field = exp_field' Source.phrase
+and exp_field' = {var : var; exp : exp; mut : mut; priv : priv}
+
+and case = case' Source.phrase
+and case' = {pat : pat; exp : exp}
+
+
+(* Declarations *)
+
+and dec = dec' Source.phrase
+and dec' =
+  | LetD of pat * exp                                  (* immutable *)
+  | VarD of var * typ * exp option                     (* mutable *)
+  | FuncD of var * typ_bind list * pat * typ * exp     (* function *)
+  | TypD of var * typ_bind list * typ                  (* type *)
+  | ClassD of actor * var * typ_bind list * pat * exp  (* class *)
+
+
+(* Program *)
+
+type prog = prog' Source.phrase
+and prog' = dec list
---
+++ src/types.ml
@@ -0,0 +1,56 @@
+type nat = int
+type int8 = int
+type int16 = int
+type unicode = int32
+
+type width =
+  | Width8
+  | Width16
+  | Width32
+  | Width64
+
+type prim =
+  | NullT
+  | BoolT
+  | NatT
+  | IntT
+  | WordT of width
+  | FloatT
+  | CharT
+  | TextT
+
+type word =
+  | Word8 of int8
+  | Word16 of int16
+  | Word32 of int32
+  | Word64 of int64
+
+type lit =
+  | NullLit
+  | BoolLit of bool
+  | NatLit of nat
+  | IntLit of int
+  | WordLit of word
+  | FloatLit of float
+  | CharLit of unicode
+  | TextLit of string
+
+type unop =
+  | PosOp                                        (* +x *)
+  | NegOp                                        (* -x *)
+  | NotOp                                        (* bitwise negation *)
+
+type binop =
+  | AddOp                                        (* x+y *)
+  | SubOp                                        (* x-y *)
+  | MulOp                                        (* x*y *)
+  | DivOp                                        (* x/y *)
+  | ModOp                                        (* x%y *)
+  | AndOp                                        (* bitwise operators... *)
+  | OrOp
+  | XorOp
+  | ShiftLOp
+  | ShiftROp
+  | RotLOp
+  | RotROp
+  | CatOp                                        (* concatenation *)
