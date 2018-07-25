ec6611ee2af52e6f37af1cd3a5a980e39a334a4e
Andreas Rossberg <rossberg@mpi-sws.org>
2018-07-25 11:22:25 +0200

    Add interface

---
+++ src/typing.mli
@@ -0,0 +1,36 @@
+open Types
+
+
+module Env : Map.S with type key = string
+
+val lookup : 'a Env.t -> string -> 'a option
+val union : 'a Env.t -> 'a Env.t -> 'a Env.t
+
+
+type val_env = (typ * mut) Env.t
+type con_env = con Env.t
+type kind_env = kind ConEnv.t
+
+type context =
+  {
+    values : val_env;
+    constructors : con_env;
+    kinds : kind_env;
+    breaks : typ Env.t;
+    continues : unit Env.t;
+    returns : typ option;
+    awaitable : bool
+  }
+
+val empty_context : context
+val union_values : context -> val_env -> context
+val union_constructors : context -> con_env -> context
+val union_kinds : context -> kind_env -> context
+
+
+exception KindError of Source.region * string
+exception TypeError of Source.region * string
+
+val norm_typ : context -> Types.typ -> Types.typ
+
+val check_prog : Syntax.prog -> val_env * con_env * kind_env
