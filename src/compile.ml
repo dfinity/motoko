e8c7e8bd6a3f4c505590ccb47b7e1bd13d56b7dd
Joachim Breitner <mail@joachim-breitner.de>
2018-09-01 15:09:50 +0200

    -c: Print all encountered unsupported terms on stderr

--- src/compile.ml
+++ src/compile.ml
@@ -8,6 +8,8 @@ module LVE = Env.Make(String)
 
 let nr x = { Wasm.Source.it = x; Wasm.Source.at = Wasm.Source.no_region }
 
+let todo fn se x = Printf.eprintf "%s: %s" fn (Wasm.Sexpr.to_string 80 se); x
+
 let reg (ref : 'a list ref) (x : 'a) =
     let i = Wasm.I32.of_int_u (List.length !ref) in
     ref := !ref @ [ x ];
@@ -19,16 +21,16 @@ let compile_lit lit = match lit with
   (* This maps int to int32, instead of a proper arbitrary precision library *)
   | IntLit n      -> [ nr (Wasm.Ast.Const (nr (Wasm.Values.I32 (Z.to_int32 n)))) ]
   | NatLit n      -> [ nr (Wasm.Ast.Const (nr (Wasm.Values.I32 (Z.to_int32 n)))) ]
-  | _ -> [ nr Unreachable ]
+  | _ -> todo "compile_lit" (Arrange.lit lit) [ nr Unreachable ]
 
 let compile_binop op = match op with
   | AddOp -> [ nr (Binary (Wasm.Values.I32 Wasm.Ast.I32Op.Add)) ]
   | SubOp -> [ nr (Binary (Wasm.Values.I32 Wasm.Ast.I32Op.Sub)) ]
-  | _ -> [ nr Unreachable ]
+  | _ -> todo "compile_binop" (Arrange.binop op) [ nr Unreachable ]
 
 let compile_relop op = match op with
   | EqOp -> [ nr (Compare (Wasm.Values.I32 Wasm.Ast.I32Op.Eq)) ]
-  | _ -> [ nr Unreachable ]
+  | _ -> todo "compile_relop" (Arrange.relop op) [ nr Unreachable ]
 
 let rec compile_exp locals lve exp = match exp.it with
   | VarE var -> [ nr (GetLocal (nr (LVE.find var.it lve))) ]
@@ -44,7 +46,7 @@ let rec compile_exp locals lve exp = match exp.it with
      compile_exp locals lve e1 @
      compile_exp locals lve e2 @
      compile_relop op
-  | _ -> [ nr Unreachable ]
+  | _ -> todo "compile_exp" (Arrange.exp exp) [ nr Unreachable ]
 
 and compile_decs locals lve decs = match decs with
   | []          -> ([], lve)
@@ -60,7 +62,7 @@ and compile_dec locals lve dec = match dec.it with
       let i = reg locals I32Type; in
       let lve1 = LVE.add name i lve in
       (code1 @ [ nr (SetLocal (nr i) ) ], lve1)
-  | _      -> ([], lve)
+  | _ -> todo "compile_dec" (Arrange.dec dec) ([], lve)
 
 let compile_func_body (funcs : func list ref) (func_types : type_ list ref) (decs : dec list) :  func =
   let locals = ref [] in
