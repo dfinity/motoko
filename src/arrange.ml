1a477d42242ba9140e7f3a1255c69c126c9daef5
Joachim Breitner <mail@joachim-breitner.de>
2018-09-03 10:36:54 +0200

    De-nit-pick arrange.ml
     * use `$$` instead of `<>` (both precedence lower than `@`)
     * remove spurious spaces

--- src/arrange.ml
+++ src/arrange.ml
@@ -2,64 +2,64 @@ open Source
 open Syntax
 open Wasm.Sexpr
 
-let (<>) head inner = Node (head, inner)
+let ($$) head inner = Node (head, inner)
 
 let rec exp e = match e.it with
-  | VarE i              -> "VarE"    <> [id i]
-  | LitE l              -> "LitE"    <> [lit !l]
-  | UnE (uo, e)         -> "UnE"     <> [unop uo; exp e]
-  | BinE (e1, bo, e2)   -> "BinE"    <> [exp e1; binop bo; exp e2]
-  | RelE (e1, ro, e2)   -> "RelE"    <> [exp e1; relop ro; exp e2]
-  | TupE es             -> "TupE"    <> List.map exp es
-  | ProjE (e, i)        -> "ProjE"   <> [exp e; Atom (string_of_int i)]
-  | ObjE (s, i, efs)    -> "ObjE"    <> [sort s; id i] @ List.map exp_field efs
-  | DotE (e, i)         -> "DotE"    <> [exp e; id i]
-  | AssignE (e1, e2)    -> "AssignE" <> [exp e1; exp e2]
-  | ArrayE es           -> "ArrayE"  <> List.map exp es
-  | IdxE (e1, e2)       -> "IdxE"    <> [exp e1; exp e2]
-  | CallE (e1, ts, e2)  -> "CallE"   <> [exp e1] @ List.map typ ts @ [exp e2]
-  | BlockE ds           -> "BlockE"  <> List.map dec ds
-  | NotE e              -> "NotE"    <> [exp e]
-  | AndE (e1, e2)       -> "AndE"    <> [exp e1; exp e2]
-  | OrE (e1, e2)        -> "OrE"     <> [exp e1; exp e2]
-  | IfE (e1, e2, e3)    -> "IfE"     <> [exp e1; exp e2; exp e3]
-  | SwitchE (e, cs)     -> "SwitchE" <> [exp e] @ List.map case cs
-  | WhileE (e1, e2)     -> "WhileE"  <> [exp e1; exp e2]
-  | LoopE (e1, None)    -> "LoopE"   <> [exp e1]
-  | LoopE (e1, Some e2) -> "LoopE"   <> [exp e1; exp e2]
-  | ForE (p, e1, e2)    -> "OrE"     <> [pat p; exp e1; exp e2]
-  | LabelE (i, t, e)    -> "LabelE"  <> [id i; typ t; exp e]
-  | BreakE (i, e)       -> "BreakE"  <> [id i; exp e]
-  | RetE e              -> "RetE"    <> [exp e]
-  | AsyncE e            -> "AsyncE"  <> [exp e]
-  | AwaitE e            -> "AwaitE"  <> [exp e]
-  | AssertE e           -> "AssertE" <> [exp e]
-  | IsE (e, t)          -> "IsE"     <> [exp e; typ t]
-  | AnnotE (e, t)       -> "AnnotE"  <> [exp e; typ t]
-  | DecE d              -> "DecE"    <> [dec d]
+  | VarE i              -> "VarE"    $$ [id i]
+  | LitE l              -> "LitE"    $$ [lit !l]
+  | UnE (uo, e)         -> "UnE"     $$ [unop uo; exp e]
+  | BinE (e1, bo, e2)   -> "BinE"    $$ [exp e1; binop bo; exp e2]
+  | RelE (e1, ro, e2)   -> "RelE"    $$ [exp e1; relop ro; exp e2]
+  | TupE es             -> "TupE"    $$ List.map exp es
+  | ProjE (e, i)        -> "ProjE"   $$ [exp e; Atom (string_of_int i)]
+  | ObjE (s, i, efs)    -> "ObjE"    $$ [sort s; id i] @ List.map exp_field efs
+  | DotE (e, i)         -> "DotE"    $$ [exp e; id i]
+  | AssignE (e1, e2)    -> "AssignE" $$ [exp e1; exp e2]
+  | ArrayE es           -> "ArrayE"  $$ List.map exp es
+  | IdxE (e1, e2)       -> "IdxE"    $$ [exp e1; exp e2]
+  | CallE (e1, ts, e2)  -> "CallE"   $$ [exp e1] @ List.map typ ts @ [exp e2]
+  | BlockE ds           -> "BlockE"  $$ List.map dec ds
+  | NotE e              -> "NotE"    $$ [exp e]
+  | AndE (e1, e2)       -> "AndE"    $$ [exp e1; exp e2]
+  | OrE (e1, e2)        -> "OrE"     $$ [exp e1; exp e2]
+  | IfE (e1, e2, e3)    -> "IfE"     $$ [exp e1; exp e2; exp e3]
+  | SwitchE (e, cs)     -> "SwitchE" $$ [exp e] @ List.map case cs
+  | WhileE (e1, e2)     -> "WhileE"  $$ [exp e1; exp e2]
+  | LoopE (e1, None)    -> "LoopE"   $$ [exp e1]
+  | LoopE (e1, Some e2) -> "LoopE"   $$ [exp e1; exp e2]
+  | ForE (p, e1, e2)    -> "OrE"     $$ [pat p; exp e1; exp e2]
+  | LabelE (i, t, e)    -> "LabelE"  $$ [id i; typ t; exp e]
+  | BreakE (i, e)       -> "BreakE"  $$ [id i; exp e]
+  | RetE e              -> "RetE"    $$ [exp e]
+  | AsyncE e            -> "AsyncE"  $$ [exp e]
+  | AwaitE e            -> "AwaitE"  $$ [exp e]
+  | AssertE e           -> "AssertE" $$ [exp e]
+  | IsE (e, t)          -> "IsE"     $$ [exp e; typ t]
+  | AnnotE (e, t)       -> "AnnotE"  $$ [exp e; typ t]
+  | DecE d              -> "DecE"    $$ [dec d]
 
 and pat p = match p.it with
   | WildP         -> Atom "WildP"
-  | VarP i        -> "VarP"       <> [ id i]
-  | TupP ps       -> "TupP"       <> List.map pat ps
-  | AnnotP (p, t) -> "AnnotP"     <> [ pat p; typ t]
-  | LitP l        -> "LitP"       <> [ lit !l ]
-  | SignP (uo, l) -> "SignP"      <> [ unop uo ; lit !l ]
+  | VarP i        -> "VarP"       $$ [ id i]
+  | TupP ps       -> "TupP"       $$ List.map pat ps
+  | AnnotP (p, t) -> "AnnotP"     $$ [ pat p; typ t]
+  | LitP l        -> "LitP"       $$ [ lit !l ]
+  | SignP (uo, l) -> "SignP"      $$ [ unop uo ; lit !l ]
 
 and lit (l:lit) = match l with
   | NullLit       -> Atom "NullLit"
-  | BoolLit true  -> "BoolLit"   <> [ Atom "true" ]
-  | BoolLit false -> "BoolLit"   <> [ Atom "false" ]
-  | NatLit n      -> "NatLit"    <> [ Atom (Z.to_string n) ]
-  | IntLit i      -> "IntLit"    <> [ Atom (Z.to_string i) ]
-  | Word8Lit w    -> "Word8Lit"  <> [ Atom (Value.Word8.to_string_u w) ]
-  | Word16Lit w   -> "Word16Lit" <> [ Atom (Value.Word16.to_string_u w) ]
-  | Word32Lit w   -> "Word32Lit" <> [ Atom (Value.Word32.to_string_u w) ]
-  | Word64Lit w   -> "Word64Lit" <> [ Atom (Value.Word64.to_string_u w) ]
-  | FloatLit f    -> "FloatLit"  <> [ Atom (Value.Float.to_string f) ]
-  | CharLit c     -> "CharLit"   <> [ Atom (string_of_int c) ]
-  | TextLit t     -> "TextLit"   <> [ Atom t ]
-  | PreLit (s,p)  -> "PreLit"    <> [ Atom s; prim p]
+  | BoolLit true  -> "BoolLit"   $$ [ Atom "true" ]
+  | BoolLit false -> "BoolLit"   $$ [ Atom "false" ]
+  | NatLit n      -> "NatLit"    $$ [ Atom (Z.to_string n) ]
+  | IntLit i      -> "IntLit"    $$ [ Atom (Z.to_string i) ]
+  | Word8Lit w    -> "Word8Lit"  $$ [ Atom (Value.Word8.to_string_u w) ]
+  | Word16Lit w   -> "Word16Lit" $$ [ Atom (Value.Word16.to_string_u w) ]
+  | Word32Lit w   -> "Word32Lit" $$ [ Atom (Value.Word32.to_string_u w) ]
+  | Word64Lit w   -> "Word64Lit" $$ [ Atom (Value.Word64.to_string_u w) ]
+  | FloatLit f    -> "FloatLit"  $$ [ Atom (Value.Float.to_string f) ]
+  | CharLit c     -> "CharLit"   $$ [ Atom (string_of_int c) ]
+  | TextLit t     -> "TextLit"   $$ [ Atom t ]
+  | PreLit (s,p)  -> "PreLit"    $$ [ Atom s; prim p]
 
 
 and unop uo = match uo with
@@ -68,12 +68,12 @@ and unop uo = match uo with
   | NotOp -> Atom "NotOp"
 
 and binop bo = match bo with
-  | AddOp    -> Atom "AddOp "
-  | SubOp    -> Atom "SubOp "
-  | MulOp    -> Atom "MulOp "
-  | DivOp    -> Atom "DivOp "
-  | ModOp    -> Atom "ModOp "
-  | AndOp    -> Atom "AndOp "
+  | AddOp    -> Atom "AddOp"
+  | SubOp    -> Atom "SubOp"
+  | MulOp    -> Atom "MulOp"
+  | DivOp    -> Atom "DivOp"
+  | ModOp    -> Atom "ModOp"
+  | AndOp    -> Atom "AndOp"
   | OrOp     -> Atom "OrOp"
   | XorOp    -> Atom "XorOp"
   | ShiftLOp -> Atom "ShiftLOp"
@@ -90,7 +90,7 @@ and relop ro = match ro with
   | LeOp  -> Atom "LeOp"
   | GeOp  -> Atom "GeOp"
 
-and case c = "case" <> [pat c.it.pat; exp c.it.exp]
+and case c = "case" $$ [pat c.it.pat; exp c.it.exp]
 
 and prim p = Atom (Type.string_of_prim p)
 
@@ -107,36 +107,36 @@ and priv p = match p.it with
   | Private -> Atom "Private"
 
 and typ_field (tf : typ_field)
-  = tf.it.id.it <> [typ tf.it.typ; mut tf.it.mut]
+  = tf.it.id.it $$ [typ tf.it.typ; mut tf.it.mut]
 
 and typ_bind (tb : typ_bind)
-  = tb.it.var.it <> [typ tb.it.bound]
+  = tb.it.var.it $$ [typ tb.it.bound]
 
 and exp_field (ef : exp_field)
-  = ef.it.id.it <> [exp ef.it.exp; mut ef.it.mut; priv ef.it.priv]
+  = ef.it.id.it $$ [exp ef.it.exp; mut ef.it.mut; priv ef.it.priv]
 
 
 and typ t = match t.it with
-  | VarT (s,ts)         -> "VarT" <> [id s] @ List.map typ ts
-  | PrimT p             -> "PrimT" <> [prim p]
-  | ObjT (s,ts)         -> "ObjT" <> [sort s] @ List.map typ_field ts
-  | ArrayT (m,t)        -> "ArrayT" <> [mut m; typ t]
-  | OptT t              -> "OptT" <> [typ t]
-  | TupT ts             -> "TupT" <> List.map typ ts
-  | FuncT (tbs, at, rt) -> "FuncT" <> List.map typ_bind tbs @ [ typ at; typ rt]
-  | AsyncT t            -> "AsyncT" <> [typ t]
-  | LikeT  t            -> "LikeT" <> [typ t]
+  | VarT (s,ts)         -> "VarT" $$ [id s] @ List.map typ ts
+  | PrimT p             -> "PrimT" $$ [prim p]
+  | ObjT (s,ts)         -> "ObjT" $$ [sort s] @ List.map typ_field ts
+  | ArrayT (m,t)        -> "ArrayT" $$ [mut m; typ t]
+  | OptT t              -> "OptT" $$ [typ t]
+  | TupT ts             -> "TupT" $$ List.map typ ts
+  | FuncT (tbs, at, rt) -> "FuncT" $$ List.map typ_bind tbs @ [ typ at; typ rt]
+  | AsyncT t            -> "AsyncT" $$ [typ t]
+  | LikeT  t            -> "LikeT" $$ [typ t]
   | AnyT                -> Atom "AnyT"
 
 and id i = Atom i.it
 
 and dec d = match d.it with
-  | ExpD e ->      "ExpD" <> [exp e ]
-  | LetD (p, e) -> "LetD" <> [pat p; exp e]
-  | VarD (i, e) -> "VarD" <> [id i; exp e]
+  | ExpD e ->      "ExpD" $$ [exp e ]
+  | LetD (p, e) -> "LetD" $$ [pat p; exp e]
+  | VarD (i, e) -> "VarD" $$ [id i; exp e]
   | FuncD (i, tp, p, t, e) ->
-    "FuncD" <> [id i] @ List.map typ_bind tp @ [pat p; typ t; exp e]
+    "FuncD" $$ [id i] @ List.map typ_bind tp @ [pat p; typ t; exp e]
   | TypD (i, tp, t) ->
-    "TypD" <> [id i] @ List.map typ_bind tp @ [typ t]
+    "TypD" $$ [id i] @ List.map typ_bind tp @ [typ t]
   | ClassD (i, tp, s, p, efs) ->
-    "ClassD" <> [id i] @ List.map typ_bind tp @ [sort s; pat p] @ List.map exp_field efs
+    "ClassD" $$ [id i] @ List.map typ_bind tp @ [sort s; pat p] @ List.map exp_field efs
