89bc362080b1966d2b3ef152eed961cd065f0699
Joachim Breitner <mail@joachim-breitner.de>
2018-09-03 12:55:15 +0200

    Refactor: Introduce an env in compile.ml

--- src/compile.ml
+++ src/compile.ml
@@ -4,8 +4,6 @@ open Wasm.Types
 open Source
 open Syntax
 
-module LVE = Env.Make(String)
-
 let nr x = { Wasm.Source.it = x; Wasm.Source.at = Wasm.Source.no_region }
 let nr_ x = { it = x; at = no_region; note = () }
 let nr__ x = { it = x; at = no_region; note = {note_typ = Type.Any; note_eff = Type.Triv } }
@@ -25,10 +23,38 @@ let update_reg (ref : 'a list ref) i (y : 'a) =
             | (x :: xs, i) -> x :: update xs (i - 1) in
   ref := update !ref (int_of_string (Wasm.I32.to_string_u i))
 
-let rec drop i (xs : 'a list) = match i, xs with
-  | 0, xs      -> xs
-  | _, []      -> []
-  | i, (_::xs) -> drop (i-1) xs
+
+(* The compiler environment.
+
+Almost immutable..
+the mutable parts (`ref`) are used to register things like locals and functions.
+This should be monotone in some sense: Stuff is only added, and it should not
+have a semantics difference in which order they are added.
+
+*)
+
+module E = struct
+  module LVE = Env.Make(String)
+  type t = {
+    funcs : func list ref;
+    func_types : func_type Wasm.Source.phrase list ref;
+    n_param : int32; (* to calculate GetLocal indices *)
+    locals : value_type list ref;
+    lve : int32 LVE.t;
+  }
+
+  let lookup_var env var =
+    match LVE.find_opt var.it env.lve with
+      | Some i -> Some (nr i)
+      | None   -> Printf.eprintf "Could not find %s\n" var.it; None
+
+  let add_local (env : t) name =
+      let i = reg env.locals I32Type; in
+      let i' = Wasm.I32.add env.n_param i in
+      ({ env with lve = LVE.add name i' env.lve }, i')
+end
+open E
+(* remove *) module LVE = Env.Make(String)
 
 let heap_ptr_global = nr
       { gtype = GlobalType (I32Type, Mutable);
@@ -71,13 +97,13 @@ let compile_relop op = match op with
   | LtOp -> [ nr (Compare (Wasm.Values.I32 Wasm.Ast.I32Op.LtU)) ]
   | _ -> todo "compile_relop" (Arrange.relop op) [ nr Unreachable ]
 
-let rec compile_lexp funcs func_types locals lve exp code = match exp.it with
-  | VarE var -> (match LVE.find_opt var.it lve with
-      | Some i -> code @ [ nr (SetLocal (nr i)) ]
-      | None   -> Printf.eprintf "Could not find %s\n" var.it; [nr Unreachable])
+let rec compile_lexp (env : E.t) exp code = match exp.it with
+  | VarE var -> (match E.lookup_var env var with
+      | Some i -> code @ [ nr (SetLocal i) ]
+      | None   -> [nr Unreachable])
   | IdxE (e1,e2) ->
-     compile_exp funcs func_types locals lve e1 @ (* offset to array *)
-     compile_exp funcs func_types locals lve e2 @ (* idx *)
+     compile_exp env e1 @ (* offset to array *)
+     compile_exp env e2 @ (* idx *)
      [ nr (Wasm.Ast.Const (nr (Wasm.Values.I32 4l))) ] @
      [ nr (Binary (Wasm.Values.I32 Wasm.Ast.I32Op.Mul)) ] @
      [ nr (Binary (Wasm.Values.I32 Wasm.Ast.I32Op.Add)) ] @
@@ -85,16 +111,16 @@ let rec compile_lexp funcs func_types locals lve exp code = match exp.it with
      [ nr (Store {ty = I32Type; align = 0; offset = 0l; sz = None}) ]
   | _ -> todo "compile_lexp" (Arrange.exp exp) [ nr Unreachable ]
 
-and compile_exp funcs func_types locals lve exp = match exp.it with
-  | VarE var -> (match LVE.find_opt var.it lve with
-      | Some i -> [ nr (GetLocal (nr i)) ]
-      | None   -> Printf.eprintf "Could not find %s\n" var.it; [nr Unreachable])
+and compile_exp (env : E.t) exp = match exp.it with
+  | VarE var -> (match E.lookup_var env var with
+      | Some i -> [ nr (GetLocal i) ]
+      | None   -> [ nr Unreachable ])
   | AssignE (e1,e2) ->
-     let code1 = compile_exp funcs func_types locals lve e2 in
-     compile_lexp funcs func_types locals lve e1 code1
+     let code1 = compile_exp env e2 in
+     compile_lexp env e1 code1
   | IdxE (e1,e2) ->
-     compile_exp funcs func_types locals lve e1 @ (* offset to array *)
-     compile_exp funcs func_types locals lve e2 @ (* idx *)
+     compile_exp env e1 @ (* offset to array *)
+     compile_exp env e2 @ (* idx *)
      [ nr (Wasm.Ast.Const (nr (Wasm.Values.I32 4l))) ] @
      [ nr (Binary (Wasm.Values.I32 Wasm.Ast.I32Op.Mul)) ] @
      [ nr (Binary (Wasm.Values.I32 Wasm.Ast.I32Op.Add));
@@ -103,36 +129,36 @@ and compile_exp funcs func_types locals lve exp = match exp.it with
   | LitE l_ref ->
      compile_lit !l_ref
   | AssertE e1 ->
-     compile_exp funcs func_types locals lve e1 @ [ nr (If ([], [], [nr Unreachable])) ]
+     compile_exp env e1 @ [ nr (If ([], [], [nr Unreachable])) ]
   | BinE (e1, op, e2) ->
-     compile_exp funcs func_types locals lve e1 @
-     compile_exp funcs func_types locals lve e2 @
+     compile_exp env e1 @
+     compile_exp env e2 @
      compile_binop op
   | RelE (e1, op, e2) ->
-     compile_exp funcs func_types locals lve e1 @
-     compile_exp funcs func_types locals lve e2 @
+     compile_exp env e1 @
+     compile_exp env e2 @
      compile_relop op
   | OrE (e1, e2) ->
-     compile_exp funcs func_types locals lve e1 @
-     compile_exp funcs func_types locals lve e2 @
+     compile_exp env e1 @
+     compile_exp env e2 @
      [ nr (Binary (Wasm.Values.I32 IntOp.Or)) ]
   | IfE (e1, e2, e3) ->
-     let code1 = compile_exp funcs func_types locals lve e1 in
-     let code2 = compile_exp funcs func_types locals lve e2 in
-     let code3 = compile_exp funcs func_types locals lve e3 in
+     let code1 = compile_exp env e1 in
+     let code2 = compile_exp env e2 in
+     let code3 = compile_exp env e3 in
      code1 @ [ nr (If ([], code2, code3)) ]
   | BlockE decs ->
-     fst (compile_decs funcs func_types locals lve decs)
+     fst (compile_decs env decs)
   | LoopE (e, None) ->
-     let code = compile_exp funcs func_types locals lve e in
+     let code = compile_exp env e in
      [ nr (Loop ([], code @ [ nr (Br (nr 0l)) ])) ] @
      [ nr Unreachable ]
   | WhileE (e1, e2) ->
-     let code1 = compile_exp funcs func_types locals lve e1 in
-     let code2 = compile_exp funcs func_types locals lve e2 in
+     let code1 = compile_exp env e1 in
+     let code2 = compile_exp env e2 in
      [ nr (Loop ([], code1 @ [ nr (If ([], code2 @ [ nr (Br (nr 1l)) ], [])) ])) ]
-  | AnnotE (e, t) -> compile_exp funcs func_types locals lve e
-  | RetE e -> compile_exp funcs func_types locals lve e @ [ nr Return ]
+  | AnnotE (e, t) -> compile_exp env e
+  | RetE e -> compile_exp env e @ [ nr Return ]
   | TupE [] -> [] (* Fishy *)
   | ArrayE es ->
      (* Calculate size *)
@@ -144,25 +170,25 @@ and compile_exp funcs func_types locals lve exp = match exp.it with
         dup @ (* Dupliate position *)
 	[ nr (Wasm.Ast.Const (nr (Wasm.Values.I32 (Wasm.I32.of_int_u (4*i)))));
           nr (Binary (Wasm.Values.I32 Wasm.Ast.I32Op.Add)) ] @
-	compile_exp funcs func_types locals lve e @
+	compile_exp env e @
         [ nr (Store {ty = I32Type; align = 0; offset = 0l; sz = None}) ]
      in
      List.concat (List.mapi init_elem es)
   | CallE ({it = VarE var; _}, [], e) ->
      let args = de_tupleE e in
-     List.concat (List.map (compile_exp funcs func_types locals lve) args)
-     @ (match LVE.find_opt var.it lve with
+     List.concat (List.map (compile_exp env) args)
+     @ (match LVE.find_opt var.it env.lve with
         | Some i -> [ nr (Call (nr i)) ]
         | None   -> Printf.eprintf "Could not find %s\n" var.it; [nr Unreachable])
   | _ -> todo "compile_exp" (Arrange.exp exp) [ nr Unreachable ]
 
 (* TODO: Mutual recursion! *)
-and compile_decs funcs func_types locals lve decs = match decs with
-  | []          -> ([], lve)
+and compile_decs env decs = match decs with
+  | []          -> ([], env)
   | (dec::decs) ->
-      let (code1, lve1) = compile_dec  funcs func_types locals lve dec    in
-      let (code2, lve2) = compile_decs funcs func_types locals lve1 decs in
-      (code1 @ code2, lve2)
+      let (code1, env1) = compile_dec  env dec    in
+      let (code2, env2) = compile_decs env1 decs in
+      (code1 @ code2, env2)
 
 and de_tupleP p = match p.it with
   | TupP ps -> ps
@@ -179,18 +205,16 @@ and param_names ps =
     | _             -> nr_ "non-var-pattern" in
   List.map param_name ps
 
-and compile_dec funcs func_types locals lve dec = match dec.it with
-  | ExpD e -> (compile_exp funcs func_types locals lve e, lve)
+and compile_dec env dec = match dec.it with
+  | ExpD e -> (compile_exp env e, env)
   | LetD ({it = VarP {it = name; _}; _}, e) ->
-      let code1 = compile_exp funcs func_types locals lve e in
-      let i = reg locals I32Type; in
-      let lve1 = LVE.add name i lve in
-      (code1 @ [ nr (SetLocal (nr i) ) ], lve1)
+      let code1 = compile_exp env e in
+      let (env1,i) = E.add_local env name; in
+      (code1 @ [ nr (SetLocal (nr i) ) ], env1)
   | VarD ({it = name; _}, e) ->
-      let code1 = compile_exp funcs func_types locals lve e in
-      let i = reg locals I32Type; in
-      let lve1 = LVE.add name i lve in
-      (code1 @ [ nr (SetLocal (nr i) ) ], lve1)
+      let code1 = compile_exp env e in
+      let (env1, i) = add_local env name in
+      (code1 @ [ nr (SetLocal (nr i) ) ], env1)
   | FuncD ({it = name; _}, [], p, rt, e) ->
       let ps = de_tupleP p in
       let rt' = match rt.it with | TupT tys -> List.map (fun _ -> I32Type) tys
@@ -198,46 +222,51 @@ and compile_dec funcs func_types locals lve dec = match dec.it with
       let ty = nr (FuncType (List.map (fun _ -> I32Type) ps, rt')) in
       let params = param_names ps in
       let dummy_fun = nr {ftype = nr 0l; locals = []; body = []} in
-      let i = reg funcs dummy_fun in
-      let lve1 = LVE.add name i lve in
-      let f = compile_func_body funcs func_types ty params lve1 e in
-      update_reg funcs i f;
+      let i = reg env.funcs dummy_fun in
+      let env1 = { env with lve = LVE.add name i env.lve } in
+      let f = compile_func_body env1 params ty e in
+      update_reg env.funcs i f;
       (* TODO: currently, lve points to function ids for functions, and locals
          for others. This breaks once we no longer only call known non-closures. *)
-      ([], lve1)
+      ([], env1)
 
-  | _ -> todo "compile_dec" (Arrange.dec dec) ([], lve)
+  | _ -> todo "compile_dec" (Arrange.dec dec) ([], env)
 
-and compile_func_body
-      (funcs : func list ref) (func_types : type_ list ref)
-      func_type (params : id list) lve
-      (e : exp) :  func =
-  (* HACK: We add the params to the locals (so that the index lines up)
-     but we remove them before passing them on to wasm *)
-  (* Also, every function has a temporary local in slot 0 *)
-  let locals = ref (List.map (fun _ -> I32Type) params @ [ I32Type ]) in
-  (* We reuse the lve from outside. This is ok for now for functions, but
-     for other variables we need to support closures. *)
-  let lve1 = List.fold_left (fun lve (x,y) -> LVE.add x y lve) lve
-                           (List.mapi (fun i x -> (x.it,Wasm.I32.of_int_u i)) params) in
-  let body = compile_exp funcs func_types locals lve1 e in
-  let ti = reg func_types func_type in
+and compile_func_body env params func_type (e : exp) : func =
+  (* Fresh set of locals *)
+  (* Every function has a temporary local as local 0 *)
+  let locals = ref [ I32Type ] in
+  let env1 = { env with
+    locals;
+    (* We reuse the lve from outside. This is ok for now for functions, but
+       for other variables we need to support closures. *)
+    n_param = Wasm.I32.of_int_u (List.length params);
+    lve = List.fold_left (fun lve (x,y) -> LVE.add x y lve) env.lve
+               (List.mapi (fun i x -> (x.it, Wasm.I32.of_int_u i)) params);
+    } in
+  let body = compile_exp env1 e in
+  let ti = reg env.func_types func_type in
   nr { ftype = nr ti;
-       locals = drop (List.length params) !locals;
+       locals = !locals;
        body = body
      }
 
 let compile (prog  : Syntax.prog) : unit =
   let m : module_ =
-    let funcs = ref [] in
-    let func_types = ref [] in
     let start_fun_ty = nr (FuncType ([], [])) in
-    let start_fun = compile_func_body funcs func_types start_fun_ty [] LVE.empty (nr__ (BlockE prog.it)) in
-    let i = reg funcs start_fun in
+    let env = {
+      funcs = ref [];
+      E.func_types = ref [];
+      locals = ref []; (* Actually unused outside start_fun *)
+      lve = LVE.empty;
+      n_param = 0l;
+    } in
+    let start_fun = compile_func_body env [] start_fun_ty (nr__ (BlockE prog.it)) in
+    let i = reg env.funcs start_fun in
 
     nr { empty_module with
-      types = !func_types;
-      funcs = !funcs;
+      types = !(env.func_types);
+      funcs = !(env.funcs);
       start = Some (nr i);
       globals = [ heap_ptr_global ];
       memories = [nr {mtype = MemoryType {min = 100l; max = None}} ];
