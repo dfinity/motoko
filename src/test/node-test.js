59a83308e3404c16e623c3dda7edc2d15061c7fb
Joachim Breitner <mail@joachim-breitner.de>
2018-09-17 23:43:45 +0200

    Simplify node sanity check

--- src/test/node-test.js
+++ src/test/node-test.js
@@ -7,18 +7,7 @@ assert.equal(typeof(empty_wasm), 'string');
 assert.equal(empty_wasm.substr(0,4), '\0asm');
 assert.equal(empty_wasm.substr(4,4), '\1\0\0\0');
 
-/* There must be a better way, ideally of providing an ArrayBuffer directly in
- * asc.js... help appreciated. */
-function strToBuffer (string) {
-  let arrayBuffer = new ArrayBuffer(string.length * 1);
-  let newUint = new Uint8Array(arrayBuffer);
-  newUint.forEach((_, i) => {
-    newUint[i] = string.charCodeAt(i);
-  });
-  return newUint;
-}
-
-WebAssembly.compile(strToBuffer(empty_wasm))
+WebAssembly.compile(Buffer.from(empty_wasm,'ascii'))
   .catch(err => assert.fail(err));
 
 
