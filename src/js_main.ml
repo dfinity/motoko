5fcd1b55dd1d229fa2cfdb1539338be719af1267
Joachim Breitner <mail@joachim-breitner.de>
2018-09-19 10:56:43 +0200

    Take a `dfinity_mode` parmeter in the JS API
    `false` for plain WASM, `true`, for dfinity flavor, to be run on the
    hypervisor.

--- src/js_main.ml
+++ src/js_main.ml
@@ -1,19 +1,17 @@
-let senv, _ = Pipeline.init ()
-
 (* TBR: propagate error messages? *)
-let js_compile_with convert source =
-  match Pipeline.compile_string senv (Js.to_string source) "js-input" with
-  | Some (module_, _) -> Js.some (convert module_)
+let js_compile_with dfinity_mode convert source =
+  match Pipeline.compile_string dfinity_mode (Js.to_string source) "js-input" with
+  | Some module_ -> Js.some (convert module_)
   | None -> Js.null
 
-let js_compile_wat =
-  js_compile_with (fun m -> Js.string (Wasm.Sexpr.to_string 80 (Wasm.Arrange.module_ m)))
-let js_compile_wasm =
-  js_compile_with (fun m -> Js.bytestring (Wasm.Encode.encode m))
+let js_compile_wat dfinity_mode =
+  js_compile_with dfinity_mode (fun m -> Js.string (Wasm.Sexpr.to_string 80 (Wasm.Arrange.module_ m)))
+let js_compile_wasm dfinity_mode =
+  js_compile_with dfinity_mode (fun m -> Js.bytestring (Wasm.Encode.encode m))
 
 let () =
   Js.export "ActorScript"
     (object%js
-      method compileWat s = js_compile_wat s
-      method compileWasm s = js_compile_wasm s
+      method compileWat dfinity_mode s = js_compile_wat dfinity_mode s
+      method compileWasm dfinity_mode s = js_compile_wasm dfinity_mode s
     end);
--- test/node-test.js
+++ test/node-test.js
@@ -1,13 +1,21 @@
 var assert = require('assert');
 var m = require('asc.js');
 
-var empty_wasm = m.ActorScript.compileWasm('');
+var empty_wasm_plain = m.ActorScript.compileWasm(false,'');
+var empty_wasm_dfinity = m.ActorScript.compileWasm(true, '');
 
-assert.equal(typeof(empty_wasm), 'string');
-assert.equal(empty_wasm.substr(0,4), '\0asm');
-assert.equal(empty_wasm.substr(4,4), '\1\0\0\0');
+assert.equal(typeof(empty_wasm_plain), 'string');
+assert.equal(empty_wasm_plain.substr(0,4), '\0asm');
+assert.equal(empty_wasm_plain.substr(4,4), '\1\0\0\0');
 
-WebAssembly.compile(Buffer.from(empty_wasm,'ascii'))
+WebAssembly.compile(Buffer.from(empty_wasm_plain,'ascii'))
   .catch(err => assert.fail(err));
 
+assert.equal(typeof(empty_wasm_dfinity), 'string');
+assert.equal(empty_wasm_dfinity.substr(0,4), '\0asm');
+assert.equal(empty_wasm_dfinity.substr(4,4), '\1\0\0\0');
 
+WebAssembly.compile(Buffer.from(empty_wasm_dfinity,'ascii'))
+  .catch(err => assert.fail(err));
+
+assert.notEqual(empty_wasm_plain, empty_wasm_dfinity);
