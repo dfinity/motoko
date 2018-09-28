c6a9a5c85d7af281561cecda96652e1f19f28b07
Paul Young <paul.young@dfinity.org>
2018-09-28 12:12:57 -0700

    Format expected test value

--- test/node-test.js
+++ test/node-test.js
@@ -26,7 +26,23 @@ assert.notEqual(empty_wasm_plain.code, empty_wasm_dfinity.code);
 
 var bad_result = m.ActorScript.compileWasm(true,'1+');
 // Uncomment to see what to paste below
-// console.log(JSON.stringify(bad_result));
-assert.deepStrictEqual(bad_result,
-  {"diagnostics":[{"range":{"start":{"line":1,"character":2},"end":{"line":1,"character":2}},"source":"actorscript","message":"unexpected token"}],"code":null}
- );
+// console.log(JSON.stringify(bad_result, null, 2));
+assert.deepStrictEqual(bad_result, {
+  "diagnostics": [
+    {
+      "range": {
+        "start": {
+          "line": 1,
+          "character": 2
+        },
+        "end": {
+          "line": 1,
+          "character": 2
+        }
+      },
+      "source": "actorscript",
+      "message": "unexpected token"
+    }
+  ],
+  "code": null
+});
