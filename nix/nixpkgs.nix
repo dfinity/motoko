ef38b5126c55b1af670a0aec513bcffbf3ddc562
Paul Young <paul.young@dfinity.org>
2018-11-28 14:35:18 -0800

    Use nixpkgs from dev

--- nix/nixpkgs.nix
+++
@@ -1,6 +0,0 @@
-with { rev = "1ec18b032cb1178f273fede7849c065cfa376a88"; };
-
-import (builtins.fetchTarball {
-  sha256 = "044p9xy4yrqkcnr3a5rs3cn6ppvrsv1vsp8rzridmxrazw7692f3";
-  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-})
