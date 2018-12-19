6a09e461e478ad8a232f1f70c489af6d89dd9c50
Joachim Breitner <mail@joachim-breitner.de>
2018-12-19 15:11:06 +0100

    Copy nameRel from branch joachim/use-before-define

---
+++ src/nameRel.ml
@@ -0,0 +1,48 @@
+(* A data structure for binary relations on names *)
+
+module M = Env.Make(String)
+module S = Set.Make(String)
+
+type t = S.t M.t
+
+let empty : t = M.empty
+
+let diag : string list -> t =
+  List.fold_left (fun rel x -> M.add x (S.singleton x) rel) empty
+
+let lookup : string -> t -> S.t = fun k rel ->
+  match M.find_opt k rel with
+  | None -> S.empty
+  | Some s -> s
+
+(* dom R = { x | (x,y) ∈ R } *)
+let dom : t -> S.t = fun rel ->
+  M.fold (fun v s dom -> if S.is_empty s then dom else S.add v dom) rel S.empty
+
+(* remove_range R S = { (x,y) | (x,y) ∈ R ∧ y ∉ S } *)
+let remove_range : S.t -> t -> t = fun s ->
+  M.map (fun s' -> S.diff s' s)
+
+(* union R1 R2 = R1 ∪ R2 = { (x,y) | (x,y) ∈ R1 ∨ (x,y) ∈ R2 } *)
+let union : t -> t -> t =
+  M.union (fun _ s1 s2 -> Some (S.union s1 s2))
+
+(* prod S1 S2 = S1 × S2 *)
+let prod : S.t -> S.t -> t = fun s1 s2 ->
+  S.fold (fun x rel -> M.add x s2 rel) s1 empty
+
+(* Just a utility function *)
+let set_bind f s =
+  S.fold (fun y -> S.union (f y)) s S.empty
+
+(* comp R1 R2 = R1 ∘ R2 = { (x,z) | (x,y) ∈ R1 ∧ (y,z) ∈ R2 } *)
+let comp : t -> t -> t = fun rel1 rel2 ->
+  M.map (set_bind (fun y -> lookup y rel2)) rel1
+
+(* We can use a relation R to represent its transitive closure R⁺,
+   and operations like dom, union, prod, comp work just fine.
+   Only removing needs special care:*)
+
+(* remove_range_trans S R = R' where remove_range S R⁺ = R'⁺ *)
+let remove_range_trans : S.t -> t -> t = fun s rel ->
+  M.map (set_bind (fun y -> if S.mem y s then S.diff (lookup y rel) s else S.singleton y)) rel
