1308bad79c7de923677a34347fb162fad9825cf1
Joachim Breitner <mail@joachim-breitner.de>
2019-02-27 17:00:32 +0100

    Make $(ASC) dependency of _out/%.done optional
    So that one can run
    
        make test ASC=asc
    
    where `asc` is on the PATH.

--- test/quick.mk
+++ test/quick.mk
@@ -5,6 +5,7 @@ TO-TEST = $(patsubst %.as,_out/%.done,$(wildcard *.as))
 quick: $(TO-TEST)
 
 # run single test, e.g. make _out/AST-56.done
-_out/%.done: %.as $(ASC) ../run.sh
+.SECONDEXPANSION:
+_out/%.done: %.as $$(wildcard $(ASC)) ../run.sh
 	@ mkdir -p _out
 	@ (../run.sh $(RUNFLAGS) $< > $@.tmp && mv $@.tmp $@) || (cat $@.tmp; rm -f $@.tmp; false)
