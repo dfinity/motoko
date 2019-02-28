c67a0121f060eba2d3f3fa7c5fbff51eb88d6922
Gabor Greif <gabor@dfinity.org>
2019-02-28 14:33:28 +0100

    Revert "more precise Invalid_argument messages"
    This reverts commit 9c29a4846541b93e9023653172483eb6b7c07466.

--- src/operator.ml
+++ src/operator.ml
@@ -14,7 +14,7 @@ let word_unop fword8 fword16 fword32 fword64 = function
   | T.Word16 -> fun v -> Word16 (fword16 (as_word16 v))
   | T.Word32 -> fun v -> Word32 (fword32 (as_word32 v))
   | T.Word64 -> fun v -> Word64 (fword64 (as_word64 v))
-  | _ -> raise (Invalid_argument "word_unop")
+  | _ -> raise (Invalid_argument "unop")
 
 let num_unop fint fword8 fword16 fword32 fword64 ffloat = function
   | T.Int -> fun v -> Int (fint (as_int v))
@@ -37,14 +37,14 @@ let unop t op =
 
 let text_binop ftext = function
   | T.Text -> fun v1 v2 -> Text (ftext (as_text v1) (as_text v2))
-  | _ -> raise (Invalid_argument "text_binop")
+  | _ -> raise (Invalid_argument "binop")
 
 let word_binop fword8 fword16 fword32 fword64 = function
   | T.Word8 -> fun v1 v2 -> Word8 (fword8 (as_word8 v1) (as_word8 v2))
   | T.Word16 -> fun v1 v2 -> Word16 (fword16 (as_word16 v1) (as_word16 v2))
   | T.Word32 -> fun v1 v2 -> Word32 (fword32 (as_word32 v1) (as_word32 v2))
   | T.Word64 -> fun v1 v2 -> Word64 (fword64 (as_word64 v1) (as_word64 v2))
-  | _ -> raise (Invalid_argument "word_binop")
+  | _ -> raise (Invalid_argument "binop")
 
 let num_binop fnat fint fword8 fword16 fword32 fword64 ffloat = function
   | T.Nat -> fun v1 v2 -> Int (fnat (as_int v1) (as_int v2))
@@ -82,7 +82,7 @@ let word_relop fword8 fword16 fword32 fword64 = function
   | T.Word16 -> fun v1 v2 -> Bool (fword16 (as_word16 v1) (as_word16 v2))
   | T.Word32 -> fun v1 v2 -> Bool (fword32 (as_word32 v1) (as_word32 v2))
   | T.Word64 -> fun v1 v2 -> Bool (fword64 (as_word64 v1) (as_word64 v2))
-  | _ -> raise (Invalid_argument "word_relop")
+  | _ -> raise (Invalid_argument "relop")
 
 let num_relop fnat fint fword8 fword16 fword32 fword64 ffloat = function
   | T.Nat -> fun v1 v2 -> Bool (fnat (as_int v1) (as_int v2))
