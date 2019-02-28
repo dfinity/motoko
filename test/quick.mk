e622174259fb00b1a3e4b6df0d88968b3f54fcaa
Gabor Greif <gabor@dfinity.org>
2019-02-28 21:12:10 +0100

    use order-only prereq for _out

--- test/quick.mk
+++ test/quick.mk
@@ -2,10 +2,14 @@
 
 TO-TEST = $(patsubst %.as,_out/%.done,$(wildcard *.as))
 
+.PHONY: quick
+
 quick: $(TO-TEST)
+
+_out:
+	@ mkdir -p $@
 
 # run single test, e.g. make _out/AST-56.done
 .SECONDEXPANSION:
-_out/%.done: %.as $$(wildcard $(ASC)) ../run.sh
-	@ mkdir -p _out
+_out/%.done: %.as $$(wildcard $(ASC)) ../run.sh  | _out
 	@ (../run.sh $(RUNFLAGS) $< > $@.tmp && mv $@.tmp $@) || (cat $@.tmp; rm -f $@.tmp; false)
