80f73df28b03ca0da489f6024bf71b36a5416347
Matthew Hammer <matthew@dfinity.org>
2019-03-08 11:06:22 -0700

    initial ActorScript mode for emacs

---
+++ emacs/actorscript-mode.el
@@ -0,0 +1,131 @@
+;; ActorScript major mode for Emacs
+;; initially based on Swift Mode.
+
+(setq actorscript-font-lock-keywords
+      (let* (
+             ;; define several category of keywords
+             ;; these are each taken from either ActorScript's `lexer.mll' or `prelude.ml' files.
+             (x-types
+              '("Any"
+                "None"
+                "Shared"
+                "Null"
+                "Bool"
+                "Nat"
+                "Int"
+                "Word8"
+                "Word16"
+                "Word32"
+                "Word64"
+                "Float"
+                "Char"
+                "Text"))
+              (x-constants
+               '("null"
+                 "true"
+                 "false"
+                 ))
+              (x-keywords
+               '("actor"
+                 "and"
+                 "async"
+                 "assert"
+                 "await"
+                 "break"
+                 "case"
+                 "class"
+                 "continue"
+                 "label"
+                 "else"
+                 "for"
+                 "func"
+                 "if"
+                 "in"
+                 "new"
+                 "not"
+                 "object"
+                 "or"
+                 "let"
+                 "loop"
+                 "private"
+                 "return"
+                 "shared"
+                 "switch"
+                 "type"
+                 "var"
+                 "while"
+                 "prim" ))
+              (x-symbols
+               '( ;"("
+                  ;")"
+                  ;"["
+                  ;"]"
+                  ;"{"
+                  ;"}"
+                  ;";"
+                  ;","
+                  ":"
+                  "<:"
+                  ;"."
+                  "?"
+                  "="
+                  "<"
+                  ">"
+                  ;"+"
+                  "-"
+                  "*"
+                  "/"
+                  "%"
+                  "**"
+                  "&"
+                  ;"|"
+                  ;"^"
+                  "<<"
+                  ">>"
+                  "<<>"
+                  "<>>"
+                  "#"
+                  "=="
+                  "!="
+                  ">="
+                  "<="
+                  ":="
+                  "+="
+                  "-="
+                  "*="
+                  "/="
+                  "%="
+                  "**="
+                  "&="
+                  "|="
+                  "^="
+                  "<<="
+                  ">>="
+                  "<<>="
+                  "<>>="
+                  "#="
+                  ))
+        ;; generate regex string for each category of keywords
+        (x-types-regexp (regexp-opt x-types 'words))
+        (x-constant-regexp (regexp-opt x-constants 'words))
+        (x-keywords-regexp (regexp-opt x-keywords 'words))
+        (x-symbols-regexp (regexp-opt x-symbols 'words))
+        )
+        ;;
+        `(
+          (,x-types-regexp . font-lock-type-face)
+          (,x-constant-regexp . font-lock-constant-face)
+          (,x-keywords-regexp . font-lock-keyword-face)
+          (,x-symbols-regexp . font-lock-keyword-face)
+          )))
+
+(define-derived-mode actorscript-mode
+  swift-mode "ActorScript"
+  "Major mode for ActorScript, aka 'CanisterScript'."
+  (setq font-lock-defaults '((actorscript-font-lock-keywords)))
+  )
+
+(add-to-list 'auto-mode-alist '("\\.as\\'" . actorscript-mode))
+
+;; add the mode to the `features' list
+(provide 'actorscript-mode)
