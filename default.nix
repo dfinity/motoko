749a6e874a905749a0df653fc60a8a616dd4c289
Joachim Breitner <mail@joachim-breitner.de>
2019-03-26 23:18:07 +0100

    https://github.com/dfinity-lab/dev/pull/697 got merged into dev

--- default.nix
+++ default.nix
@@ -35,8 +35,8 @@ let real-dvm =
     then
       let dev = builtins.fetchGit {
         url = "ssh://git@github.com/dfinity-lab/dev";
-        ref = "gabor/unicode";
-        rev = "9ab39df3b38418e113bcb2e77e31709fc2a3d031";
+        ref = "master";
+        rev = "32794f05c17a4083bccadfa40396a20c5b08e3de";
       }; in
       (import dev {}).dvm
     else null
