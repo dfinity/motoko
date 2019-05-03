d2ec0a3cee631c8bbef092c3f09c51fe14d770e0
chenyan-dfinity <yan.chen@dfinity.org>
2019-05-03 14:18:28 -0700

    - Add backend to emit javascript code, just pretty printer for now. - Remove dependency on AS src TODO: type def cycle detection, enum, import

---
+++ idl/compile_js.ml
@@ -0,0 +1,86 @@
+
+open Format
+open Syntax_idl
+open Source
+
+let str ppf s = pp_print_string ppf s; pp_print_cut ppf ()
+let id ppf s = str ppf s.it; pp_print_cut ppf ()
+let space = pp_print_space             
+let kwd ppf s = str ppf s; space ppf ()
+                
+let rec concat ppf f sep list =
+  match list with
+  | [] -> ()
+  | e::[] -> f ppf e; pp_print_cut ppf ()
+  | e::tail -> f ppf e; str ppf sep; space ppf (); concat ppf f sep tail
+
+let rec pp_typ ppf t =
+  pp_open_box ppf 1;
+  (match t.it with
+  | VarT s -> id ppf s
+  | PrimT p -> str ppf ("IDL."^(Arrange_idl.string_of_prim p))
+  | RecordT ts ->
+     str ppf "IDL.Obj({";
+     concat ppf pp_field "," ts;
+     str ppf "})";
+  | VecT t -> str ppf "IDL.Arr("; pp_typ ppf t; str ppf ")";
+  | OptT t -> str ppf "IDL.Opt("; pp_typ ppf t; str ppf ")";
+  | VariantT ts -> str ppf "IDL.Variant({"; concat ppf pp_field "," ts; str ppf "})";
+  | TupT ts -> str ppf "IDL.Tuple("; concat ppf pp_typ "," ts; str ppf ")";
+  | FuncT (ms, t1, t2) ->
+     str ppf "IDL.message(";
+     pp_typ ppf t1;
+     kwd ppf ",";
+     pp_typ ppf t2;
+     str ppf ")";
+  | ServT ts ->
+     pp_open_hovbox ppf 1;
+     str ppf "IDL.ActorInterface({";
+     concat ppf pp_meth "," ts;
+     str ppf "})";
+     pp_close_box ppf ();
+  | PreT -> ()
+  );
+  pp_close_box ppf ()
+
+and pp_field ppf tf =
+  pp_open_box ppf 1;
+  id ppf tf.it.name; kwd ppf ":"; pp_typ ppf tf.it.typ;
+  pp_close_box ppf ()
+
+and pp_meth ppf meth =
+  pp_open_box ppf 1;
+  id ppf meth.it.var;
+  kwd ppf ":";
+  pp_typ ppf meth.it.bound;
+  pp_close_box ppf ()
+  
+let pp_dec ppf dec =
+  pp_open_hovbox ppf 1;
+  kwd ppf "const";
+  (match dec.it with
+   | TypD (x, t) ->
+      kwd ppf x.it;
+      kwd ppf "="; kwd ppf "()"; kwd ppf "=>";
+      pp_typ ppf t
+   | ActorD (x, tp) ->
+      id ppf x; space ppf (); kwd ppf "="; kwd ppf "new";
+      str ppf "IDL.ActorInterface({";
+      concat ppf pp_meth "," tp;
+      str ppf "})"
+  | ActorVarD (x, var) -> id ppf x; space ppf (); kwd ppf "="; id ppf var;
+  );
+  pp_close_box ppf ();
+  pp_print_cut ppf ()
+    
+let pp_prog ppf prog =
+  pp_open_vbox ppf 0;
+  List.map (pp_dec ppf) prog.it;
+  pp_close_box ppf ()
+   
+let compile (scope : Typing_idl.scope) (prog : Syntax_idl.prog) =
+  let buf = Buffer.create 100 in
+  let ppf = formatter_of_buffer buf in
+  pp_prog ppf prog;
+  pp_print_flush ppf ();
+  buf
---
+++ idl/flags.ml
@@ -0,0 +1,11 @@
+let trace = ref false
+let verbose = ref false
+let print_depth = ref 2
+let await_lowering = ref true
+let async_lowering = ref true
+let dump_parse = ref false
+let dump_tc = ref false
+let dump_lowering = ref false
+let interpret_ir = ref false
+let source_map = ref false
+let prelude = ref true
