dfinity/motoko

commit 508054834ed82ee55031ffeb23a0278d12b633b1

Backend: link against as-rts.wasm (#383)
this is the next (and probabaly second-to-last) chunk of work from #349:
Linking as-rts.wasm in the backend.

The `--no-link` flag is mostly for debugging; the unlinked module
isn’t yet useful (but might be if we start to do dynamic linking on the
chain).

default.nix
@@ -141,8 +141,8 @@ rec {
     '';
   };
 
-  native = stdenv.mkDerivation {
-    name = "asc";
+  asc-bin = stdenv.mkDerivation {
+    name = "asc-bin";
 
     src = sourceByRegex ./. [
       "src/"
@@ -172,6 +172,16 @@ rec {
     '';
   };
 
+  native = nixpkgs.symlinkJoin {
+    name = "asc";
+    paths = [ asc-bin rts ];
+    buildInputs = [ nixpkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/asc \
+        --set-default ASC_RTS "$out/rts/as-rts.wasm"
+    '';
+  };
+
   native_test = stdenv.mkDerivation {
     name = "native.test";
 
@@ -190,7 +200,6 @@ rec {
       ] ++
       (if test-dvm then [ real-dvm ] else []) ++
       llvmBuildInputs;
-
     buildPhase = ''
         patchShebangs .
         ${llvmEnv}
@@ -210,8 +219,8 @@ rec {
     '';
   };
 
-  native-coverage = native.overrideAttrs (oldAttrs: {
-    name = "asc-coverage";
+  asc-bin-coverage = asc-bin.overrideAttrs (oldAttrs: {
+    name = "asc-bin-coverage";
     buildPhase =
       "export BISECT_COVERAGE=YES;" +
       oldAttrs.buildPhase;
@@ -221,8 +230,18 @@ rec {
       '';
   });
 
+  native-coverage = nixpkgs.symlinkJoin {
+    name = "asc-covergage";
+    paths = [ asc-bin-coverage rts ];
+    buildInputs = [ nixpkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/asc \
+        --set-default ASC_RTS "$out/rts/as-rts.wasm"
+    '';
+  };
+
   coverage-report = stdenv.mkDerivation {
-    name = "native.coverage";
+    name = "coverage-report";
 
     src = sourceByRegex ./. (
       test_files ++
@@ -257,7 +276,7 @@ rec {
   };
 
 
-  js = native.overrideAttrs (oldAttrs: {
+  js = asc-bin.overrideAttrs (oldAttrs: {
     name = "asc.js";
 
     buildInputs = commonBuildInputs ++ [
@@ -272,14 +291,15 @@ rec {
     '';
 
     installPhase = ''
-      mkdir -p $out
-      cp src/asc.js $out
+      mkdir -p $out/bin
+      cp -v src/asc.js $out/bin
+      cp -vr ${rts}/rts $out
     '';
 
-    doInstallCheck = true;
+    doInstallCheck = true; # need to fix loading the rts
 
     installCheckPhase = ''
-      NODE_PATH=$out/ node --experimental-wasm-mut-global --experimental-wasm-mv test/node-test.js
+      NODE_PATH=$out/bin node --experimental-wasm-mut-global --experimental-wasm-mv test/node-test.js
     '';
 
   });

src/compile.ml
@@ -165,6 +165,7 @@ module E = struct
     (* Static *)
     mode : mode;
     prelude : prog; (* The prelude. Re-used when compiling actors *)
+    rts : CustomModule.extended_module option; (* The rts. Re-used when compiling actors *)
     trap_with : t -> string -> G.t;
       (* Trap with message; in the env for dependency injection *)
 
@@ -199,8 +200,9 @@ module E = struct
   }
 
   (* The initial global environment *)
-  let mk_global mode prelude trap_with dyn_mem : t = {
+  let mk_global mode rts prelude trap_with dyn_mem : t = {
     mode;
+    rts;
     prelude;
     trap_with;
     local_vars_env = NameEnv.empty;
@@ -385,6 +387,7 @@ module E = struct
       | None   -> Printf.eprintf "Could not find %s\n" name.it; raise Not_found
 
   let get_prelude (env : t) = env.prelude
+  let get_rts (env : t) = env.rts
 
   let get_trap_with (env : t) = env.trap_with
   let trap_with env msg = env.trap_with env msg
@@ -585,15 +588,47 @@ module Func = struct
 
 end (* Func *)
 
+module RTS = struct
+  (* The connection to the C parts of the RTS *)
+  let system_imports env =
+    E.add_func_import env "rts" "as_memcpy" [I32Type; I32Type; I32Type] [];
+    E.add_func_import env "rts" "version" [] [I32Type];
+    E.add_func_import env "rts" "bigint_of_word32" [I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_to_word32" [I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_of_word64" [I64Type] [I32Type];
+    E.add_func_import env "rts" "bigint_to_word64" [I32Type] [I64Type];
+    E.add_func_import env "rts" "bigint_eq" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_lt" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_gt" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_le" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_ge" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_add" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_sub" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_mul" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_mod" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_div" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_neg" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_lshd" [I32Type; I32Type] [I32Type];
+    E.add_func_import env "rts" "bigint_abs" [I32Type] [I32Type];
+
+    E.add_export env (nr {
+      name = Wasm.Utf8.decode "alloc_bytes";
+      edesc = nr (FuncExport (nr (E.built_in env "alloc_bytes")))
+    });
+
+end (* RTS *)
+
 module Heap = struct
-  (* General heap object functionalty (allocation, setting fields, reading fields) *)
+  (* General heap object functionality (allocation, setting fields, reading fields) *)
 
   (* Memory addresses are 32 bit (I32Type). *)
   let word_size = 4l
 
   (* We keep track of the end of the used heap in this global, and bump it if
-     we allocate stuff. This the actual memory offset, not-skewed yet *)
-  let heap_global = 2l
+     we allocate stuff. This is the actual memory offset, not-skewed yet *)
+  let base_global = 3l
+  let heap_global = 4l
+  let get_heap_base = G.i (GlobalGet (nr base_global))
   let get_heap_ptr = G.i (GlobalGet (nr heap_global))
   let set_heap_ptr = G.i (GlobalSet (nr heap_global))
   let get_skewed_heap_ptr = get_heap_ptr ^^ compile_add_const ptr_skew
@@ -694,21 +729,7 @@ module Heap = struct
   (* Convenience functions related to memory *)
   (* Copying bytes (works on unskewed memory addresses) *)
   let memcpy env =
-    Func.share_code3 env "memcpy" (("to", I32Type), ("from", I32Type), ("n", I32Type)) [] (fun env get_to get_from get_n ->
-      get_n ^^
-      from_0_to_n env (fun get_i ->
-          get_to ^^
-          get_i ^^
-          G.i (Binary (Wasm.Values.I32 I32Op.Add)) ^^
-
-          get_from ^^
-          get_i ^^
-          G.i (Binary (Wasm.Values.I32 I32Op.Add)) ^^
-          G.i (Load {ty = I32Type; align = 0; offset = 0l; sz = Some (Wasm.Memory.Pack8, Wasm.Memory.ZX)}) ^^
-
-          G.i (Store {ty = I32Type; align = 0; offset = 0l; sz = Some Wasm.Memory.Pack8})
-      )
-    )
+    G.i (Call (nr (E.built_in env "rts_as_memcpy")))
 
   (* Copying words (works on skewed memory addresses) *)
   let memcpy_words_skewed env =
@@ -731,6 +752,22 @@ module Heap = struct
 
 end (* Heap *)
 
+module Stack = struct
+  (* The RTS includes C code which requires a shadow stack in linear memory.
+     We reserve some space for it at the beginning of memory space (just like
+     wasm-l would), this way stack overflow would cause out-of-memory, and not
+     just overwrite static data.
+
+     We don’t use the stack space (yet), but we could easily start to use it for
+     scratch space, as long as we don’t need more than 64k.
+  *)
+
+  let stack_global = 2l
+
+  let end_of_stack = page_size (* 64k of stack *)
+
+end (* Stack *)
+
 module ElemHeap = struct
   (* The ElemHeap adds a level of indirection for references (elements, as in
      ElemRef). This way, the fake orthogonal persistence code can easily
@@ -741,17 +778,14 @@ module ElemHeap = struct
      target orthogonal persistence anyways.
   *)
 
-  let ref_counter_global = 3l
+  let ref_counter_global = 5l
   let get_ref_ctr = G.i (GlobalGet (nr ref_counter_global))
   let set_ref_ctr = G.i (GlobalSet (nr ref_counter_global))
 
   (* For now, we allocate a fixed size range. This obviously cannot stay. *)
   let max_references = 1024l
 
-  (* By placing the ElemHeap at memory location 0, we incidentally make sure that
-     the 0l pointer is never a valid pointer.
-  *)
-  let ref_location = 0l
+  let ref_location = Stack.end_of_stack
 
   let table_end : int32 = Int32.(add ref_location (mul max_references Heap.word_size))
 
@@ -917,6 +951,8 @@ module Tagged = struct
 
      All tagged heap objects have a size of at least two words
      (important for GC, which replaces them with an Indirection).
+
+     Attention: This mapping is duplicated in rts/rts.c, so update both!
    *)
 
   type tag =
@@ -3361,7 +3397,7 @@ module GC = struct
     let (set_begin_to_space, get_begin_to_space) = new_local env "begin_to_space" in
     let (set_end_to_space, get_end_to_space) = new_local env "end_to_space" in
 
-    compile_unboxed_const end_of_static_space ^^ compile_add_const ptr_skew ^^ set_begin_from_space ^^
+    Heap.get_heap_base ^^ compile_add_const ptr_skew ^^ set_begin_from_space ^^
     Heap.get_skewed_heap_ptr ^^ set_begin_to_space ^^
     Heap.get_skewed_heap_ptr ^^ set_end_to_space ^^
 
@@ -4185,6 +4221,11 @@ and compile_exp (env : E.t) exp =
          compile_exp_vanilla env e ^^
          Prim.prim_abs env
 
+       | "rts_version" ->
+         SR.Vanilla,
+         compile_exp_as env SR.unit e ^^
+         G.i (Call (nr (E.built_in env "rts_version")))
+
        | "Nat->Word8"
        | "Int->Word8" ->
          SR.Vanilla,
@@ -4762,7 +4803,7 @@ This function compiles the prelude, just to find out the bound names.
 *)
 and find_prelude_names env =
   (* Create a throw-away environment *)
-  let env0 = E.mk_global (E.mode env) (E.get_prelude env) (fun _ _ -> G.i Unreachable) 0l in
+  let env0 = E.mk_global (E.mode env) None (E.get_prelude env) (fun _ _ -> G.i Unreachable) 0l in
   let env1 = E.mk_fun_env env0 0l 0 in
   let (env2, _) = compile_prelude env1 in
   E.in_scope_set env2
@@ -4824,11 +4865,13 @@ and actor_lit outer_env this ds fs at =
   let wasm_binary =
     let env = E.mk_global
       (E.mode outer_env)
+      (E.get_rts outer_env)
       (E.get_prelude outer_env)
       (E.get_trap_with outer_env)
       ClosureTable.table_end in
 
     if E.mode env = DfinityMode then Dfinity.system_imports env;
+    RTS.system_imports env;
 
     let start_fun = Func.of_body env [] [] (fun env3 -> G.with_region at @@
       (* Compile the prelude *)
@@ -4878,6 +4921,15 @@ and actor_fake_object_idx env name =
 
 and conclude_module env module_name start_fi_o =
 
+  (* Wrap the start function with the RTS initialization *)
+  let rts_start_fi = E.add_fun env "rts_start" (Func.of_body env [] [] (fun env1 ->
+    Heap.get_heap_base ^^
+    Heap.set_heap_ptr ^^
+    match start_fi_o with
+    | Some fi -> G.i (Call fi)
+    | None -> G.nop
+  )) in
+
   Dfinity.default_exports env;
   GC.register env (E.get_end_of_static_memory env);
 
@@ -4907,15 +4959,31 @@ and conclude_module env module_name start_fi_o =
       nr { gtype = GlobalType (I32Type, Mutable);
         value = nr (G.to_instr_list compile_unboxed_zero)
       };
-      (* end-of-heap pointer *)
+      (* stack pointer *)
       nr { gtype = GlobalType (I32Type, Mutable);
+        value = nr (G.to_instr_list (compile_unboxed_const Stack.end_of_stack))
+      };
+      (* beginning-of-heap pointer, may be changed by linker *)
+      nr { gtype = GlobalType (I32Type, Immutable);
         value = nr (G.to_instr_list (compile_unboxed_const (E.get_end_of_static_memory env)))
+      };
+      (* end-of-heap pointer, initialized to __heap_base upon start *)
+      nr { gtype = GlobalType (I32Type, Mutable);
+        value = nr (G.to_instr_list (compile_unboxed_const 0xDEAFBEEFl))
       };
       (* reference counter *)
       nr { gtype = GlobalType (I32Type, Mutable);
         value = nr (G.to_instr_list compile_unboxed_zero)
       };
       ] in
+  E.add_export env (nr {
+    name = Wasm.Utf8.decode "__stack_pointer";
+    edesc = nr (GlobalExport (nr Stack.stack_global))
+  });
+  E.add_export env (nr {
+    name = Wasm.Utf8.decode "__heap_base";
+    edesc = nr (GlobalExport (nr Heap.base_global))
+  });
 
   let data = List.map (fun (offset, init) -> nr {
     index = nr 0l;
@@ -4931,7 +4999,7 @@ and conclude_module env module_name start_fi_o =
         index = nr 0l;
         offset = nr (G.to_instr_list (compile_unboxed_const ni'));
         init = List.mapi (fun i _ -> nr (Wasm.I32.of_int_u (ni + i))) funcs } ];
-      start = start_fi_o;
+      start = Some (nr rts_start_fi);
       globals = globals;
       memories = memories;
       imports = func_imports @ other_imports;
@@ -4957,12 +5025,15 @@ and conclude_module env module_name start_fi_o =
              ];
     } in
 
-  emodule
+  match E.get_rts env with
+  | None -> emodule
+  | Some rts -> LinkModule.link emodule "rts" rts
 
-let compile mode module_name (prelude : Ir.prog) (progs : Ir.prog list) : CustomModule.extended_module =
-  let env = E.mk_global mode prelude Dfinity.trap_with ClosureTable.table_end in
+let compile mode module_name rts (prelude : Ir.prog) (progs : Ir.prog list) : CustomModule.extended_module =
+  let env = E.mk_global mode rts prelude Dfinity.trap_with ClosureTable.table_end in
 
   if E.mode env = DfinityMode then Dfinity.system_imports env;
+  RTS.system_imports env;
 
   let start_fun = compile_start_func env (prelude :: progs) in
   let start_fi = E.add_fun env "start" start_fun in

src/compile.mli
@@ -1,3 +1,3 @@
 type mode = WasmMode | DfinityMode
 
-val compile : mode -> string -> Ir.prog -> Ir.prog list -> CustomModule.extended_module
+val compile : mode -> string -> CustomModule.extended_module option -> Ir.prog -> Ir.prog list -> CustomModule.extended_module

src/flags.ml
@@ -9,3 +9,4 @@ let dump_lowering = ref false
 let interpret_ir = ref false
 let source_map = ref false
 let prelude = ref true
+let link = ref true

src/main.ml
@@ -45,6 +45,8 @@ let argspec = Arg.align
   "-no-await", Arg.Clear Flags.await_lowering, " no await-lowering (with -iR)";
   "-no-async", Arg.Clear Flags.async_lowering, " no async-lowering (with -iR)";
 
+  "-no-link", Arg.Clear Flags.link, " do not statically link-in runtime";
+
   "-dp", Arg.Set Flags.dump_parse, " dump parse";
   "-dt", Arg.Set Flags.dump_tc, " dump type-checked AST";
   "-dl", Arg.Set Flags.dump_lowering, " dump intermediate representation ";
@@ -88,7 +90,8 @@ let process_files files : unit =
       | [n] -> out_file := Filename.remove_extension (Filename.basename n) ^ ".wasm"
       | ns -> eprintf "asc: no output file specified"; exit 1
     end;
-    let module_ = exit_on_failure Pipeline.(compile_files !compile_mode files) in
+    let module_ = exit_on_failure
+      Pipeline.(compile_files !compile_mode !(Flags.link) files) in
     let oc = open_out !out_file in
     let (source_map, wasm) = CustomModuleEncode.encode module_ in
     output_string oc wasm; close_out oc;

src/pipeline.ml
@@ -426,6 +426,27 @@ let show_translation =
 
 (* Compilation *)
 
+let load_as_rts () =
+
+  let load_file f =
+    let ic = open_in f in
+    let n = in_channel_length ic in
+    let s = Bytes.create n in
+    really_input ic s 0 n;
+    close_in ic;
+    Bytes.to_string s in
+
+  (*
+  The RTS can be found via environment (in particular when built via nix),
+  or relative to the directory of the invoked asc (when developing)
+  *)
+  let wasm_filename =
+    match Sys.getenv_opt "ASC_RTS" with
+    | Some filename -> filename
+    | None -> Filename.(concat (dirname Sys.argv.(0)) "../rts/as-rts.wasm") in
+  let wasm = load_file wasm_filename in
+  CustomModuleDecode.decode "rts.wasm" wasm
+
 type compile_mode = Compile.mode = WasmMode | DfinityMode
 type compile_result = (CustomModule.extended_module, Diag.messages) result
 
@@ -443,27 +464,28 @@ let lower_prog senv lib_env libraries progs name =
   let prog_ir = show_translation true initial_stat_env prog_ir name in
   prog_ir
 
-let compile_prog mode lib_env libraries progs : compile_result =
+let compile_prog mode do_link lib_env libraries progs : compile_result =
   let prelude_ir = Desugar.transform prelude in
   let name = name_progs progs in
   let prog_ir = lower_prog initial_stat_env lib_env libraries progs name in
   phase "Compiling" name;
-  let module_ = Compile.compile mode name prelude_ir [prog_ir] in
+  let rts = if do_link then Some (load_as_rts ()) else None in
+  let module_ = Compile.compile mode name rts prelude_ir [prog_ir] in
   Ok module_
 
-let compile_files mode files : compile_result =
+let compile_files mode do_link files : compile_result =
   match load_progs (parse_files files) initial_stat_env with
   | Error msgs -> Error msgs
   | Ok ((libraries, progs, senv), msgs) ->
     Diag.print_messages msgs;
-    compile_prog mode senv.Typing.lib_env libraries progs
+    compile_prog mode do_link senv.Typing.lib_env libraries progs
 
 let compile_string mode s name : compile_result =
   match load_decl (parse_string s name) initial_stat_env with
   | Error msgs -> Error msgs
   | Ok ((libraries, prog, senv, _t, _sscope), msgs) ->
     Diag.print_messages msgs;
-    compile_prog mode senv.Typing.lib_env libraries [prog]
+    compile_prog mode false senv.Typing.lib_env libraries [prog]
 
 
 (* Interpretation (IR) *)

src/pipeline.mli
@@ -8,4 +8,4 @@ val run_files_and_stdin : string list -> unit Diag.result
 type compile_mode = WasmMode | DfinityMode
 type compile_result = (CustomModule.extended_module, Diag.messages) result
 val compile_string : compile_mode -> string -> string -> compile_result
-val compile_files : compile_mode -> string list -> compile_result
+val compile_files : compile_mode -> bool -> string list -> compile_result

src/prelude.ml
@@ -36,6 +36,7 @@ func charToText(c : Char) : Text = (prim "Char->Text" : Char -> Text) c;
 func printInt(x : Int) { print (@text_of_Int x) };
 func printChar(x : Char) { print (charToText x) };
 func print(x : Text) { (prim "print" : Text -> ()) x };
+func rts_version() : Text { (prim "rts_version" : () -> Text) () };
 
 // Hashing
 func hashInt(n : Int) : Word32 = (prim "Int~hash" : Int -> Word32) n;
@@ -339,6 +340,7 @@ let prim = function
                                           | code -> Wasm.Utf8.encode [code]
                                in k (Text str)
   | "print" -> fun v k -> Printf.printf "%s%!" (as_text v); k unit
+  | "rts_version" -> fun v k -> as_unit v; k (Text "0.1")
   | "decodeUTF8" -> fun v k ->
                     let s = as_text v in
                     let open Int32 in

commit a4d146e97d9d15e3c1c9dd23ec57b96cdae7543c

Fix nix-shell
the reorganisation in 508054834ed82ee55031ffeb23a0278d12b633b1 broke
the nix-shell setup. This fixes it, I hope.

CI doesn’t test `nix-shell`, so cookie points for the reviewer who
actually (gasp) checks out the branch and tests it.

default.nix
@@ -200,6 +200,7 @@ rec {
       ] ++
       (if test-dvm then [ real-dvm ] else []) ++
       llvmBuildInputs;
+
     buildPhase = ''
         patchShebangs .
         ${llvmEnv}
@@ -424,11 +425,13 @@ rec {
     # https://github.com/NixOS/nix/issues/955
     #
 
-    buildInputs =
-      native.buildInputs ++
-      builtins.filter (i: i != native) native_test.buildInputs ++
+    buildInputs = nixpkgs.lib.lists.unique (builtins.filter (i: i != native) (
+      asc-bin.buildInputs ++
+      rts.buildInputs ++
+      native_test.buildInputs ++
       users-guide.buildInputs ++
-      [ nixpkgs.ncurses ];
+      [ nixpkgs.ncurses ]
+    ));
 
     shellHook = llvmEnv;
     TOMMATHSRC = libtommath;