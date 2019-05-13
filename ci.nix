9cbb1fb9e7692376f4154fcd3c2a87a00758a90e
Joachim Breitner <mail@joachim-breitner.de>
2019-05-13 22:38:25 +0200

    Nix: Inject the Git Revision in the finall all-systems-go derivation (#396)
    This way even rebases or pushes that don’t affect CI will get built and
    reported by hydra.

--- ci.nix
+++ ci.nix
@@ -1,16 +1,19 @@
+{ src ? { rev = null; } }:
 let
   nixpkgs = (import ./nix/nixpkgs.nix).nixpkgs { };
   nixpkgs-linux = (import ./nix/nixpkgs.nix).nixpkgs { system = "x86_64-linux"; };
   nixpkgs-darwin = (import ./nix/nixpkgs.nix).nixpkgs { system = "x86_64-darwin"; };
+
+  inject-rev = drv: drv.overrideAttrs (attrs: { rev = src.rev; });
 in
 rec {
   linux = import ./default.nix { nixpkgs = nixpkgs-linux; };
   darwin = import ./default.nix { nixpkgs = nixpkgs-darwin; };
-  all-systems-go = nixpkgs.releaseTools.aggregate {
+  all-systems-go = inject-rev (nixpkgs.releaseTools.aggregate {
     name = "all-systems-go";
     constituents = [
       linux.all-systems-go
       darwin.all-systems-go
     ];
-  };
+  });
 }
