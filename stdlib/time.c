e269c6b1d8441df7bfc05ee8620b0788af181db2
Matthew Hammer <matthew@dfinity.org>
2019-05-16 14:07:38 -0400

    clean up huge Makefile; improve timing util to report msec, not usec

--- stdlib/time.c
+++ stdlib/time.c
@@ -1,24 +1,37 @@
-#include <stdlib.h> 
-#include <stdio.h> 
-#include <string.h> 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/time.h>
 
 int main(int argc, char** args) {
   struct timeval tv_0;
   struct timeval tv_1;
+
+  if (argc < 2) {
+    fprintf(stderr, "usage: %s <command-string> <output-line-prefix>\n", args[0]);
+    return -1;
+  }
+
   gettimeofday(&tv_0,0);
+  //
+  // first argument is the command to run and time:
+  //
   system(args[1]);
   gettimeofday(&tv_1,0);
-  printf("%s%d\n",
+  printf("%s%.3f\n",
          //
          // second argument is the line prefix for the output...
          //
          args[2],
          //
-         // ...and line ends with time, in micro seconds.
+         // ...and the output line ends with the measured time, in milliseconds.
          //
-         (int) ((tv_1.tv_sec - tv_0.tv_sec) * 1000000)
-         +
-         (int) (tv_1.tv_usec - tv_0.tv_usec)
+         ((float)
+          (
+           (int) ((tv_1.tv_sec - tv_0.tv_sec) * 1000000)
+           +
+           (int) (tv_1.tv_usec - tv_0.tv_usec) )
+          )
+         / 1000.0f
          );
 }
