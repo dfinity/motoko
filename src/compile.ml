a8078d06f2d1ce41ebaebd79c52736733e828dd8
Joachim Breitner <mail@joachim-breitner.de>
2019-05-28 09:15:12 +0200

    Backend: Consider more blocks as top-level (#445)
    * Consider more blocks as top-level
    
    this was previously just a typo in `compile_top_lvl_expr`, I think.
    
    If the top-level contains further blocks, then they can also be compiled
    as top-level (yay, less dynamic allocation, more calls to known
    functions).
    
    This uncovered a bug around `AllocHow.decs_top_lvl`, which needs to know
    which variables are captured in the body to make the right decision. So
    feed that through. Not particulary pretty, but at least correct.
    
    * A similar bug existed in AllocHow.decs_local

--- src/compile.ml
+++ src/compile.ml
@@ -4212,6 +4212,10 @@ module AllocHow = struct
     then is_static env how0 (Freevars.exp exp)
     else false
 
+  let is_local_mut _ = function
+    | LocalMut -> true
+    | _ -> false
+
   let dec_local env (seen, how0) dec =
     let (f,d) = Freevars.dec dec in
     let captured = Freevars.captured_vars f in
@@ -4239,9 +4243,7 @@ module AllocHow = struct
 
     (* Do we capture anything else?
        For local blocks, mutable things must be heap allocated.
-       On the top-level, all captured non-static things must be heap allocated.
     *)
-    let is_local_mut _ h = h = LocalMut in
     let how3 =
       map_of_set StoreHeap
         (S.inter (set_of_map (M.filter is_local_mut how0)) captured) in
@@ -4250,13 +4252,16 @@ module AllocHow = struct
     let seen' = S.union seen d
     in (seen', how)
 
-  let decs_local env decs : allocHow =
+  let decs_local env decs captured_in_body : allocHow =
     let rec go how =
       let _seen, how1 = List.fold_left (dec_local env) (S.empty, how) decs in
-      if M.equal (=) how how1 then how else go how1 in
+      let how2 = map_of_set StoreHeap
+        (S.inter (set_of_map (M.filter is_local_mut how1)) captured_in_body) in
+      let how' = join how1 how2 in
+      if M.equal (=) how how' then how else go how' in
     go M.empty
 
-  let decs_top_lvl env decs : allocHow =
+  let decs_top_lvl env decs captured_in_body : allocHow =
     let how0 = M.empty in
     (* All non-function are at least locals *)
     let how1 =
@@ -4267,17 +4272,18 @@ module AllocHow = struct
           | _ -> join how (map_of_set LocalMut d) in
       List.fold_left go how0 decs in
     (* All captured non-functions are heap allocated *)
-    let how2 =
+    let how2 = join how1 (map_of_set StoreStatic (S.inter (set_of_map how1) captured_in_body)) in
+    let how3 =
       let go how dec =
         let (f,d) = Freevars.dec dec in
         let captured = Freevars.captured_vars f in
         join how (map_of_set StoreStatic (S.inter (set_of_map how1) captured)) in
-      List.fold_left go how1 decs in
-    how2
+      List.fold_left go how2 decs in
+    how3
 
-  let decs env lvl decs : allocHow = match lvl with
-    | TopLvl -> decs_top_lvl env decs
-    | NotTopLvl -> decs_local env decs
+  let decs env lvl decs captured_in_body : allocHow = match lvl with
+    | TopLvl -> decs_top_lvl env decs captured_in_body
+    | NotTopLvl -> decs_local env decs captured_in_body
 
   (* Functions to extend the environment (and possibly allocate memory)
      based on how we want to store them. *)
@@ -4755,7 +4761,8 @@ and compile_exp (env : E.t) ae exp =
       (code1 ^^ StackRep.adjust env sr1 sr)
       (code2 ^^ StackRep.adjust env sr2 sr)
   | BlockE (decs, exp) ->
-    let (ae', code1) = compile_decs env ae AllocHow.NotTopLvl decs in
+    let captured = Freevars.captured_vars (Freevars.exp exp) in
+    let (ae', code1) = compile_decs env ae AllocHow.NotTopLvl decs captured in
     let (sr, code2) = compile_exp env ae' exp in
     (sr, code1 ^^ code2)
   | LabelE (name, _ty, e) ->
@@ -4882,7 +4889,8 @@ and compile_exp_as env ae sr_out e =
         compile_exp_as env ae SR.UnboxedReference e
       ) es
     | _ , BlockE (decs, exp) ->
-      let (ae', code1) = compile_decs env ae AllocHow.NotTopLvl decs in
+      let captured = Freevars.captured_vars (Freevars.exp exp) in
+      let (ae', code1) = compile_decs env ae AllocHow.NotTopLvl decs captured in
       let code2 = compile_exp_as env ae' sr_out exp in
       code1 ^^ code2
     (* Fallback to whatever stackrep compile_exp chooses *)
@@ -5105,8 +5113,8 @@ and compile_dec env pre_ae how dec : ASEnv.t * G.t * (ASEnv.t -> G.t) =
         Var.set_val env ae name.it
       )
 
-and compile_decs env ae lvl decs : ASEnv.t * G.t =
-  let how = AllocHow.decs ae lvl decs in
+and compile_decs env ae lvl decs captured_in_body : ASEnv.t * G.t =
+  let how = AllocHow.decs ae lvl decs captured_in_body in
   let rec go pre_ae decs = match decs with
     | []          -> (pre_ae, G.nop, fun _ -> G.nop)
     | [dec]       -> compile_dec env pre_ae how dec
@@ -5125,7 +5133,8 @@ and compile_decs env ae lvl decs : ASEnv.t * G.t =
 
 and compile_top_lvl_expr env ae e = match e.it with
   | BlockE (decs, exp) ->
-    let (ae', code1) = compile_decs env ae AllocHow.NotTopLvl decs in
+    let captured = Freevars.captured_vars (Freevars.exp e) in
+    let (ae', code1) = compile_decs env ae AllocHow.TopLvl decs captured in
     let code2 = compile_top_lvl_expr env ae' exp in
     code1 ^^ code2
   | _ ->
@@ -5133,13 +5142,19 @@ and compile_top_lvl_expr env ae e = match e.it with
     code ^^ StackRep.drop env sr
 
 and compile_prog env ae (ds, e) =
-    let (ae', code1) = compile_decs env ae AllocHow.TopLvl ds in
+    let captured = Freevars.captured_vars (Freevars.exp e) in
+    let (ae', code1) = compile_decs env ae AllocHow.TopLvl ds captured in
     let code2 = compile_top_lvl_expr env ae' e in
     (ae', code1 ^^ code2)
 
 and compile_static_exp env pre_ae how exp = match exp.it with
   | FuncE (name, cc, typ_binds, args, _rt, e) ->
-      let mk_body env ae = compile_exp_as env ae (StackRep.of_arity cc.Value.n_res) e in
+      let mk_body env ae =
+        assert begin (* Is this really closed? *)
+          List.for_all (fun v -> ASEnv.NameEnv.mem v ae.ASEnv.vars)
+            (Freevars.M.keys (Freevars.exp e))
+        end;
+        compile_exp_as env ae (StackRep.of_arity cc.Value.n_res) e in
       FuncDec.closed env cc name args mk_body exp.at
   | _ -> assert false
 
@@ -5239,7 +5254,7 @@ and actor_lit outer_env this ds fs at =
       let ae2 = ASEnv.add_local_deferred ae1 this.it SR.Vanilla Dfinity.get_self_reference false in
 
       (* Compile the declarations *)
-      let (ae3, decls_code) = compile_decs env ae2 AllocHow.TopLvl ds in
+      let (ae3, decls_code) = compile_decs env ae2 AllocHow.TopLvl ds Freevars.S.empty in
 
       (* Export the public functions *)
       List.iter (export_actor_field env ae3) fs;
@@ -5264,7 +5279,7 @@ and main_actor env ae1 this ds fs =
   let ae2 = ASEnv.add_local_deferred ae1 this.it SR.Vanilla Dfinity.get_self_reference false in
 
   (* Compile the declarations *)
-  let (ae3, decls_code) = compile_decs env ae2 AllocHow.TopLvl ds in
+  let (ae3, decls_code) = compile_decs env ae2 AllocHow.TopLvl ds Freevars.S.empty in
 
   (* Export the public functions *)
   List.iter (export_actor_field env ae3) fs;
