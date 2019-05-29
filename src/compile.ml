83cf3d6854885f2ebece926b899cae1c15c3fd9d
Gabor Greif <gabor@dfinity.org>
2019-05-29 17:52:03 +0200

    AST-63 arithmetic shift right operator (#450)
    * implements signed (arithmetic) shift right operator `+>>`
    
    * `+>>` operates on word types by inserting the
    sign (topmost) bit as many times as demanded.
    
    * made `>>` a _logical shift right_ in the interpreter (it used to be signed shift)
    
    * adds tests

--- src/compile.ml
+++ src/compile.ml
@@ -1339,12 +1339,6 @@ module UnboxedSmallWord = struct
     G.i (Unary (Wasm.Values.I32 I32Op.Ctz)) ^^
     msb_adjust ty
 
-  (* Kernel for arithmetic (signed) shift, according to the word invariant. *)
-  let shrs_kernel ty =
-    lsb_adjust ty ^^
-    G.i (Binary (Wasm.Values.I32 I32Op.ShrS)) ^^
-    sanitize_word_result ty
-
   (* Kernel for testing a bit position, according to the word invariant. *)
   let btst_kernel env ty =
     let (set_b, get_b) = new_local env "b"
@@ -1448,7 +1442,7 @@ sig
   (* literals *)
   val compile_lit : E.t -> Big_int.big_int -> G.t
 
-  (* arithmetics *)
+  (* arithmetic *)
   val compile_abs : E.t -> G.t
   val compile_neg : E.t -> G.t
   val compile_add : E.t -> G.t
@@ -4436,10 +4430,15 @@ let rec compile_binop env t op =
   | Type.(Prim (Word8|Word16|Word32 as ty)),  ShLOp -> UnboxedSmallWord.(
      lsb_adjust ty ^^ clamp_shift_amount ty ^^
      G.i (Binary (Wasm.Values.I32 I32Op.Shl)))
-  | Type.(Prim Word64),                       ShROp -> G.i (Binary (Wasm.Values.I64 I64Op.ShrU))
-  | Type.(Prim (Word8|Word16|Word32 as ty)),  ShROp -> UnboxedSmallWord.(
+  | Type.(Prim Word64),                       UShROp -> G.i (Binary (Wasm.Values.I64 I64Op.ShrU))
+  | Type.(Prim (Word8|Word16|Word32 as ty)),  UShROp -> UnboxedSmallWord.(
      lsb_adjust ty ^^ clamp_shift_amount ty ^^
      G.i (Binary (Wasm.Values.I32 I32Op.ShrU)) ^^
+     sanitize_word_result ty)
+  | Type.(Prim Word64),                       SShROp -> G.i (Binary (Wasm.Values.I64 I64Op.ShrS))
+  | Type.(Prim (Word8|Word16|Word32 as ty)),  SShROp -> UnboxedSmallWord.(
+     lsb_adjust ty ^^ clamp_shift_amount ty ^^
+     G.i (Binary (Wasm.Values.I32 I32Op.ShrS)) ^^
      sanitize_word_result ty)
   | Type.(Prim Word64),                       RotLOp -> G.i (Binary (Wasm.Values.I64 I64Op.Rotl))
   | Type.Prim Type.                  Word32,  RotLOp -> G.i (Binary (Wasm.Values.I32 I32Op.Rotl))
@@ -4701,10 +4700,6 @@ and compile_exp (env : E.t) ae exp =
            in match p with
              | "Array.init" -> compile_kernel_as SR.Vanilla (Arr.init env)
              | "Array.tabulate" -> compile_kernel_as SR.Vanilla (Arr.tabulate env)
-             | "shrs8" -> compile_kernel_as SR.Vanilla (UnboxedSmallWord.shrs_kernel Type.Word8)
-             | "shrs16" -> compile_kernel_as SR.Vanilla (UnboxedSmallWord.shrs_kernel Type.Word16)
-             | "shrs" -> compile_kernel_as SR.UnboxedWord32 (G.i (Binary (Wasm.Values.I32 I32Op.ShrS)))
-             | "shrs64" -> compile_kernel_as SR.UnboxedWord64 (G.i (Binary (Wasm.Values.I64 I64Op.ShrS)))
              | "btst8" -> compile_kernel_as SR.Vanilla (UnboxedSmallWord.btst_kernel env Type.Word8)
              | "btst16" -> compile_kernel_as SR.Vanilla (UnboxedSmallWord.btst_kernel env Type.Word16)
              | "btst" -> compile_kernel_as SR.UnboxedWord32 (UnboxedSmallWord.btst_kernel env Type.Word32)
--- src/prelude.ml
+++ src/prelude.ml
@@ -80,25 +80,21 @@ func word32ToChar(w : Word32) : Char = (prim "Word32->Char" : Word32 -> Char) w;
 func decodeUTF8(s : Text) : (Word32, Char) = (prim "decodeUTF8" : Text -> (Word32, Char)) s;
 
 // Exotic bitwise operations
-func shrsWord8(w : Word8, amount : Word8) : Word8 = (prim "shrs8" : (Word8, Word8) -> Word8) (w, amount);
 func popcntWord8(w : Word8) : Word8 = (prim "popcnt8" : Word8 -> Word8) w;
 func clzWord8(w : Word8) : Word8 = (prim "clz8" : Word8 -> Word8) w;
 func ctzWord8(w : Word8) : Word8 = (prim "ctz8" : Word8 -> Word8) w;
 func btstWord8(w : Word8, amount : Word8) : Bool = (prim "btst8" : (Word8, Word8) -> Word8) (w, amount) != (0 : Word8);
 
-func shrsWord16(w : Word16, amount : Word16) : Word16 = (prim "shrs16" : (Word16, Word16) -> Word16) (w, amount);
 func popcntWord16(w : Word16) : Word16 = (prim "popcnt16" : Word16 -> Word16) w;
 func clzWord16(w : Word16) : Word16 = (prim "clz16" : Word16 -> Word16) w;
 func ctzWord16(w : Word16) : Word16 = (prim "ctz16" : Word16 -> Word16) w;
 func btstWord16(w : Word16, amount : Word16) : Bool = (prim "btst16" : (Word16, Word16) -> Word16) (w, amount) != (0 : Word16);
 
-func shrsWord32(w : Word32, amount : Word32) : Word32 = (prim "shrs" : (Word32, Word32) -> Word32) (w, amount);
 func popcntWord32(w : Word32) : Word32 = (prim "popcnt" : Word32 -> Word32) w;
 func clzWord32(w : Word32) : Word32 = (prim "clz" : Word32 -> Word32) w;
 func ctzWord32(w : Word32) : Word32 = (prim "ctz" : Word32 -> Word32) w;
 func btstWord32(w : Word32, amount : Word32) : Bool = (prim "btst" : (Word32, Word32) -> Word32) (w, amount) != (0 : Word32);
 
-func shrsWord64(w : Word64, amount : Word64) : Word64 = (prim "shrs64" : (Word64, Word64) -> Word64) (w, amount);
 func popcntWord64(w : Word64) : Word64 = (prim "popcnt64" : Word64 -> Word64) w;
 func clzWord64(w : Word64) : Word64 = (prim "clz64" : Word64 -> Word64) w;
 func ctzWord64(w : Word64) : Word64 = (prim "ctz64" : Word64 -> Word64) w;
@@ -302,16 +298,6 @@ let prim = function
   | "Word32->Char" -> fun v k ->
                       let i = Conv.of_signed_Word32 (as_word32 v)
                       in k (Char i)
-
-  | "shrs8" | "shrs16" | "shrs" | "shrs64" ->
-     fun v k ->
-     let w, a = as_pair v
-     in k (match w with
-           | Word8  y -> Word8  (Word8 .shr_s y  (as_word8  a))
-           | Word16 y -> Word16 (Word16.shr_s y  (as_word16 a))
-           | Word32 y -> Word32 (Word32.shr_s y  (as_word32 a))
-           | Word64 y -> Word64 (Word64.shr_s y  (as_word64 a))
-           | _ -> failwith "shrs")
 
   | "popcnt8" | "popcnt16" | "popcnt" | "popcnt64" ->
      fun v k ->
