1e54367d617db4f33ddc471684ce4b134cc7617b
Matthew Hammer <matthew@dfinity.org>
2019-06-19 07:06:48 -0600

    minor paren nit

--- src/flags/flags.ml
+++ src/flags/flags.ml
@@ -15,4 +15,4 @@ let profile = ref false
 let profile_verbose = ref false
 let profile_file = ref "profiling-counters.csv"
 let profile_line_prefix = ref ""
-let profile_field_names : (string list) ref = ref []
+let profile_field_names : string list ref = ref []
