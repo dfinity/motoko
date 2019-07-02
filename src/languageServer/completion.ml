a114ccb5bbecce9d6216d332719ad36f3bb36b68
Christoph <christoph.hegemann1337@gmail.com>
2019-07-02 15:37:58 +0200

    displays types alongside completions

--- src/languageServer/completion.ml
+++ src/languageServer/completion.ml
@@ -2,13 +2,29 @@ open As_types
 open As_frontend
 module Lsp_t = Lsp.Lsp_t
 
+type ide_decl =
+  | ValueDecl of (string * Type.typ)
+  | TypeDecl of (string * Type.typ)
+
 module Index = Map.Make(String)
-type completion_index = (string list) Index.t
+type completion_index = (ide_decl list) Index.t
 
 let string_of_list f xs =
   List.map f xs
   |> String.concat "; "
   |> fun x -> "[ " ^ x ^ " ]"
+
+let item_of_ide_decl = function
+  | ValueDecl(lbl, ty) ->
+     (lbl, Some(Type.string_of_typ ty))
+  | TypeDecl(lbl, ty) ->
+     (lbl, Some(Type.string_of_typ ty))
+
+let string_of_ide_decl = function
+  | ValueDecl(lbl, ty) ->
+     "ValueDecl(" ^ String.escaped lbl ^ ", " ^ Type.string_of_typ ty ^ ")"
+  | TypeDecl(lbl, ty) ->
+     "TypeDecl(" ^ String.escaped lbl ^ ", " ^ Type.string_of_typ ty ^ ")"
 
 let lib_files () : string list =
   let lib_dir = "lib" in
@@ -17,11 +33,11 @@ let lib_files () : string list =
   |> List.filter (fun file -> String.equal (Filename.extension file) ".as")
   |> List.map (fun file -> Filename.concat lib_dir file)
 
-let read_single_module_lib (ty: Type.typ): string list option =
+let read_single_module_lib (ty: Type.typ): ide_decl list option =
   match ty with
   | Type.Obj (Type.Module, fields) ->
      fields
-     |> List.map (fun Type.{ lab = lab; typ = typ } -> lab)
+     |> List.map (fun Type.{ lab = lab; typ = typ } -> ValueDecl(lab, typ))
      |> Lib.Option.pure
   | _ -> None
 
@@ -46,7 +62,7 @@ let string_of_index index =
        (fun (path, decls) ->
          path
          ^ " =>\n    "
-         ^ string_of_list Base.Fn.id decls
+         ^ string_of_list string_of_ide_decl decls
          ^ "\n")
 
 (* Given the source of a module, figure out under what names what
@@ -100,7 +116,7 @@ let completions (* index *) logger file line column =
   let index = make_index () in
   let imported = find_imported_modules file in
   match find_completion_prefix logger file line column with
-  | None -> [ "ERROR: Couldn't determine completion prefix "]
+  | None -> [ ("ERROR: Couldn't determine completion prefix", None)]
   | Some prefix ->
      let module_path =
        imported
@@ -113,16 +129,18 @@ let completions (* index *) logger file line column =
      | Some mp ->
         (match Index.find_opt (snd mp) index with
          | Some decls ->
-            decls
+            List.map item_of_ide_decl decls
          | None ->
-            [ "ERROR: Couldn't find module in index: " ^ index_keys ])
+            [ (("ERROR: Couldn't find module in index: " ^ index_keys), None) ])
      | None ->
-        [ "ERROR: Couldn't find module for prefix: " ^ prefix ]
+        [ (("ERROR: Couldn't find module for prefix: " ^ prefix), None) ]
 
 let completion_handler logger file position =
   let line = position.Lsp_t.position_line in
   let column = position.Lsp_t.position_character in
-  let completion_item lbl = Lsp_t.{ completion_item_label = lbl } in
+  let completion_item (lbl, detail) =
+    Lsp_t.{ completion_item_label = lbl
+          ; completion_item_detail = detail } in
   `CompletionResponse
     (List.map completion_item (completions logger file line column))
 
