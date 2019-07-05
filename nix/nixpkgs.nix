9c32b4e7e22c61ab118eee1d99501b770c7a7e8b
Joachim Breitner <mail@joachim-breitner.de>
2019-07-05 14:15:29 +0200

    Try to bump nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,8 +1,8 @@
 rec {
-  rev = "e0c7712eac67c6b820d9d1020f46bac96fd8cede";
+  rev = "bfef52d95f4d8031286753d5d677e5179328c6ce";
 
   src = builtins.fetchTarball {
-    sha256 = "08rcnqxkninl5a560ss39s4nbqf0a677q6qh1fh7i0lr9pxf6aan";
+    sha256 = "0b2633a5dc8h3zjydwzyrw1jbn1la41l5qz71sk96z3a3df39m9c";
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
   };
 
