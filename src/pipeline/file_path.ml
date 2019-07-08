c4abf9525a50db2a627fa5c0b869960a092607e2
Christoph <christoph.hegemann1337@gmail.com>
2019-07-08 09:38:00 +0200

    Applies review suggestions from Andreas
    - File names can't contain slashes, so a simple String.split is good
      enough to figure out the components

--- src/pipeline/file_path.ml
+++ src/pipeline/file_path.ml
@@ -1,20 +1,9 @@
-let path_components path =
-  let rec go = function
-    | "/" -> []
-    | "." -> ["."]
-    | path ->
-        let base = Filename.basename path in
-        let dir = Filename.dirname path in
-        base :: go dir
-  in List.rev (go path)
-
 let normalise file_path =
   let has_trailing_slash =
     Base.Option.is_some (Base.String.chop_suffix ~suffix:"/" file_path) in
-  let has_leading_slash =
-    Base.Option.is_some (Base.String.chop_prefix ~prefix:"/" file_path) in
+  let has_leading_slash = not (Filename.is_relative file_path) in
   let acc = Stack.create () in
-  path_components file_path
+  Lib.String.split file_path '/'
   |> List.iter
        (function
         | "" -> ()
@@ -22,7 +11,7 @@ let normalise file_path =
         | ".." ->
            if Stack.is_empty acc
            then Stack.push ".." acc
-           else Base.Fn.ignore (Stack.pop acc)
+           else ignore (Stack.pop acc)
         | segment -> Stack.push segment acc);
   let result = Stack.fold (fun x y -> y ^ "/" ^ x) "" acc in
   let prepend_leading_slash s = if has_leading_slash then "/" ^ s else s in
