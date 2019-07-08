198a455a7aaae6edbf7966aeeecd9e8c70657607
Christoph <christoph.hegemann1337@gmail.com>
2019-07-08 21:42:00 +0200

    address review nit

--- src/pipeline/file_path.ml
+++ src/pipeline/file_path.ml
@@ -14,11 +14,10 @@ let normalise file_path =
            else ignore (Stack.pop acc)
         | segment -> Stack.push segment acc);
   let result = Stack.fold (fun x y -> y ^ "/" ^ x) "" acc in
-  let prepend_leading_slash s = if has_leading_slash then "/" ^ s else s in
-  prepend_leading_slash
-    (if has_trailing_slash
-     then result
-     else Base.String.chop_suffix_exn ~suffix:"/" result)
+  let prefix = if has_leading_slash then "/" else "" in
+  prefix ^ (if has_trailing_slash
+    then result
+    else Base.String.chop_suffix_exn ~suffix:"/" result)
 
 let relative_to base path =
   Base.String.chop_prefix
