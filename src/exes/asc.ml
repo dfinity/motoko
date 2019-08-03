99ed25a268d77f250def79de1872c1f80710b7d7
Joachim Breitner <mail@joachim-breitner.de>
2019-08-03 14:40:25 +0200

    Forgot to add flag to main

--- src/exes/asc.ml
+++ src/exes/asc.ml
@@ -68,6 +68,7 @@ let argspec = Arg.align
   "-no-dfinity-api",
     Arg.Unit (fun () -> compile_mode := Pipeline.WasmMode),
       " do not import the DFINITY system API";
+  "-no-multi-value", Arg.Clear Codegen.Flags.multi_value, " avoid multi-value extension";
 
   "-dp", Arg.Set Pipeline.Flags.dump_parse, " dump parse";
   "-dt", Arg.Set Pipeline.Flags.dump_tc, " dump type-checked AST";
