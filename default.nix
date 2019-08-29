3454ae59c0e4dd1d3315bcb05126539679da7d08
Joachim Breitner <mail@joachim-breitner.de>
2019-08-29 14:56:52 +0200

    Switch to my branch that supports debug.print

--- default.nix
+++ default.nix
@@ -31,8 +31,8 @@ let dev = import (builtins.fetchGit {
 
 let dfinity-repo = import (builtins.fetchGit {
   url = "ssh://git@github.com/dfinity-lab/dfinity";
-  ref = "master";
-  rev = "3b612083b047ad613fd48cab069bd45c05989cc6";
+  ref = "joachim/systemapi/debug.log";
+  rev = "32176d4c7d2890558b9100f97b11f6304a53a5a0";
 }) { system = nixpkgs.system; }; in
 
 # Include drun
