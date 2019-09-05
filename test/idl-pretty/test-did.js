7f2448230fa9e52f58f87b3e740732f8de4d39c0
Gabor Greif <gabor@dfinity.org>
2019-09-05 13:26:51 +0200

    maual rework

--- test/idl-pretty/test-did.js
+++ test/idl-pretty/test-did.js
@@ -3,3 +3,4 @@ const g =
  IDL.Obj({'0': IDL.Bool, '1': IDL.Int, '2': IDL.Int, '3': IDL.Int32})
 const actor_t = new IDL.ActorInterface({
  'testfun': IDL.Message(IDL.Obj({'0': g}), IDL.Obj({}))})
+module.exports = { g, actor_t }
