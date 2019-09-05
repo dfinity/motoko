80eb6f8b0eeb46808f0a8339566803e017c219b6
Gabor Greif <gabor@dfinity.org>
2019-09-05 13:34:32 +0200

    check that obj-in-obj works
    ... it does!
    
    while int32 has encoding problems

--- test/idl-pretty/index.js
+++ test/idl-pretty/index.js
@@ -1,7 +1,7 @@
 var x = require("./test-did.js")
 var fs = require("fs")
 
-var gInstance = {"0": true, "1": 422, "2": -422, "3": -422}
+var gInstance = {"0": true, "1": 422, "2": -422, "3": {"0": true, "1": 12345}}
 console.log(x.g)
 var resultBuffer = x.g.encode(gInstance)
 
