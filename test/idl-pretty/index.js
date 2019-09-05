aeb6de4da84510552693aaab75ff868ba0a54e31
Gabor Greif <gabor@dfinity.org>
2019-09-05 13:17:49 +0200

    WIP: int32

--- test/idl-pretty/index.js
+++ test/idl-pretty/index.js
@@ -1,8 +1,8 @@
 var x = require("./test-did.js")
 var fs = require("fs")
 
-var gInstance = {"0": true, "1": 422}
-
+var gInstance = {"0": true, "1": 422, "2": -422, "3": -422}
+console.log(x.g)
 var resultBuffer = x.g.encode(gInstance)
 
 fs.writeFileSync("bytes", resultBuffer)
--- test/idl-pretty/test-did.js
+++ test/idl-pretty/test-did.js
@@ -1,6 +1,5 @@
 const IDL = require('IDL')
-const g = IDL.Obj({'0': IDL.Bool, '1': IDL.Int})
+const g =
+ IDL.Obj({'0': IDL.Bool, '1': IDL.Int, '2': IDL.Int, '3': IDL.Int32})
 const actor_t = new IDL.ActorInterface({
-  'testfun': IDL.Message(IDL.Obj({'0': g}), IDL.Obj({}))})
-
-module.exports = { g, actor_t }
+ 'testfun': IDL.Message(IDL.Obj({'0': g}), IDL.Obj({}))})
