9328732fa407ef901765ec751afbbb232eb85e59
Claudio Russo <claudio@dfinity.org>
2019-09-12 12:09:19 +0100

    correct translation of shard call_conv (#650)

--- src/ir_passes/async.ml
+++ src/ir_passes/async.ml
@@ -369,7 +369,6 @@ module Transform() = struct
             | T.Tup [] ->
               FuncE (x, cc, t_typ_binds typbinds, t_args args, List.map t_typ typT, t_exp exp)
             | T.Async res_typ ->
-              let cc' = Call_conv.message_cc (cc.Call_conv.n_args + 1) in
               let res_typ = t_typ res_typ in
               let reply_typ = replyT nary res_typ in
               let r = fresh_var "r" reply_typ in
@@ -389,6 +388,7 @@ module Transform() = struct
                   t_exp cps -*- k
                 | _ -> assert false
               in
+              let cc' = Call_conv.message_cc (List.length args') in
               FuncE (x, cc', typbinds', args', [], exp')
             | _ -> assert false
           end
