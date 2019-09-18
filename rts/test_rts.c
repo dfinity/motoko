27417a5b96cf85974da170d0341e901512575dcb
Gabor Greif <gabor@dfinity.org>
2019-09-18 19:33:17 +0200

    invalid sequences
    transcribed from Markus Kuhn

--- rts/test_rts.c
+++ rts/test_rts.c
@@ -40,11 +40,62 @@ int main () {
 
 
   extern bool utf8_valid(const char*, size_t);
-  const int cases = 2;
+  const int cases = 33;
   const char* utf8_inputs[cases] = {
     "abcd",
+
+    // from https://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-test.txt
+    //
+    // 4.1  Examples of an overlong ASCII character
+    "\xc0\xaf",
+    "\xe0\x80\xaf",
+    "\xf0\x80\x80\xaf",
+    "\xf8\x80\x80\x80\xaf",
+    "\xfc\x80\x80\x80\x80\xaf",
+
+    // 4.2  Maximum overlong sequences
+    "\xc1\xbf",
+    "\xe0\x9f\xbf",
+    "\xf0\x8f\xbf\xbf",
+    "\xf8\x87\xbf\xbf\xbf",
+    "\xfc\x83\xbf\xbf\xbf\xbf",
+
+    // 4.3  Overlong representation of the NUL character
+    "\xc0\x80",
+    "\xe0\x80\x80",
+    "\xf0\x80\x80\x80",
+    "\xf8\x80\x80\x80\x80",
+    "\xfc\x80\x80\x80\x80\x80",
+
+    // 5.1 Single UTF-16 surrogates
+    "\xed\xa0\x80",
+    "\xed\xad\xbf",
+    "\xed\xae\x80",
+    "\xed\xaf\xbf",
+    "\xed\xb0\x80",
+    "\xed\xbe\x80",
+    "\xed\xbf\xbf",
+
+    // 5.2 Paired UTF-16 surrogates
+    "\xed\xa0\x80\xed\xb0\x80",
+    "\xed\xa0\x80\xed\xbf\xbf",
+    "\xed\xad\xbf\xed\xb0\x80",
+    "\xed\xad\xbf\xed\xbf\xbf",
+    "\xed\xae\x80\xed\xb0\x80",
+    "\xed\xae\x80\xed\xbf\xbf",
+    "\xed\xaf\xbf\xed\xb0\x80",
+    "\xed\xaf\xbf\xed\xbf\xbf",
+
+    // 3.5  Impossible bytes
+    "\xfe",
     "\xff"
   };
   for (int i = 0; i < cases; ++i)
-    printf("valid UTF-8 test #%d: %s\n", i + 1, utf8_valid(utf8_inputs[i], strlen(utf8_inputs[i])) ? "ok" : "not ok");
+  {
+    bool invalid = i > 0;
+    printf("%svalid UTF-8 test #%d: %s\n",
+	   invalid ? "in" : "",
+	   i + 1,
+	   invalid != utf8_valid(utf8_inputs[i], strlen(utf8_inputs[i])) ? "ok" : "not ok");
+  }
 }
