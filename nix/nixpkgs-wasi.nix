eb10a7f652344bd851cddd3c01368acbc592de98
Joachim Breitner <mail@joachim-breitner.de>
2019-09-21 11:19:59 +0200

    Bump wasmtime some more
    to get version 20190521

--- nix/nixpkgs-wasi.nix
+++ nix/nixpkgs-wasi.nix
@@ -1,6 +1,6 @@
-with { rev = "2d5769884d573aef748443ee1e686444ab58ba78"; };
+with { rev = "f0fec244ca380b9d3e617ee7b419c59758c8b0f1"; };
 
 import (builtins.fetchTarball {
-  sha256 = "0p49zlz6gdjrm0z92xszkl1lhrnisgm13s2mxfkbdq8bmp9q7i3l";
+  sha256 = "0ga51457fb30b8j9v8is7wwf9ld9p51nizm8yhj09l23qpyh8np9";
   url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
 })
