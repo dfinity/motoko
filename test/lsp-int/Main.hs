861e9f10b44c59100ac3fb3eeef018a84ef7eaf9
Christoph <christoph.hegemann1337@gmail.com>
2019-09-23 14:49:10 +0200

    adds a test for completions
    Also makes all the imports explicit and gives an informative error if
    too few arguments are provided to the test binary

--- test/lsp-int/Main.hs
+++ test/lsp-int/Main.hs
@@ -3,15 +3,29 @@
 
 module Main where
 
-import Language.Haskell.LSP.Test
-import Language.Haskell.LSP.Types
-
+import Control.Lens ((^.))
+import Control.Monad (unless)
 import Control.Monad.IO.Class (liftIO)
 import Data.Default
 import Data.Text (Text)
+import Language.Haskell.LSP.Test
+import Language.Haskell.LSP.Types (TextDocumentIdentifier(..), Position(..), HoverContents(..), MarkupContent(..), MarkupKind(..), TextEdit(..), Range(..))
+import Language.Haskell.LSP.Types.Lens (contents, label, detail)
 import System.Directory (setCurrentDirectory)
 import System.Environment (getArgs)
-import Test.Hspec (shouldBe)
+import Test.Hspec (shouldBe, shouldMatchList)
+
+completionTestCase
+  :: TextDocumentIdentifier
+  -> Position
+  -> [(Text, Maybe Text)]
+  -> Session ()
+completionTestCase doc pos expected = do
+  actual <- getCompletions doc pos
+  liftIO
+    (shouldMatchList
+      (map (\c -> (c^.label, c^.detail)) actual)
+      expected)
 
 hoverTestCase
   :: TextDocumentIdentifier
@@ -20,7 +34,7 @@ hoverTestCase
   -> Session ()
 hoverTestCase doc pos expected = do
   actual <- getHover doc pos
-  liftIO (shouldBe (fmap _contents actual) expected)
+  liftIO (shouldBe (fmap (^.contents) actual) expected)
 
 plainMarkup :: Text -> Maybe HoverContents
 plainMarkup t =
@@ -32,7 +46,13 @@ plainMarkup t =
 
 main :: IO ()
 main = do
-  [as_ide, project] <- getArgs
+  args <- getArgs
+  unless (length args == 2)
+    (putStrLn
+      "This test expects two command line arguments,\
+      \the path to the as-ide binary and the path to\
+      \the test project it's supposed to run in")
+  let [as_ide, project] = args
   setCurrentDirectory project
   runSession as_ide fullCaps "." $ do
     initRes <- initializeResponse
@@ -49,7 +69,18 @@ main = do
       doc
       (Position 50 50)
       Nothing
+    --     14 | List.push<Int>(x, s);
+    -- ==> 14 | List.pus
+    let edit = TextEdit (Range (Position 13 11) (Position 13 27)) "pus"
+    _ <- applyEdit doc edit
+    completionTestCase
+      doc
+    -- 14 | List.pus|
+      (Position 13 14)
+      [("push",Just "<T>(T, List<T>) -> List<T>")]
     pure ()
+
+
 
 -- To make tests involving diagnostics work, we need the compiler
 -- pipeline to read its files from the VFS
