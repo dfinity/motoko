e106dcf341741e7579aebbe46cccb1321aeef7aa
Joachim Breitner <mail@joachim-breitner.de>
2019-09-27 14:30:15 +0200

    Resume prior AFL run

--- test/fuzz-idl/runafl.sh
+++ test/fuzz-idl/runafl.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+if test -e afl_out
+then
+# continue
+AFL_SKIP_CPUFREQ=1 afl-fuzz -i - -o afl_out/ -- ./main @@
+else
+# start from scratch
 AFL_SKIP_CPUFREQ=1 afl-fuzz -i afl_in/ -o afl_out/ -- ./main @@
+fi
+
