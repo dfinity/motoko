19f0c28610f102d1c67d49aaa4c740ce4d19b85c
Joachim Breitner <mail@joachim-breitner.de>
2019-09-30 11:36:35 +0200

    Track dfinity master again

--- default.nix
+++ default.nix
@@ -32,8 +32,8 @@ let dev = import (builtins.fetchGit {
 
 let dfinity-repo = import (builtins.fetchGit {
   url = "ssh://git@github.com/dfinity-lab/dfinity";
-  ref = "mw/memory-grow";
-  rev = "bee6bc75433a8b20db70321abc6c2d31ede814c6";
+  ref = "master";
+  rev = "a50367859416ef7c12ca103b7fc03f5a7654f6ad";
 }) { system = nixpkgs.system; }; in
 
 let real-dvm =
