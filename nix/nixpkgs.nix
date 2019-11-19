fd2523865a2c34e6152d36b72c1f96e8dc10b8b1
Joachim Breitner <mail@joachim-breitner.de>
2019-11-19 16:02:49 +0100

    Bump nixpkgs
    following https://github.com/dfinity-lab/common/pull/68

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,8 +1,8 @@
 rec {
-  rev = "e0c7712eac67c6b820d9d1020f46bac96fd8cede";
+  rev = "96c9578020133fe64feab90c00f3cb880d53ad0d";
 
   src = builtins.fetchTarball {
-    sha256 = "08rcnqxkninl5a560ss39s4nbqf0a677q6qh1fh7i0lr9pxf6aan";
+    sha256 = "03rn7gn8r129a8cj527nhs7k28ibzwqw083iirwvas2x4k9mir9z";
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
   };
 
