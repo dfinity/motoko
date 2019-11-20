dece039f2277e139a9116f8af848f86ed71c8ea9
Joachim Breitner <mail@joachim-breitner.de>
2019-11-20 14:48:02 +0100

    IC Stub/Backend: Create canister
    In order to support patterns useful for and used in the Motoko test suite, the
    IC stub gains some extra features that are not spec’ed or implemented in the
    official client. These are:
    
     * The ability to create calls from `canister_init`.
    
     * The ability to create additional canisters, in a way where the creating
       canisters learns the id of the created canister synchronously, and can send
       messages to it right away.
    
       The interface for that is
    
          ic.create_canister : (mod_src : i32, mod_size : i32, arg_src : i32, arg_size : i32) -> (idx : i32)
          ic.created_canister_id_size : (idx : i32) -> (size : i32)
          ic.created_canister_id_copy : (idx : i32, dst : i32, offset : i32, size : i32) -> ()
    
       where the `idx` is only valid within the same function invokation.

--- ic-stub/src/IC/Canister/Interface.hs
+++ ic-stub/src/IC/Canister/Interface.hs
@@ -4,16 +4,21 @@
 This module defines the various entry points provided by "IC.Canister.Imp" as a data type describing the arguments and results.
 
 The point of this abstraction is to to allow adding new entry points, or changing the arguments or result values, without changing the modules "IC.Canister.Pure" or "IC.Canister.Persisted", that sit between "IC.Canister.Imp" and "IC.Canister".
+
+Certain features here are beyond the specified abilities, in order to support the Motoko test suite. In particular:
+
+ * Creating canisters
+ * Issuing calls from canister_init
+
 -}
 module IC.Canister.Interface where
 
 import IC.Types
 import IC.Wasm.Winter (Module)
 
-type UpdateResult = ([MethodCall], Maybe Response)
 
 data CanisterMethod r where
-    Initialize :: Module -> EntityId -> Blob -> CanisterMethod ()
+    Initialize :: ExistingCanisters -> Module -> EntityId -> Blob -> CanisterMethod InitResult
     Query :: MethodName -> EntityId -> Blob -> CanisterMethod Response
-    Update :: MethodName -> EntityId -> Blob -> CanisterMethod UpdateResult
-    Callback :: Callback -> EntityId -> Response -> CanisterMethod UpdateResult
+    Update :: MethodName -> ExistingCanisters -> EntityId -> Blob -> CanisterMethod UpdateResult
+    Callback :: Callback -> ExistingCanisters -> EntityId -> Response -> CanisterMethod UpdateResult
