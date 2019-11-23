5f0a9e3b8d87cb1ee28ba0caf33c90bed43e94af
Joachim Breitner <mail@joachim-breitner.de>
2019-11-23 17:30:25 +0100

    Pass `system` from `ci.nix` and `shell.nix`

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? (import ./nix/nixpkgs.nix).nixpkgs {},
+{ system ? null,
 }:
-(import ./default.nix { inherit nixpkgs; export-shell = true; }).shell
+(import ./default.nix { inherit system; export-shell = true; }).shell
 
