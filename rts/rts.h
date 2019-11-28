261e26d52049a3846ffccae35724a69b0c6ce37e
Joachim Breitner <mail@joachim-breitner.de>
2019-11-28 11:46:49 +0100

    Garbage collect closures (#944)
    This defines a suitable data stucture in the C code (a packed array with
    integrated free list). It is compatible with the RTS's idea of an array,
    so it can be copied and traversed by the GC.
    
    There is a test case that checks that that closed-over values (a large
    array in this case) are indeed freed. We can add such test cases for
    other GC-related things now.
    
    There is also a unit test of the C data structure in test_rts.c.

--- rts/rts.h
+++ rts/rts.h
@@ -13,12 +13,21 @@
 /*
 Motoko pointers are offset by one. So let us represent
 them as a typedef, and access the fields using the payload macro.
+
+Note that for the purpose of ./test_rts, this needs to be 64 bit safe. So do
+_not_ encode that a word is 4 bytes!
 */
 typedef intptr_t as_ptr;
-#define FIELD(p,n) (((size_t *)(p+1))[n])
+#define SKEW(p) ((as_ptr)p-1)
+#define UNSKEW(p) ((size_t *)((as_ptr)p+1))
+#define FIELD(p,n) (UNSKEW(p)[n])
 #define TAG(p) FIELD(p,0)
-#define TEXT_LEN(p) ((char *)(&FIELD(p,1)))
-#define TEXT_PAYLOAD(p) ((char *)(&FIELD(p,2)))
+#define TEXT_LEN(p) (FIELD(p,1))
+#define TEXT_PAYLOAD(p) ((uint8_t *)(&FIELD(p,2)))
+
+#define ARRAY_LEN(p) (FIELD(p,1))
+#define ARRAY_HEADER_SIZE 2
+#define ARRAY_FIELD(p,n) (FIELD(p,ARRAY_HEADER_SIZE+n))
 
 /* Heap tags. Needs to stay in sync with compile.ml */
 enum as_heap_tag {
@@ -57,16 +66,18 @@ So in order to allocate on the Motoko heap from C, we import
 alloc_bytes from the Motoko RTS:
 */
 from_rts as_ptr alloc_bytes(size_t n);
+from_rts as_ptr alloc_words(size_t n);
 char *alloc(size_t n);
 
 
 /* IDL code */
 /*@ assigns \nothing; ensures \false ; */
-from_rts __attribute__ ((noreturn)) void idl_trap(const char* str, size_t n);
+from_rts __attribute__ ((noreturn)) void rts_trap(const char* str, size_t n);
 from_rts __attribute__ ((noreturn)) void bigint_trap();
 
 /** Functions used in multiple modules of the RTS */
 export void as_memcpy(char *str1, const char *str2, size_t n);
 export __attribute__ ((noreturn)) void idl_trap_with(const char *str1);
+export __attribute__ ((noreturn)) void rts_trap_with(const char *str1);
 
 #endif /* RTS_H */
