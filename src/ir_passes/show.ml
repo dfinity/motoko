0153d7126a091193c351b0ca784c321617a80943
Claudio Russo <claudio@dfinity.org>
2019-12-10 10:23:54 +0000

    Update src/ir_passes/show.ml

--- src/ir_passes/show.ml
+++ src/ir_passes/show.ml
@@ -124,7 +124,7 @@ and t_exp' env = function
     RetE (t_exp env exp1)
   | ThrowE exp1 ->
     ThrowE (t_exp env exp1)
-  | AsyncE (tb, e, typ) -> AsyncE (tb, t_exp env e, typ) (* TBR *)
+  | AsyncE (tb, e, typ) -> AsyncE (tb, t_exp env e, typ)
   | AwaitE e -> AwaitE (t_exp env e)
   | AssertE exp1 ->
     AssertE (t_exp env exp1)
