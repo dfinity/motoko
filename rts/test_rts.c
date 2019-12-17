8a21d5aa753a2ee4e44a4d568174e70aa9212dd1
Gabor Greif <gabor@dfinity.org>
2019-12-17 13:23:43 +0100

    ooops

--- rts/test_rts.c
+++ rts/test_rts.c
@@ -172,7 +172,7 @@ int main () {
   blob0p[3] = blob0p[4] = blob0p[5] = blob0p[6] = '0';
   (void)crc8_decode(blob0);
 
-  as_ptr blob1 = alloc_blob(7);
+  as_ptr blob1 = alloc_blob(15);
   char* blob1p = (char*)BLOB_PAYLOAD(blob1);
   memcpy(blob1p, "ic:C0FEFED00D41", 15);
   (void)crc8_decode(blob1);
