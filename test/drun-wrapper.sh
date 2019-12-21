7886a2ecc1e233e9abf1e870b387d8c3c7ec4f2b
Joachim Breitner <mail@joachim-breitner.de>
2019-12-21 22:34:10 +0100

    Try to use perl on darwin

--- test/drun-wrapper.sh
+++ test/drun-wrapper.sh
@@ -18,5 +18,5 @@ export LANG=C.UTF-8
 # (drun currently expects a decimal number for the canister id)
 
 (echo "install 1125899906842624 $1 0x";
- if [ -n "$2" ]; then perl -ne 'print "$1 1125899906842624 $2\n" if m,^//CALL (ingress|query) (.*),' $2; fi;
+ if [ -n "$2" ]; then LANG=C perl -ne 'print "$1 1125899906842624 $2\n" if m,^//CALL (ingress|query) (.*),' $2; fi;
 ) | $DRUN -c "$CONFIG" /dev/stdin
