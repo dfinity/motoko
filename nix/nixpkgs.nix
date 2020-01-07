65157570f64388e123d7c2d36fd2f057f6d5e94a
Gabor Greif <gabor@dfinity.org>
2020-01-07 22:07:08 +0100

    bump nixpkgs to get stock wasmtime

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,8 +1,8 @@
 rec {
-  rev = "96c9578020133fe64feab90c00f3cb880d53ad0d";
+  rev = "82875a20ba444110396f95537b18247898e40e22";
 
   src = builtins.fetchTarball {
-    sha256 = "03rn7gn8r129a8cj527nhs7k28ibzwqw083iirwvas2x4k9mir9z";
+    sha256 = "1xy2zn3hkcv66ddvscr3l32jcx1qg9h14zvhmy5zf0pcfb8gn42i";
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
   };
 
