991be7f10b8be24fbc7467d8ab2a696bc5324a31
Christoph <christoph.hegemann1337@gmail.com>
2020-01-09 13:09:19 +0100

    shows prim types on hover

--- src/languageServer/hover.ml
+++ src/languageServer/hover.ml
@@ -14,6 +14,17 @@ let markup_content (msg : string) : Lsp.markup_content =
         markup_content_value = msg }
 
 let hover_handler index position file_contents project_root file_path =
+  let current_uri_opt = Lib.FilePath.relative_to project_root file_path in
+  let toplevel_decls =
+     let prim_decls =
+       Lib.Option.get (lookup_module "prim" index) [] in
+     let current_module_decls =
+       current_uri_opt
+       |> Lib.Fun.flip Option.bind (fun uri -> lookup_module uri index)
+       |> Lib.Fun.flip Lib.Option.get [] in
+     current_module_decls @ prim_decls in
+  let mk_hover_result ide_decl =
+    Lsp.{ hover_result_contents = markup_content (hover_detail ide_decl) } in
   let hover_result =
     Option.bind
       (Source_file.identifier_at_pos
@@ -30,13 +41,11 @@ let hover_handler index position file_contents project_root file_path =
                 List.find_opt
                   (fun d -> name_of_ide_decl d = resolved.Source_file.ident)
                   decls)
-           |> Option.map (fun ide_decl ->
-                  Lsp.{ hover_result_contents =
-                          markup_content (hover_detail ide_decl) })
-        | Source_file.Ident _ ->
-           (* At some point we'll want to look this Ident up in the
-              local context*)
-           None
+           |> Option.map mk_hover_result
+        | Source_file.Ident ident ->
+           toplevel_decls
+           |> List.find_opt (fun d -> name_of_ide_decl d = ident)
+           |> Option.map mk_hover_result
         | Source_file.Unresolved _ ->
            None)
     in
