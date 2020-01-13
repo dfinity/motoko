dbe8dde02a585bf7374da3969a221feb57bcca89
Christoph <christoph.hegemann1337@gmail.com>
2020-01-13 13:43:17 +0100

    Fixes tests for new way of accessing Prim

--- test/lsp-int-test-project/app.mo
+++ test/lsp-int-test-project/app.mo
@@ -1,9 +1,10 @@
 import Stack "ListClient.mo";
+import Prim "mo:prim";
 
 actor {
     func main(): () {
         let st = Stack.empty();
         let newst = Stack.push(0, st);
-        let primTest: Word8 = natToWord8(5);
+        let primTest: Word8 = Prim.natToWord8(5);
     }
 }
--- test/lsp-int/Main.hs
+++ test/lsp-int/Main.hs
@@ -2,6 +2,7 @@
 {-# language DuplicateRecordFields #-}
 {-# language ExplicitForAll #-}
 {-# language ScopedTypeVariables #-}
+{-# language BlockArguments #-}
 
 module Main where
 
@@ -53,6 +54,14 @@ waitForActualDiagnostics = do
   diags <- waitForDiagnostics
   if null diags then waitForActualDiagnostics else pure diags
 
+-- | Brackets an action with the closing and opening of a TextDocument
+withDoc :: String -> (TextDocumentIdentifier -> Session a) -> Session a
+withDoc path action = do
+  doc <- openDoc path "motoko"
+  res <- action doc
+  closeDoc doc
+  pure res
+
 plainMarkup :: Text -> Maybe HoverContents
 plainMarkup t =
   Just
@@ -98,102 +107,104 @@ main = do
         log "Initializing"
         initRes <- initializeResponse
         log "Hover tests"
-        doc <- openDoc "ListClient.mo" "motoko"
-        hoverTestCase
-          doc
-          (Position 13 11)
-          (plainMarkup "push : <T>(T, List<T>) -> List<T>")
-        hoverTestCase
-          doc
-          (Position 16 11)
-          (plainMarkup "pop : <T>List<T> -> (?T, List<T>)")
-        hoverTestCase
-          doc
-          (Position 50 50)
-          Nothing
-        appDoc <- openDoc "app.mo" "motoko"
-        hoverTestCase
-          appDoc
-          (Position 6 33)
-          (plainMarkup "natToWord8 : Nat -> Word8")
-        closeDoc appDoc
+        withDoc "ListClient.mo" \doc -> do
+          hoverTestCase
+            doc
+            (Position 14 11)
+            (plainMarkup "push : <T>(T, List<T>) -> List<T>")
+          hoverTestCase
+            doc
+            (Position 17 11)
+            (plainMarkup "pop : <T>List<T> -> (?T, List<T>)")
+          hoverTestCase
+            doc
+            (Position 50 50)
+            Nothing
+        withDoc "app.mo" \doc -> do
+          hoverTestCase
+            doc
+            (Position 7 39)
+            (plainMarkup "natToWord8 : Nat -> Word8")
         log "Completion tests"
-        -- Completing primitives:
-        actual <- getCompletions doc (Position 7 0)
-        liftIO
-          (shouldBe
-           ([("word32ToNat", Just "Word32 -> Nat")])
-           (mapMaybe (\c -> guard (c^.label == "word32ToNat")
-                       *> pure (c^.label, c^.detail)) actual))
         -- Completing top level definitions:
-        actual <- getCompletions doc (Position 7 0)
-        liftIO
-          (shouldBe
-           ([("empty", Just "() -> Stack")])
-           (mapMaybe (\c -> guard (c^.label == "empty")
-                       *> pure (c^.label, c^.detail)) actual))
-        --     14 | List.push<Int>(x, s);
-        -- ==> 14 | List.pus
-        let edit = TextEdit (Range (Position 13 11) (Position 13 27)) "pus"
-        _ <- applyEdit doc edit
-        completionTestCase
-          doc
-        -- 14 | List.pus|
-          (Position 13 14)
-          [("push",Just "<T>(T, List<T>) -> List<T>")]
-        closeDoc doc
-        doc <- openDoc "ListClient.mo" "motoko"
-        --     1 | import List
-        -- ==> 1 | ort List
-        let edit = TextEdit (Range (Position 0 1) (Position 0 3)) ""
-        _ <- applyEdit doc edit
-        sendNotification TextDocumentDidSave (DidSaveTextDocumentParams doc)
-        (diagnostic:_) <- waitForDiagnostics
-        liftIO (diagnostic^.message `shouldBe` "unexpected token")
-        closeDoc doc
+        withDoc "ListClient.mo" \doc -> do
+          actual <- getCompletions doc (Position 7 0)
+          liftIO
+            (shouldBe
+             ([("empty", Just "() -> Stack")])
+             (mapMaybe (\c -> guard (c^.label == "empty")
+                         *> pure (c^.label, c^.detail)) actual))
+          --     15 | List.push<Int>(x, s);
+          -- ==> 15 | List.pus
+          let edit = TextEdit (Range (Position 14 11) (Position 14 27)) "pus"
+          _ <- applyEdit doc edit
+          completionTestCase
+            doc
+          -- 15 | List.pus|
+            (Position 14 14)
+            [("push",Just "<T>(T, List<T>) -> List<T>")]
+
+        -- Completing primitives:
+        withDoc "ListClient.mo" \doc -> do
+          let edit = TextEdit (Range (Position 15 0) (Position 15 0)) "Prim."
+          _ <- applyEdit doc edit
+          actual <- getCompletions doc (Position 15 6)
+          liftIO
+            (shouldBe
+             (mapMaybe (\c -> guard (c^.label == "word32ToNat")
+                         *> pure (c^.label, c^.detail)) actual)
+             ([("word32ToNat", Just "Word32 -> Nat")]))
+
+        withDoc "ListClient.mo" \doc -> do
+          --     1 | import List
+          -- ==> 1 | ort List
+          let edit = TextEdit (Range (Position 0 1) (Position 0 3)) ""
+          _ <- applyEdit doc edit
+          sendNotification TextDocumentDidSave (DidSaveTextDocumentParams doc)
+          (diagnostic:_) <- waitForDiagnostics
+          liftIO (diagnostic^.message `shouldBe` "unexpected token")
 
         log "Lexer failures don't crash the server"
-        doc <- openDoc "ListClient.mo" "motoko"
-        -- Creates an unclosed text literal, which triggers a lexer error
-        let edit = TextEdit (Range (Position 0 1) (Position 0 3)) "\"hello"
-        _ <- applyEdit doc edit
-        -- We're just testing that the server doesn't crash here
-        getCompletions doc (Position 0 0)
-        getHover doc (Position 0 0)
-        closeDoc doc
+        withDoc "ListClient.mo" \doc -> do
+          -- Creates an unclosed text literal, which triggers a lexer error
+          let edit = TextEdit (Range (Position 0 1) (Position 0 3)) "\"hello"
+          _ <- applyEdit doc edit
+          -- We're just testing that the server doesn't crash here
+          getCompletions doc (Position 0 0)
+          getHover doc (Position 0 0)
 
         log "Finds errors in non-saved files"
-        -- It finds errors in transitive modules that have been changed in
-        -- the vfs but not yet stored to disc
-        doc <- openDoc "ListClient.mo" "motoko"
-        let edit = TextEdit (Range (Position 0 1) (Position 0 3)) ""
-        _ <- applyEdit doc edit
-        appDoc <- openDoc "app.mo" "motoko"
-        sendNotification TextDocumentDidSave (DidSaveTextDocumentParams appDoc)
-        diagnostic:_ <- waitForActualDiagnostics
-        liftIO (diagnostic^.message `shouldBe` "unexpected token")
-        closeDoc doc
-        closeDoc appDoc
+        withDoc "ListClient.mo" \doc -> do
+          -- It finds errors in transitive modules that have been changed in
+          -- the vfs but not yet stored to disc
+
+          let edit = TextEdit (Range (Position 0 1) (Position 0 3)) ""
+          _ <- applyEdit doc edit
+          withDoc "app.mo" \appDoc -> do
+            sendNotification TextDocumentDidSave (DidSaveTextDocumentParams appDoc)
+            diagnostic:_ <- waitForActualDiagnostics
+            liftIO (diagnostic^.message `shouldBe` "unexpected token")
 
         log "Rebuilding with package paths"
-        -- It knows how to handle package paths for rebuilding, and also
-        -- for completions
-        doc <- openDoc "app.mo" "motoko"
-        let edit = TextEdit (Range (Position 1 0) (Position 1 0)) "\nimport MyDep \"mo:mydep/broken.mo\""
-        _ <- applyEdit doc edit
-        sendNotification TextDocumentDidSave (DidSaveTextDocumentParams doc)
-        [diag] <- waitForActualDiagnostics
-        liftIO (diag^.message `shouldBe` "operator not defined for operand types\n  Text\nand\n  Nat")
+        withDoc "app.mo" \doc -> do
+          -- It knows how to handle package paths for rebuilding, and also
+          -- for completions
+          let edit = TextEdit (Range (Position 3 0) (Position 3 0)) "\nimport MyDep \"mo:mydep/broken.mo\""
+          _ <- applyEdit doc edit
+          sendNotification TextDocumentDidSave (DidSaveTextDocumentParams doc)
+          [diag] <- waitForActualDiagnostics
+          liftIO (diag^.message `shouldBe` "operator not defined for operand types\n  Text\nand\n  Nat")
 
         log "Completions from package paths"
-        -- Imports the non-broken dependency module
-        let edit2 = TextEdit (Range (Position 2 0) (Position 3 0)) "\nimport MyDep \"mo:mydep/lib.mo\""
-        _ <- applyEdit doc edit2
-        sendNotification TextDocumentDidSave (DidSaveTextDocumentParams doc)
-        let edit3 = TextEdit (Range (Position 3 0) (Position 3 0)) "\nMyDep."
-        _ <- applyEdit doc edit3
-        completionTestCase
-          doc
-          -- MyDep.|
-          (Position 4 6)
-          [("print_hello", Just "() -> Text")]
+        withDoc "app.mo" \doc -> do
+          -- Imports the non-broken dependency module
+          let edit2 = TextEdit (Range (Position 3 0) (Position 3 0)) "\nimport MyDep \"mo:mydep/lib.mo\""
+          _ <- applyEdit doc edit2
+          sendNotification TextDocumentDidSave (DidSaveTextDocumentParams doc)
+          let edit3 = TextEdit (Range (Position 4 0) (Position 4 0)) "\nMyDep."
+          _ <- applyEdit doc edit3
+          completionTestCase
+            doc
+            -- MyDep.|
+            (Position 5 6)
+            [("print_hello", Just "() -> Text")]
