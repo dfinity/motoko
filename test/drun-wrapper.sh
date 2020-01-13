6f3eb9d4e0baa112fabf11eee2bbb330168f51e8
Joachim Breitner <mail@joachim-breitner.de>
2020-01-13 16:49:16 +0100

    More extra-rounds (#1105)
    we have to tell `drun` to wait for a certain amount of time (rounds)
    after each message to allow for subsequent computation in the call tree.
    This bumps this number to something that might cover all our test cases (even
    `flatten-awaitables` and `large-array.)
    
    This hack should go away with DFN-1269.
    
    Also bumps drun mostly to get https://github.com/dfinity-lab/dfinity/pull/2270
    
    Also included
    ```
    947195fb1 Drun: wait extra batches, then print result (#2270)
    a544b6dec DFN-1267: Create Rust trait and types for DKG API based on crypto spec (#2225)
    698fb5cfb ExecEnv: use vanilla slog everywhere (#2253)
    0dfe895c6 Merge pull request #2258 from dfinity-lab/johnw/debug-syms
    52dc9e6db Merge pull request #2252 from dfinity-lab/rsub/xport-metrics
    afc6db283 Merge pull request #2062 from dfinity-lab/faraz
    87d5594e4 Merge pull request #2243 from dfinity-lab/david-con-209
    7ead6c55f Merge pull request #2229 from dfinity-lab/fs/mastersquash
    e55afef23 Merge pull request #2137 from dfinity-lab/INF-532
    91917d470 Merge pull request #2169 from dfinity-lab/akhi3030/call-simple-update
    ```

--- test/drun-wrapper.sh
+++ test/drun-wrapper.sh
@@ -26,6 +26,11 @@ export LANG=C.UTF-8
 # 1125899906842624
 # (drun currently expects a decimal number for the canister id)
 
+# this number is determined empirically: how many extra batches are needed
+# until all call-trees have finished (even those that return early).
+# Usually darwin needs more! (Should go away with DFN-1269)
+EXTRA_BATCHES=100
+
 (echo "install 1125899906842624 $1 0x";
  if [ -n "$2" ]; then LANG=C perl -ne 'print "$1 1125899906842624 $2\n" if m,^//CALL (ingress|query) (.*),' $2; fi;
-) | $DRUN -c "$CONFIG" --extra-batches 1 /dev/stdin
+) | $DRUN -c "$CONFIG" --extra-batches $EXTRA_BATCHES /dev/stdin
