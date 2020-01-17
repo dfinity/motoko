f8903d683e9376fb62b8d550d72e892188a28ad1
Nicolas Mattia <nicolas@dfinity.org>
2020-01-17 15:59:30 +0100

    Import packages in nix/default.nix

--- default.nix
+++ default.nix
@@ -2,32 +2,8 @@
   replay ? 0,
   system ? builtins.currentSystem,
 }:
-let
-  sourcesnix = builtins.fetchurl https://raw.githubusercontent.com/nmattia/niv/506b896788d9705899592a303de95d8819504c55/nix/sources.nix;
-  nixpkgs_src = (import sourcesnix { sourcesFile = ./nix/sources.json; pkgs = abort "This (rigid, skolem) type variable is bound by aadvarks"; }).nixpkgs;
-in
 
-let nixpkgs = import nixpkgs_src {
-  inherit system;
-  overlays = [
-    (self: super: { sources = import sourcesnix { sourcesFile = ./nix/sources.json; pkgs = super; }; })
-    # Selecting the ocaml version
-    (self: super: { ocamlPackages = self.ocaml-ng.ocamlPackages_4_08; })
-    # Additional ocaml package
-    (self: super: {
-      ocamlPackages = super.ocamlPackages // {
-        wasm = import ./nix/ocaml-wasm.nix {
-          inherit (self) stdenv ocaml sources;
-          inherit (self.ocamlPackages) findlib ocamlbuild;
-        };
-        vlq = import ./nix/ocaml-vlq.nix {
-          inherit (self) stdenv ocaml dune sources;
-          inherit (self.ocamlPackages) findlib;
-        };
-      };
-    })
-  ];
-}; in
+let nixpkgs = import ./nix { inherit system; }; in
 
 let llvm = import ./nix/llvm.nix { inherit (nixpkgs) system sources; }; in
 
