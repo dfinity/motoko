426e64a007f3802f5623aa0a82691d8b80be3f8b
Gabor Greif <gabor@dfinity.org>
2020-01-21 18:40:49 +0100

    workarounds for stuff missing from Turtle/Filesystem
    these should be upstreamed
    
    See:
    https://github.com/Gabriel439/Haskell-Turtle-Library/issues/368
    https://mail.haskell.org/pipermail/haskell-cafe/2012-October/104030.html

--- test/random/Main.hs
+++ test/random/Main.hs
@@ -32,6 +32,15 @@ import Data.IORef
 import Turtle
 -- import Debug.Trace (traceShowId, traceShow)
 
+
+-- for missing stuff
+import qualified System.IO
+import qualified GHC.IO.Handle.FD
+import qualified Control.Exception
+import qualified Data.Text.IO (hPutStrLn)
+import qualified Control.Monad.Managed
+
+
 main = defaultMain tests
   where tests :: TestTree
         tests = testGroup "Motoko tests" [arithProps, conversionProps, utf8Props, matchingProps]
@@ -65,6 +74,52 @@ matchingProps = testGroup "Pattern matching"
   [ QC.testProperty "intra-actor" $ prop_matchStructured
   , QC.testProperty "inter-actor" $ prop_matchInActor
   ]
+
+
+-- Stuff that is missing from Turtle
+
+-- | Stream lines of `Text` to a file, blocking
+
+pipe :: MonadIO io => Turtle.FilePath -> Shell Line -> io ()
+pipe file s = sh (do
+    handle <- using (writeonlyblocking file)
+    line   <- s
+    liftIO (Data.Text.IO.hPutStrLn handle (lineToText line)) )
+
+
+-- | Acquire a `Managed` blocking write-only `Handle` from a `FilePath`
+writeonlyblocking :: Control.Monad.Managed.MonadManaged managed => Turtle.FilePath -> managed System.IO.Handle
+writeonlyblocking file = using (managed (withBlockingTextFile file System.IO.WriteMode))
+
+
+-- Stuff that is missing from Filesystem
+
+-- | Open a file in text mode, and pass its 'Handle' to a provided
+-- computation. The 'Handle' will be automatically closed when the
+-- computation returns.
+--
+-- This computation throws 'IOError' on failure. See &#8220;Classifying
+-- I/O errors&#8221; in the "System.IO.Error" documentation for information on
+-- why the failure occured.
+withBlockingTextFile :: Turtle.FilePath -> System.IO.IOMode -> (System.IO.Handle -> IO a) -> IO a
+withBlockingTextFile path mode = Control.Exception.bracket (openBlockingTextFile path mode) System.IO.hClose
+
+-- | Open a file in text mode, and return an open 'Handle'. The 'Handle'
+-- should be closed with 'IO.hClose' when it is no longer needed.
+--
+-- 'withBlockingTextFile' is easier to use, because it will handle the
+-- 'Handle'&#x2019;s lifetime automatically.
+--
+-- This computation throws 'IOError' on failure. See &#8220;Classifying
+-- I/O errors&#8221; in the "System.IO.Error" documentation for information on
+-- why the failure occured.
+openBlockingTextFile path = GHC.IO.Handle.FD.openFileBlocking (encodeString path)
+
+
+-- See discussion https://mail.haskell.org/pipermail/haskell-cafe/2012-October/104030.html
+
+-- import GHC.IO.Handle.FD
+-- openFileBlocking :: FilePath -> IOMode -> IO Handle
 
 -- ######## Embedder.hs
 
@@ -101,10 +156,13 @@ invokeEmbedder embedder wasm = go embedder
             procs "mkfifo" [c] empty
             consumer <- forkShell $ inshell ("drun --extra-batches 100 " <> c) empty
             liftIO $ putStrLn "SLEEPING"
-            sleep 1 -- FIXME! See https://github.com/Gabriel439/Haskell-Turtle-Library/issues/368
+            --sleep 1 -- FIXME! See https://github.com/Gabriel439/Haskell-Turtle-Library/issues/368
             let install = unsafeTextToLine $ format ("install ic:2A012B "%s%" 0x") w
-            Turtle.output (fileArg control) (pure install
-                                            <|> "ingress ic:2A012B do 0x4449444c0000")
+            -- Turtle.output (fileArg control) (pure install
+            --                                 <|> "ingress ic:2A012B do 0x4449444c0000")
+
+            pipe (fileArg control) (pure install
+                                   <|> "ingress ic:2A012B do 0x4449444c0000")
             lns <- wait consumer
             liftIO $ putStrLn "WAITED"
             view lns
