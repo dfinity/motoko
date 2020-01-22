e697208b698f7aa37f454682828803dcf5a6a843
Joachim Breitner <mail@joachim-breitner.de>
2020-01-22 09:43:32 +0100

    Add LEB rountrip tests to test_rts.c
    shows bugs with length calculation and “not shortest encoding”

--- rts/test_rts.c
+++ rts/test_rts.c
@@ -4,7 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
+
 #include "rts.h"
+#include "buf.h"
 
 typedef intptr_t as_ptr;
 
@@ -18,12 +20,12 @@ as_ptr alloc_words(size_t n) {
     return alloc_bytes(sizeof(size_t) * n);
 };
 
-void rts_trap() {
-  printf("RTS trap\n");
+void rts_trap(const char* str, size_t n) {
+  printf("RTS trap: %.*s\n", (int)n, str);
   abort();
 }
-void bigint_trap() {
-  printf("Bigint trap\n");
+void bigint_trap(const char* str, size_t n) {
+  printf("Bigint trap: %.*s\n", (int)n, str);
   exit(1);
 }
 
@@ -39,6 +41,42 @@ void assert(bool check, const char *fmt, ...) {
   }
 }
 
+extern as_ptr bigint_of_word32(uint32_t b);
+extern as_ptr bigint_sub(as_ptr a, as_ptr b);
+extern as_ptr bigint_add(as_ptr a, as_ptr b);
+extern as_ptr bigint_mul(as_ptr a, as_ptr b);
+extern as_ptr bigint_pow(as_ptr a, as_ptr b);
+extern bool bigint_eq(as_ptr a, as_ptr b);
+extern int bigint_leb128_size(as_ptr n);
+extern void bigint_leb128_encode(as_ptr n, unsigned char *buf);
+extern as_ptr bigint_leb128_decode(buf *buf);
+extern int bigint_sleb128_size(as_ptr n);
+extern void bigint_sleb128_encode(as_ptr n, unsigned char *buf);
+extern as_ptr bigint_sleb128_decode(buf *buf);
+
+
+void test_bigint_leb128(as_ptr n) {
+  unsigned char b[100];
+  int s = bigint_leb128_size(n);
+  bigint_leb128_encode(n, b);
+  buf buf = { b, b + 100 };
+  as_ptr n2 = bigint_leb128_decode(&buf);
+
+  printf("roundtrip: %s ", bigint_eq(n, n2) ? "ok" : (ret = EXIT_FAILURE, "not ok"));
+  printf("size: %s\n", (buf.p - b) == s ? "ok" : (ret = EXIT_FAILURE, "not ok"));
+}
+
+void test_bigint_sleb128(as_ptr n) {
+  unsigned char b[100];
+  int s = bigint_sleb128_size(n);
+  bigint_sleb128_encode(n, b);
+  buf buf = { b, b + 100 };
+  as_ptr n2 = bigint_sleb128_decode(&buf);
+
+  printf("roundtrip: %s ", bigint_eq(n, n2) ? "ok" : (ret = EXIT_FAILURE, "not ok"));
+  printf("size: %s\n", (buf.p - b) == s ? "ok" : (ret = EXIT_FAILURE, "not ok"));
+}
+
 int main () {
   printf("Motoko RTS test suite\n");
 
@@ -48,11 +86,6 @@ int main () {
    */
   printf("Testing BigInt...\n");
 
-  extern as_ptr bigint_of_word32(uint32_t b);
-  extern as_ptr bigint_mul(as_ptr a, as_ptr b);
-  extern as_ptr bigint_pow(as_ptr a, as_ptr b);
-  extern bool bigint_eq(as_ptr a, as_ptr b);
-
   printf("70**32 = 70**31 * 70: %s\n",
    bigint_eq(
     bigint_pow(bigint_of_word32(70), bigint_of_word32(32)),
@@ -60,6 +93,31 @@ int main () {
       bigint_pow(bigint_of_word32(70), bigint_of_word32(31)),
       bigint_of_word32(70)
 	       )) ? "ok" : (ret = EXIT_FAILURE, "not ok"));
+
+  /*
+   * Testing BigInt (s)leb128 encoding
+   */
+  as_ptr one = bigint_of_word32(1);
+  as_ptr two = bigint_of_word32(2);
+  for (uint32_t i = 0; i < 100; i++){
+    as_ptr two_pow_i = bigint_pow(two, bigint_of_word32(i));
+    printf("leb128 2^%u-1: ", i);
+    test_bigint_leb128(bigint_sub(two_pow_i, one));
+    printf("leb128 2^%u:   ", i);
+    test_bigint_leb128(two_pow_i);
+    printf("leb128 2^%u+1: ", i);
+    test_bigint_leb128(bigint_add(two_pow_i, one));
+  }
+
+  for (uint32_t i = 0; i < 100; i++){
+    as_ptr two_pow_i = bigint_pow(two, bigint_of_word32(i));
+    printf("sleb128 2^%u-1: ", i);
+    test_bigint_sleb128(bigint_sub(two_pow_i, one));
+    printf("sleb128 2^%u:   ", i);
+    test_bigint_sleb128(two_pow_i);
+    printf("sleb128 2^%u+1: ", i);
+    test_bigint_sleb128(bigint_add(two_pow_i, one));
+  }
 
   /*
    * Testing UTF8
