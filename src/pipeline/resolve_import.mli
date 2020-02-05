20df62263fe1510ee59c8db19f5ec0cd949a368c
Christoph Hegemann <6189397+kritzcreek@users.noreply.github.com>
2020-02-05 17:14:52 +0100

    Makes the resolve logic more lenient around extensions (#1184)
    * Makes the resolve logic more lenient around extension
    
    This allows imports like `list.mo` to resolve to `list.mo.mo` or
    `list.mo/lib.mo`.
    
    * Removes the "musn't have extension" error for imports
    
    * fixes formatting and test

--- src/pipeline/resolve_import.mli
+++ src/pipeline/resolve_import.mli
@@ -33,7 +33,4 @@ val resolve_flags : flags -> resolved_flags Diag.result
 val resolve : flags -> Syntax.prog -> filepath -> resolved_imports Diag.result
 
 (* Exported for tests *)
-val append_extension :
-  (filepath -> bool) ->
-  filepath ->
-  (filepath, Source.region -> Diag.message) result
+val append_extension : (filepath -> bool) -> filepath -> filepath
