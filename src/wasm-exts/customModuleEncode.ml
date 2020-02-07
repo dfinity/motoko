f27ba6eaf745b9e090def0e4824fc6d751e8c0d8
Gabor Greif <gabor@dfinity.org>
2020-02-07 02:15:26 +0100

    fix to relative sta and en
    these look reasonable now
    
    Emitting dw_LNE_end_sequence resets the line counter,
    so no runaway line numbers any more!
    
    cleanups!

--- src/wasm-exts/customModuleEncode.ml
+++ src/wasm-exts/customModuleEncode.ml
@@ -826,48 +826,31 @@ standard_opcode_lengths[DW_LNS_set_isa] = 1
                 uleb128 1; (* file_names_count *)
                 List.iter zero_terminated ["charToText.mo"];
             );
-            Dwarf5.(Machine.moves u8 uleb128 sleb128 write32
-                      [- dw_LNE_set_address; 0x2345;
-                       dw_LNS_copy;
 
-                       dw_LNS_advance_pc; 1;
-                       dw_LNS_copy;
+            let code_start = !code_section_start in
+            let rel addr = addr - code_start in
+            let lookup_file = function
+              | f when f = "" -> 0
+              | file -> 1 (* FIXME: for now *) in
+            let stepping (prg, state) (addr, {file; line; column} as instr) : int list * Dwarf5.Machine.state =
+              let f = lookup_file file in
+              let stmt = Instrs.mem instr !statement_positions in
+              let state' = rel addr, (f, line, column), 0, (stmt, false, false, false) in
+              (* FIXME: quadratic *)
+              prg @ Dwarf5.Machine.infer state state', state'
+            in
 
-                       dw_LNS_advance_pc; 1;
-                       dw_LNS_set_column; 13;
-                       dw_LNS_copy;
+            let sequence (sta, notes, en) =
+              Printf.printf "LINES::::  SEQUENCE start/END    ADDR: %x - %x\n" (rel sta) (rel en);
+              Instrs.iter (fun (addr, {file; line; column} as instr) -> Printf.printf "\tLINES::::  Instr    ADDR: %x - (%s:%d:%d)    %s\n" (rel addr) file line column (if Instrs.mem instr !statement_positions then "is_stmt" else "")) notes;
 
-                       dw_LNS_advance_pc; 1;
-                       - dw_LNE_end_sequence]);
+              let seq = Instrs.to_seq notes in
+              let prg, (addr, _, _, _) = Seq.fold_left stepping Dwarf5.([- dw_LNE_set_address; rel sta; dw_LNS_copy], Machine.start_state) seq in
+              Dwarf5.(Machine.moves u8 uleb128 sleb128 write32
+                        (prg @ [dw_LNS_advance_pc; rel en - addr; - dw_LNE_end_sequence]))
 
-
-            let bumm = Dwarf5.(Machine.(infer
-                                          start_state
-                                          (0x2345, default_loc, 0, default_flags)))
-            in Dwarf5.Machine.moves u8 uleb128 sleb128 write32 bumm;
-
-               let prepend x xs () = Seq.Cons (x, xs) in
-               let code_start = !code_section_start in
-               let rel addr = addr - code_start in
-               let lookup_file file = 1 (* FIXME: for now *) in
-               let stepping (prg, state) (addr, {file; line; column} as instr) : int list * Dwarf5.Machine.state =
-                 let f = lookup_file file in
-                 let stmt = Instrs.mem instr !statement_positions in
-                 let state' = rel addr, (f, line, column), 0, (stmt, false, false, false) in
-                 (* FIXME: quadratic *)
-                 prg @ Dwarf5.Machine.infer state state', state'
-             in
-
-              let sequence (sta, notes, en) =
-                Printf.printf "LINES::::  SEQUENCE start/END    ADDR: %x - %x\n" (rel sta) (rel en);
-                Instrs.iter (fun (addr, {file; line; column} as instr) -> Printf.printf "\tLINES::::  Instr    ADDR: %x - (%s:%d:%d)    %s\n" (rel addr) file line column (if Instrs.mem instr !statement_positions then "is_stmt" else "")) notes;
-
-                let seq = Instrs.to_seq notes in
-                let prg, _ = Seq.fold_left stepping Dwarf5.([- dw_LNE_set_address; sta; dw_LNS_copy], Machine.start_state) seq in
-                Dwarf5.Machine.moves u8 uleb128 sleb128 write32 prg
-
-              in
-              Sequ.iter sequence !sequence_bounds
+            in
+            Sequ.iter sequence !sequence_bounds
         )
       in
       custom_section ".debug_line" debug_line_section_body () (fs <> [])
