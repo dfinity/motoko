db82c6dffdfc049979b933226dcbf74a50b3ad79
Joachim Breitner <mail@joachim-breitner.de>
2020-02-22 06:42:58 +0100

    Nix: Include sources.nix in repo
    the fact that since #1113, the `sources.nix`, which is central to any
    nix evaluation, is fetched remotely almost prevented me from doing work
    offline. (Luckily I knew about `--option tarball-ttl 100000`).
    
    But even offline, it seems silly for nix to check github every hour if
    this file has changed (it will not, because it is pinned using a git
    revision.) Copying to the repo seems to make more sense.
    
    (Alternatively, it would make sense to fetch it in a way that specifies
    the expected file hash, so that nix will download it exactly once, maybe
    using `builtins.path` somehow.)
    
    Unfortunately, other repos that we pull in still fetch that file
    remotely, so you still need to pass `--option tarball-ttl 100000` to
    work offline. I’ll use this PR to notify our nix gurus of this issue.

--- nix/default.nix
+++ nix/default.nix
@@ -1,13 +1,12 @@
 { system ? builtins.currentSystem }:
 let
-  sourcesnix = builtins.fetchurl https://raw.githubusercontent.com/nmattia/niv/506b896788d9705899592a303de95d8819504c55/nix/sources.nix;
-  nixpkgs_src = (import sourcesnix { sourcesFile = ./sources.json; inherit pkgs; }).nixpkgs;
+  nixpkgs_src = (import ./sources.nix { sourcesFile = ./sources.json; inherit pkgs; }).nixpkgs;
 
   pkgs =
     import nixpkgs_src {
       inherit system;
       overlays = [
-        (self: super: { sources = import sourcesnix { sourcesFile = ./sources.json; pkgs = super; }; })
+        (self: super: { sources = import ./sources.nix { sourcesFile = ./sources.json; pkgs = super; }; })
         # Selecting the ocaml version
         (self: super: { ocamlPackages = self.ocaml-ng.ocamlPackages_4_08; })
         # Additional ocaml package
