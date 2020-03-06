f1e3bf91fce7648272f8f647f390c1f461b511bb
chenyan-dfinity <yan.chen@dfinity.org>
2020-03-06 15:37:04 -0800

    update rts to decode principal

--- rts/idl.c
+++ rts/idl.c
@@ -72,8 +72,6 @@ export int32_t read_i32_of_sleb128(buf *buf) {
 #define IDL_PRIM_reserved (-16)
 #define IDL_PRIM_empty    (-17)
 
-#define IDL_PRIM_lowest   (-17)
-
 #define IDL_CON_opt       (-18)
 #define IDL_CON_vec       (-19)
 #define IDL_CON_record    (-20)
@@ -81,6 +79,12 @@ export int32_t read_i32_of_sleb128(buf *buf) {
 #define IDL_CON_func      (-22)
 #define IDL_CON_service   (-23)
 
+#define IDL_REF_principal (-24)
+
+bool is_primitive_type(int32_t ty) {
+  static const int32_t IDL_PRIM_lowest = -17;
+  return ty >= IDL_PRIM_lowest || ty == IDL_REF_principal;
+}
 
 /*
  * This function parses the IDL magic header and type description. It
@@ -115,36 +119,36 @@ export void parse_idl_header(buf *buf, uint8_t ***typtbl_out, uint8_t **main_typ
   for (int i = 0; i < n_types; i++) {
     typtbl[i] = buf->p;
     int32_t ty = read_i32_of_sleb128(buf);
-    if (ty >= IDL_PRIM_lowest) {
+    if (is_primitive_type(ty)) {
       idl_trap_with("primitive type in type table"); // illegal
     } else if (ty == IDL_CON_opt) {
       int32_t t = read_i32_of_sleb128(buf);
-      if (t < IDL_PRIM_lowest || t >= n_types) idl_trap_with("type index out of range");
+      if (!is_primitive_type(t) || t >= n_types) idl_trap_with("type index out of range");
     } else if (ty == IDL_CON_vec) {
       int32_t t = read_i32_of_sleb128(buf);
-      if (t < IDL_PRIM_lowest || t >= n_types) idl_trap_with("type index out of range");
+      if (!is_primitive_type(t) || t >= n_types) idl_trap_with("type index out of range");
     } else if (ty == IDL_CON_record) {
       for (uint32_t n = read_u32_of_leb128(buf); n > 0; n--) {
         read_u32_of_leb128(buf);
         int32_t t = read_i32_of_sleb128(buf);
-        if (t < IDL_PRIM_lowest || t >= n_types) idl_trap_with("type index out of range");
+        if (!is_primitive_type(t) || t >= n_types) idl_trap_with("type index out of range");
       }
     } else if (ty == IDL_CON_variant) {
       for (uint32_t n = read_u32_of_leb128(buf); n > 0; n--) {
         read_u32_of_leb128(buf);
         int32_t t = read_i32_of_sleb128(buf);
-        if (t < IDL_PRIM_lowest || t >= n_types) idl_trap_with("type index out of range");
+        if (!is_primitive_type(t) || t >= n_types) idl_trap_with("type index out of range");
       }
     } else if (ty == IDL_CON_func) {
       // arg types
       for (uint32_t n = read_u32_of_leb128(buf); n > 0; n--) {
         int32_t t = read_i32_of_sleb128(buf);
-        if (t < IDL_PRIM_lowest || t >= n_types) idl_trap_with("type index out of range");
+        if (!is_primitive_type(t) || t >= n_types) idl_trap_with("type index out of range");
       }
       // ret types
       for (uint32_t n = read_u32_of_leb128(buf); n > 0; n--) {
         int32_t t = read_i32_of_sleb128(buf);
-        if (t < IDL_PRIM_lowest || t >= n_types) idl_trap_with("type index out of range");
+        if (!is_primitive_type(t) || t >= n_types) idl_trap_with("type index out of range");
       }
       // annotations
       for (uint32_t n = read_u32_of_leb128(buf); n > 0; n--) {
@@ -157,7 +161,7 @@ export void parse_idl_header(buf *buf, uint8_t ***typtbl_out, uint8_t **main_typ
         (buf->p) += size;
         // type
         int32_t t = read_i32_of_sleb128(buf);
-        if (t < IDL_PRIM_lowest || t >= n_types) idl_trap_with("type index out of range");
+        if (!is_primitive_type(t) || t >= n_types) idl_trap_with("type index out of range");
       }
     } else { // future type
       uint32_t n = read_u32_of_leb128(buf);
@@ -168,7 +172,8 @@ export void parse_idl_header(buf *buf, uint8_t ***typtbl_out, uint8_t **main_typ
   *main_types_out = buf->p;
   for (uint32_t n = read_u32_of_leb128(buf); n > 0; n--) {
     int32_t t = read_i32_of_sleb128(buf);
-    if (t < IDL_PRIM_lowest || t >= n_types) idl_trap_with("type index out of range");
+    if (!is_primitive_type(t) || t >= n_types)
+      idl_trap_with("type index out of range");
   }
 
   *typtbl_out = typtbl;
@@ -227,6 +232,12 @@ export void skip_any(buf *b, uint8_t **typtbl, int32_t t, int32_t depth) {
         return;
       case IDL_PRIM_empty:
         idl_trap_with("skip_any: encountered empty");
+      case IDL_REF_principal:
+        {
+          advance(b, 1);
+          uint32_t len = read_u32_of_leb128(b);
+          advance(b, len);
+        }
       default:
         idl_trap_with("skip_any: unknown prim");
     }
