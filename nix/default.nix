b980bd73c5ffcc33fb48b5f2360fe0d4d9b9d319
Gabor Greif <gabor@dfinity.org>
2020-04-09 14:33:02 +0200

    bump to ocamlPackages_4_09
    Caveat: Be sure to re-enter your nix-shell!

--- nix/default.nix
+++ nix/default.nix
@@ -12,7 +12,8 @@ let
       overlays = [
         (self: super: { sources = import sourcesnix { sourcesFile = ./sources.json; pkgs = super; }; })
         # Selecting the ocaml version
-        # (self: super: { ocamlPackages = super.ocamlPackages; })
+        (self: super: { ocamlPackages = self.ocaml-ng.ocamlPackages_4_09; })
+        # Additional ocaml package
         (
           self: super: {
             # Additional ocaml package
