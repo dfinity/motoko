9776481fb7a6041d3668719ff2110347b61f2075
Claudio Russo <claudio@dfinity.org>
2020-04-21 17:14:04 +0100

    Revert "Stable variables desugaring (Part I) (#1338)"
    This reverts commit 746ff81b2525d33173d9afbb2b80fb52b8f68840.

--- src/mo_def/arrange.ml
+++ src/mo_def/arrange.ml
@@ -132,13 +132,6 @@ and vis v = match v.it with
   | Public  -> Atom "Public"
   | Private -> Atom "Private"
 
-and stab s_opt = match s_opt with
-  | None -> Atom "(Flexible)"
-  | Some s ->
-    (match s.it with
-    | Flexible -> Atom "Flexible"
-    | Stable -> Atom "Stable")
-
 and typ_field (tf : typ_field)
   = tf.it.id.it $$ [typ tf.it.typ; mut tf.it.mut]
 
@@ -149,7 +142,7 @@ and typ_bind (tb : typ_bind)
   = tb.it.var.it $$ [typ tb.it.bound]
 
 and exp_field (ef : exp_field)
-  = "Field" $$ [dec ef.it.dec; vis ef.it.vis; stab ef.it.stab]
+  = "Field" $$ [dec ef.it.dec; vis ef.it.vis]
 
 and operator_type t = Atom (Type.string_of_typ t)
 
