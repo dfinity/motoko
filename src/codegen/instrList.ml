a979dd230191c2d2953c59f8b3042ab709ad9b01
Gabor Greif <gabor@dfinity.org>
2020-04-29 13:08:23 +0200

    preliminary hack

--- src/codegen/instrList.ml
+++ src/codegen/instrList.ml
@@ -300,7 +300,7 @@ let rec dw_tag : dw_TAG -> t =
       (dw_attrs [Low_pc 0(*FIXME*); High_pc 0(*FIXME*); Name name; Decl_line pos.Source.line; Decl_column pos.Source.column; Prototyped true; External false])
   | Formal_parameter (name, pos, ty, slot) ->
     fakeBlock dw_TAG_formal_parameter
-      (dw_attrs [Name name; Decl_line pos.Source.line; Decl_column pos.Source.column; TypeRef (snd (dw_type_ref ty)); Location "\xED\x02\x00\x9F"(*FIXME*)])
+      (dw_attrs [Name name; Decl_line pos.Source.line; Decl_column pos.Source.column; TypeRef (snd (dw_type_ref ty)); Location "\xED\x00\x01\x9F"(*FIXME*)])
   (*| Variable ->  *)
   | Type ty -> dw_type ty
   | _ -> assert false
