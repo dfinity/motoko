2ad8eeda9b882a0daba7a6c0aee6fc13ae18864e
Joachim Breitner <mail@joachim-breitner.de>
2020-05-05 16:37:19 +0200

    Backend: Simplify passing types to G.if_, G.look_ (#1472)
    once we actually want to return more than one value from these we need
    to do some extra work, but the DSL interface should be no uglier than
    this (which is ugly enough).
    
    This is just some cleanup to follow up on
    https://github.com/dfinity-lab/motoko/pull/1467, reverting most of the
    nosiy changes to `compile.ml` and putting some adjustment into
    `instrList.ml`
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/codegen/instrList.ml
+++ src/codegen/instrList.ml
@@ -10,6 +10,7 @@ features are
 open Wasm.Ast
 open Wasm.Source
 open Wasm.Values
+open Wasm.Types
 
 let combine_shifts const op = function
   | I32 opl, ({it = I32 l'; _} as cl), I32 opr, I32 r' when opl = opr ->
@@ -102,17 +103,22 @@ let with_region (pos : Source.region) (body : t) : t =
 
 (* Depths-managing combinators *)
 
-let if_ (ty : block_type) (thn : t) (els : t) : t =
-  fun d pos rest ->
-    (If (ty, to_nested_list d pos thn, to_nested_list d pos els) @@ pos) :: rest
+let as_block_type : stack_type -> block_type = function
+  | [] -> ValBlockType None
+  | [t] -> ValBlockType (Some t)
+  | _ -> raise (Invalid_argument "instrList block combinators do not support multi-value yet")
 
-let block_ (ty : block_type) (body : t) : t =
+let if_ (ty : stack_type) (thn : t) (els : t) : t =
   fun d pos rest ->
-    (Block (ty, to_nested_list d pos body) @@ pos) :: rest
+    (If (as_block_type ty, to_nested_list d pos thn, to_nested_list d pos els) @@ pos) :: rest
 
-let loop_ (ty : block_type) (body : t) : t =
+let block_ (ty : stack_type) (body : t) : t =
   fun d pos rest ->
-    (Loop (ty, to_nested_list d pos body) @@ pos) :: rest
+    (Block (as_block_type ty, to_nested_list d pos body) @@ pos) :: rest
+
+let loop_ (ty : stack_type) (body : t) : t =
+  fun d pos rest ->
+    (Loop (as_block_type ty, to_nested_list d pos body) @@ pos) :: rest
 
 (* Remember depth *)
 type depth = int32 Lib.Promise.t
@@ -137,7 +143,7 @@ let branch_to_ (p : depth) : t =
 
 (* Convenience combinations *)
 
-let labeled_block_ (ty : block_type) depth (body : t) : t =
+let labeled_block_ (ty : stack_type) depth (body : t) : t =
   block_ ty (remember_depth depth body)
 
 (* Intended to be used within assert *)
