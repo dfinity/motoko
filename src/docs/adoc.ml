9061b9ff79922de48d82aec59a3f4ab6d2be2b12
Christoph <christoph.hegemann1337@gmail.com>
2020-06-02 23:37:01 +0200

    Move the documentation comment in adocs
    It's now placed between declaration heading and its signature

--- src/docs/adoc.ml
+++ src/docs/adoc.ml
@@ -16,14 +16,14 @@ let adoc_of_function_arg : Buffer.t -> function_arg_doc -> unit =
   Plain.opt_typ buf arg.typ
 
 let adoc_header : Buffer.t -> int -> string -> unit =
- fun buf lvl s ->
-  bprintf buf "%s %s\n" (String.make (lvl + 3) '=') s
+ fun buf lvl s -> bprintf buf "%s %s\n" (String.make (lvl + 3) '=') s
 
 let adoc_signature : Buffer.t -> int -> (unit -> unit) -> unit =
  fun buf lvl f ->
   Buffer.add_string buf (String.make (lvl + 3) '=');
   bprintf buf " ";
-  surround buf "`" (fun _ -> surround buf "+" f)
+  surround buf "`" (fun _ -> surround buf "+" f);
+  bprintf buf "\n\n"
 
 let adoc_of_doc_type : Buffer.t -> Extract.doc_type -> unit =
  fun buf dt ->
@@ -31,13 +31,15 @@ let adoc_of_doc_type : Buffer.t -> Extract.doc_type -> unit =
   | DTPlain t -> Plain.plain_of_typ buf t
   | DTObj (t, fields) -> Plain.plain_of_typ buf t
 
-let rec adoc_of_declaration : Buffer.t -> int -> declaration_doc -> unit =
- fun buf lvl doc ->
+let rec adoc_of_declaration :
+    Buffer.t -> int -> (unit -> unit) -> declaration_doc -> unit =
+ fun buf lvl doc_comment doc ->
   let header = adoc_header buf lvl in
   let signature = adoc_signature buf lvl in
   match doc with
   | Function function_doc ->
       header function_doc.name;
+      doc_comment ();
       signature (fun _ ->
           bprintf buf "func %s" function_doc.name;
           Plain.sep_by' buf "<" ", " ">" (adoc_of_typ_bind buf)
@@ -48,11 +50,13 @@ let rec adoc_of_declaration : Buffer.t -> int -> declaration_doc -> unit =
           Plain.opt_typ buf function_doc.typ)
   | Value value_doc ->
       header value_doc.name;
+      doc_comment ();
       signature (fun _ ->
           bprintf buf "let %s" value_doc.name;
           Plain.opt_typ buf value_doc.typ)
   | Type type_doc ->
       header type_doc.name;
+      doc_comment ();
       signature (fun _ ->
           bprintf buf "type %s" type_doc.name;
           Plain.sep_by' buf "<" ", " ">" (adoc_of_typ_bind buf)
@@ -61,18 +65,24 @@ let rec adoc_of_declaration : Buffer.t -> int -> declaration_doc -> unit =
           adoc_of_doc_type buf type_doc.typ)
   | Class class_doc ->
       header class_doc.name;
+      doc_comment ();
       signature (fun _ ->
           bprintf buf "class %s" class_doc.name;
           Plain.sep_by' buf "<" ", " ">" (adoc_of_typ_bind buf)
             class_doc.type_args);
       bprintf buf "\n\n";
       List.iter (adoc_of_doc buf (lvl + 1)) class_doc.fields
-  | Unknown unknown -> signature (fun _ -> bprintf buf "Unknown %s" unknown)
+  | Unknown unknown ->
+      signature (fun _ -> bprintf buf "Unknown %s" unknown);
+      doc_comment ()
 
 and adoc_of_doc : Buffer.t -> int -> doc -> unit =
  fun buf lvl { doc_comment; declaration } ->
-  adoc_of_declaration buf lvl declaration;
-  surround buf "\n\n" (fun _ -> Option.iter (Buffer.add_string buf) doc_comment)
+  let doc_comment () =
+    surround buf "\n\n" (fun _ ->
+        Option.iter (Buffer.add_string buf) doc_comment)
+  in
+  adoc_of_declaration buf lvl doc_comment declaration
 
 let render_docs : string -> doc list -> string =
  fun module_docs docs ->
