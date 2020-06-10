a87a35e9b8cb218d0c3158f8316bee50b2c7d985
Joachim Breitner <mail@joachim-breitner.de>
2020-06-10 17:16:47 +0200

    Update src/lowering/desugar.mli
    Co-authored-by: Claudio Russo <claudio@dfinity.org>

--- src/lowering/desugar.mli
+++ src/lowering/desugar.mli
@@ -2,7 +2,7 @@ open Mo_def
 open Ir_def
 
 (* These declaratoins are all internal, either from prelude (@-prefixed)
-   to to bring libraries into scope *)
+   or to bring libraries into scope *)
 type import_declaration = Ir.dec list
 
 val transform_lib : Syntax.lib -> import_declaration
