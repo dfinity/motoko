8c65b3347c50ad46e89ca2870cc72c52ea8c1f0c
Joachim Breitner <mail@joachim-breitner.de>
2020-06-10 11:55:04 +0200

    Make desguaring hack worse

--- src/lowering/desugar.ml
+++ src/lowering/desugar.ml
@@ -572,26 +572,30 @@ and to_args typ po p : Ir.arg list * (Ir.exp -> Ir.exp) * T.control * T.typ list
 
 and comp_unit ds : Ir.comp_unit =
   let open Ir in
-  let find_last_expr ds (e : exp) =
-    if ds = [] then [], e else
+
+  let find_last_expr (ds,e) =
+
+    let find_last_actor = function
+      | ds1, {it = ActorE (ds2, fs, up, t); _} ->
+        (* NB: capture! *)
+        ActorU (ds1 @ ds2, fs, up, t)
+      | ds1, {it = FuncE (_name, _sort, _control, [], [], _, {it = ActorE (ds2, fs, up, t);_}); _} ->
+        ActorU (ds1 @ ds2, fs, up, t)
+      | _ ->
+        ProgU (ds @ [ expD e ]) in
+
+    if ds = [] then ProgU [] else
     match Lib.List.split_last ds, e with
     | (ds1', {it = LetD ({it = VarP i1; _}, e'); _}), {it = PrimE (TupPrim, []); _} ->
-      ds1', e'
+      find_last_actor (ds1', e')
     | (ds1', {it = LetD ({it = VarP i1; _}, e'); _}), {it = VarE i2; _} when i1 = i2 ->
-      ds1', e'
-    | _ -> ds, e in
-
-  let find_last_actor (ds,e) = match find_last_expr ds e with
-    | ds1, {it = ActorE (ds2, fs, up, t); _} ->
-      (* NB: capture! *)
-      ActorU (ds1 @ ds2, fs, up, t)
-    | ds1, {it = FuncE (_name, _sort, _control, [], [], _, {it = ActorE (ds2, fs, up, t);_}); _} ->
-      ActorU (ds1 @ ds2, fs, up, t)
-    | ds, e -> ProgU (ds @ [ expD e ]) in
+      find_last_actor (ds1', e')
+    | _ ->
+      find_last_actor (ds, e) in
 
   (* find the last actor. This hack can hopefully go away
      after Claudios improvements to the source *)
-  find_last_actor (block false ds)
+  find_last_expr (block false ds)
 
 and prog (p : Syntax.prog) : Ir.prog =
   begin match p.it with
