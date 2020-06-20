6f571b8d669dd2f165b9b23fedb02625d0532eb2
Joachim Breitner <mail@joachim-breitner.de>
2020-06-20 17:43:48 +0200

    Strip nix hash a bit
    so that it will hopefully not be picked up as a “dependency”

--- src/source_id/gen.sh
+++ src/source_id/gen.sh
@@ -7,7 +7,7 @@ file="source_id.ml"
 if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = true ]
 then echo "let id = \"$(git describe --always --dirty)\"" > $file.tmp
 elif [ -n "$out" ]
-then echo "let id = \"$(echo $out|cut -d/ -f4)\"" > $file.tmp
+then echo "let id = \"$(echo $out|cut -d/ -f4|cut -d- -f1)\"" > $file.tmp
 else echo "let id = \"unidentified version\"" > $file.tmp
 fi
 if [ ! -e $file ] || ! cmp -s $file.tmp $file
