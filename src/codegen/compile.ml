cab2a30bf2ecc2a8c95abf0a32a8fddb836fd72e
Gabor Greif <gabor@dfinity.org>
2020-07-01 13:24:51 +0200

    merge snafu

--- src/codegen/compile.ml
+++ src/codegen/compile.ml
@@ -7080,8 +7080,8 @@ and compile_exp (env : E.t) ae exp =
     | OtherPrim "Float->Text", [e] ->
       SR.Vanilla,
       compile_exp_as env ae SR.UnboxedFloat64 e ^^
-      compile_unboxed_const (UnboxedSmallWord.vanilla_lit Type.Word8 6) ^^
-      compile_unboxed_const (UnboxedSmallWord.vanilla_lit Type.Word8 0) ^^
+      compile_unboxed_const (TaggedSmallWord.vanilla_lit Type.Word8 6) ^^
+      compile_unboxed_const (TaggedSmallWord.vanilla_lit Type.Word8 0) ^^
       E.call_import env "rts" "float_fmt"
 
     | OtherPrim "fmtFloat->Text", [f; prec; mode] ->
