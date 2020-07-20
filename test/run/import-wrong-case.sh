1693d2b3e1c72f30028d66f6732a84e8ef401422
Gabor Greif <gabor@dfinity.org>
2020-07-20 10:57:30 +0200

    don't rely on script status code instead provoke some explanatory
    output when the script fails. This way the subsequent
    `.stdout`-comparison will fail with the desired effect.

--- test/run/import-wrong-case.sh
+++ test/run/import-wrong-case.sh
@@ -6,6 +6,9 @@ then
   true
 else
   # on osx, this should give the expected warning
-  $MOC --check lib/pkg/wrong-case-import.mo |
-    grep -q -F "warning, file other-Module.mo has been located with a name of different case"
+  WARNING='file other-Module.mo has been located with a name of different case'
+
+  $MOC --check lib/pkg/wrong-case-import.mo \
+      |& grep -q -F "warning, $WARNING" \
+      || echo "warning '$WARNING' not encountered"
 fi
