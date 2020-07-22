e8685a54692a0772c159c03a054c6f0684eae2ca
Gabor Greif <gabor@dfinity.org>
2020-07-22 16:56:35 +0200

    make lookup table static

--- rts/blob.c
+++ rts/blob.c
@@ -130,7 +130,7 @@ static void accum_base32(struct Pump* pump, uint8_t d) {
   // values are one more than base32 value
   // 0 elements signify invalid alphabet letter
   // fillers/padding have upper bit set
-  const uint8_t conv[0x80] = {
+  static const uint8_t conv[0x80] = {
     ['-'] = 0xF0, ['='] = 0xF1,
     ['A'] = 1, ['B'] = 2, ['C'] = 3, ['D'] = 4, ['E'] = 5, ['F'] = 6, ['G'] = 7, ['H'] = 8, ['I'] = 9, ['J'] = 10, ['K'] = 11,
     ['L'] = 12, ['M'] = 13, ['N'] = 14, ['O'] = 15, ['P'] = 16, ['Q'] = 17, ['R'] = 18, ['S'] = 19, ['T'] = 20, ['U'] = 21, ['V'] = 22,
