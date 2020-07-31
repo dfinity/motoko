25cb55d1061b6b10573eb0c034c29203522c2637
Christoph <christoph.hegemann1337@gmail.com>
2020-07-31 11:08:52 +0200

    match the Motoko version scheme

--- release.nix
+++ release.nix
@@ -1,11 +1,11 @@
 { src ? null }:
 let
-  # doRelease is true when the git tag is of the right release format like `0.1.2`.
+  # doRelease is true when the git tag is of the right release format like `0.42`.
   doRelease = src != null && versionMatches != null;
 
-  # versionMatch is `null` if `src.gitTag` is not of the right format like "1.23.456"
-  # and it's a list of matches like [ "1.23.456" ] when it is.
-  versionMatches = builtins.match "([0-9]+\.[0-9]+\.[0-9]+)" src.gitTag;
+  # versionMatch is `null` if `src.gitTag` is not of the right format like "0.42"
+  # and it's a list of matches like [ "0.42" ] when it is.
+  versionMatches = builtins.match "([0-9]+\.[0-9]+)" src.gitTag;
   releaseVersion = if versionMatches == null then "latest" else builtins.head versionMatches;
 
   ci = import ./ci.nix { inherit src releaseVersion; };
