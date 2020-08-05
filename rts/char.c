b0db217d5ad294ac4311b9c900e0331408c962ac
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-08-05 11:00:50 +0300

    Showing how to use printf ... and fail!

--- rts/char.c
+++ rts/char.c
@@ -12,11 +12,14 @@ export int32_t char_to_lower(int32_t c) {
   return (int32_t)towlower((unsigned)c);
 }
 
+int printf(const char *__restrict, ...);
+
 // Boolean values in Motoko are 0 for False and 1 for True, so we make sure we
 // return those here don't do any marshalling in generated code.
 #define CHAR_PRED(rts_fn, musl_fn) \
   export int32_t rts_fn(int32_t c) { \
     extern int musl_fn(unsigned); \
+    printf("just testing\n"); \
     return musl_fn((unsigned)c) != 0; \
   }
 
