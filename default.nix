29726b0dcd7470c4785b634148e59725befaf933
chenyan-dfinity <yan.chen@dfinity.org>
2020-09-08 12:12:44 -0700

    nix

--- default.nix
+++ default.nix
@@ -67,7 +67,7 @@ let commonBuildInputs = pkgs:
     pkgs.ocamlPackages.ppx_tools_versioned
     pkgs.ocamlPackages.obelisk
     pkgs.ocamlPackages.uucp
-    pkgs.ocamlPackages.bigstringaf
+    pkgs.ocamlPackages.bigstring
     pkgs.perl
   ]; in
 
