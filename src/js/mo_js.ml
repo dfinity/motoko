e67a1deed22f6283fc30dc10c3b58932e848d903
chenyan-dfinity <yan.chen@dfinity.org>
2020-09-10 15:35:48 -0700

    checkpoint

--- src/js/mo_js.ml
+++ src/js/mo_js.ml
@@ -1,9 +1,9 @@
-(*open Wasm_exts
+open Wasm_exts
 open Source
 open Mo_config
- *)
+
 module Js = Js_of_ocaml.Js
-(*
+
 let position_of_pos pos =
   object%js
     (* The LSP spec requires zero-based positions *)
@@ -37,48 +37,44 @@ let js_check source =
     val diagnostics = Js.array (diagnostics_of_msgs msgs)
     val code = Js.null
   end
- *)
+
 let js_run source =
   let results = Pipeline.run_string (Js.to_string source) in
   let result = String.concat "\n" results in
   Js.string result
-  (*
+  
 let js_compile_with mode_string do_link source convert =
   let mode =
     match Js.to_string mode_string with
-    | "wasm" -> Flags.WasmMode
     | "wasi" -> Flags.WASIMode
     | "dfinity" -> Flags.ICMode
-    | "icref" -> Flags.RefMode
     | _ -> raise (Invalid_argument "js_compile_with: Unexpected mode")
   in
   match Pipeline.compile_string mode (Js.to_bool do_link) (Js.to_string source) Filename.current_dir_name with
   | Ok (module_, msgs) ->
-    let (code, map) = convert module_ in
+    let code = convert module_ in
     object%js
       val diagnostics = Js.array (diagnostics_of_msgs msgs)
       val code = Js.some code
-      val map = Js.some map
     end
   | Error msgs ->
     object%js
       val diagnostics = Js.array (diagnostics_of_msgs msgs)
       val code = Js.null
-      val map = Js.null
     end
 
 let js_compile_wasm mode do_link s =
   js_compile_with mode do_link s
     (fun m ->
-      let (map, wasm) = CustomModuleEncode.encode m in
+      let (_, wasm) = CustomModuleEncode.encode m in
       let len = String.length wasm in
-      Js_of_ocaml.Typed_array.Bigstring.to_arrayBuffer (Bigstringaf.of_string ~off:0 ~len:len wasm), Js.string map
+      Js_of_ocaml.Typed_array.Bigstring.to_arrayBuffer (Bigstringaf.of_string ~off:0 ~len:len wasm)
     )
-   *)
+
 let () =
   Js.export "Motoko"
     (object%js
-      (*method check s = js_check s
-      method compileWasm mode do_link s = js_compile_wasm mode do_link s*)
+      method check s = js_check s
+      method compileWasm mode do_link s = js_compile_wasm mode do_link s
       method run s = js_run s
     end);
