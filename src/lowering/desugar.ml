c2abc2e9dc4cc80bc32776c3815e05166a771cf6
Claudio Russo <claudio@dfinity.org>
2020-09-17 14:21:08 +0100

    fix bugs

--- src/lowering/desugar.ml
+++ src/lowering/desugar.ml
@@ -136,15 +136,13 @@ and exp' at note = function
   (* Funds *)
   | S.CallE ({it=S.AnnotE ({it=S.PrimE "fundsBalance";_},_);_}, _, e) ->
     I.PrimE (I.SystemFundsBalancePrim, [exp e])
-  | S.CallE ({it=S.AnnotE ({it=S.PrimE "fundsAvailable";_},_);_}, _, {it=S.TupE es;_}) ->
-    assert (es = []);
-    I.PrimE (I.SystemTimePrim, [])
-  | S.CallE ({it=S.AnnotE ({it=S.PrimE "fundsRefunded";_},_);_}, _, {it=S.TupE es;_}) ->
-    assert (es = []);
-    I.PrimE (I.SystemTimePrim, [])
+  | S.CallE ({it=S.AnnotE ({it=S.PrimE "fundsAvailable";_},_);_}, _, e) ->
+    I.PrimE (I.SystemFundsAvailablePrim, [exp e])
+  | S.CallE ({it=S.AnnotE ({it=S.PrimE "fundsRefunded";_},_);_}, _, e) ->
+    I.PrimE (I.SystemFundsRefundedPrim, [exp e])
   | S.CallE ({it=S.AnnotE ({it=S.PrimE "fundsAccept";_},_);_}, _, {it=S.TupE es;_}) ->
     assert (List.length es = 2);
-    I.PrimE (I.SystemTimePrim, exps es)
+    I.PrimE (I.SystemFundsAcceptPrim, exps es)
   | S.CallE ({it=S.AnnotE ({it=S.PrimE p;_},_);_}, _, {it=S.TupE es;_}) ->
     I.PrimE (I.OtherPrim p, exps es)
   | S.CallE ({it=S.AnnotE ({it=S.PrimE p;_},_);_}, _, e) ->
