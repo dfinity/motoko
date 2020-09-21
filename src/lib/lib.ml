c2dc9de37d3b9b37104590c909fe713afcc86395
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-09-21 10:50:57 +0300

    Slightly refactor err msg generation in open_in
    Instead of allocating two error strings ahead of time and then and
    picking one, allocate just the used one after checking the condition.

--- src/lib/lib.ml
+++ src/lib/lib.ml
@@ -574,12 +574,13 @@ struct
     if not (Array.exists (fun name -> name = base) files) then
       begin
         let open Stdlib.String in
-        let message = Printf.sprintf "file %s has been located with a name of different case" base in
-        let message' = Printf.sprintf "file %s has been located with a different name" base in
         let base = lowercase_ascii base in
         if Array.exists (fun name -> lowercase_ascii name = base) files then
+          let message = Printf.sprintf "file %s has been located with a name of different case" base in
           ic, [message]
-        else ic, [message']
+        else
+          let message = Printf.sprintf "file %s has been located with a different name" base in
+          ic, [message]
       end
     else ic, []
 end
