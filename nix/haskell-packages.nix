b31082923db8f9cd8823ddc29c5d932f1403140c
Joachim Breitner <mail@joachim-breitner.de>
2020-10-01 17:36:34 +0200

    Blindly remove some haskell version pins

--- nix/haskell-packages.nix
+++ nix/haskell-packages.nix
@@ -1,13 +1,6 @@
 nix: subpath:
   let stdenv = nix.stdenv; in
   self: super: {
-  haskell-lsp-types = self.haskell-lsp-types_0_20_0_0;
-
-  haskell-lsp = self.haskell-lsp_0_20_0_1;
-
-  parser-combinators = self.parser-combinators_1_2_1;
-
-  lsp-test = nix.haskell.lib.dontCheck self.lsp-test_0_10_1_0;
 
   lsp-int = super.callPackage generated/lsp-int.nix {};
 
