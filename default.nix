61bdf72471330d6e2e75cf753b7a4f63289ec4f5
Joachim Breitner <mail@joachim-breitner.de>
2020-10-08 09:29:54 +0200

    Try this

--- default.nix
+++ default.nix
@@ -296,6 +296,7 @@ rec {
       src = subpath ./src;
       buildInputs = commonBuildInputs staticpkgs;
       checkPhase = ''
+        patchShebangs .
         make DUNE_OPTS="--display=short" unit-tests
       '';
       installPhase = ''
