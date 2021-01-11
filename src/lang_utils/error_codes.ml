42378ccaed1d89b4553c6b155112b7989e2aab23
Christoph <christoph.hegemann1337@gmail.com>
2021-01-11 12:01:39 +0100

    adds a module that collects _all_ error codes

---
+++ src/lang_utils/error_codes.ml
@@ -0,0 +1,6 @@
+(* A list of all error codes used in the compiler. The second field in
+   each tuple represents a possible long-form description of the
+   error. *)
+let error_codes : (string * string option) list =
+  [
+  ]
