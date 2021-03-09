be1e9db7096cb0c292df9dc3569290e8b1af70ff
Joachim Breitner <mail@joachim-breitner.de>
2021-03-09 16:35:00 +0100

    Fix rust visiblity

--- rts/motoko-rts/src/utf8.rs
+++ rts/motoko-rts/src/utf8.rs
@@ -10,6 +10,6 @@ pub(crate) unsafe extern "C" fn utf8_validate(str: *const libc::c_char, len: u32
 
 /// Returns whether the string is valid UTF-8
 #[no_mangle]
-pub(crate) unsafe fn utf8_valid(str: *const libc::c_char, len: u32) -> bool {
+pub unsafe fn utf8_valid(str: *const libc::c_char, len: u32) -> bool {
     core::str::from_utf8(core::slice::from_raw_parts(str as *const _, len as usize)).is_ok()
 }
