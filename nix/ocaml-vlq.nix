d5e3399f3822e8d97b43b932082b0deb16c206f7
Joachim Breitner <mail@joachim-breitner.de>
2021-05-19 11:53:26 +0200

    Build vlq with buildDunePackage and dune2

--- nix/ocaml-vlq.nix
+++ nix/ocaml-vlq.nix
@@ -1,25 +1,16 @@
-{ stdenv, fetchFromGitHub, ocaml, findlib, dune_1 }:
+{ buildDunePackage, fetchFromGitHub }:
 
 let version = "v0.2.0"; in
-stdenv.mkDerivation {
-  name = "ocaml${ocaml.version}-vlq-${version}";
+buildDunePackage {
+  pname = "vlq";
+  inherit version;
+
+  useDune2 = true;
 
   src = fetchFromGitHub {
     owner = "flowtype";
     repo = "ocaml-vlq";
     rev = version;
     sha256 = "09jdgih2n2qwpxnlbcca4xa193rwbd1nw7prxaqlg134l4mbya83";
-  };
-
-  buildInputs = [ ocaml findlib dune_1 ];
-
-  buildPhase = "dune build";
-
-  installPhase = "dune install --prefix $out --libdir $OCAMLFIND_DESTDIR vlq";
-
-  meta = {
-    homepage = https://github.com/flowtype/ocaml-vlq;
-    platforms = ocaml.meta.platforms or [];
-    description = "A simple library for encoding variable-length quantities";
   };
 }
