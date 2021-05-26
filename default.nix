13c25e3fa4f746539b61d1174e4c4437efe0ecfd
Joachim Breitner <mail@joachim-breitner.de>
2021-05-26 10:27:10 +0200

    Does this work?

--- default.nix
+++ default.nix
@@ -20,8 +20,8 @@ let haskellPackages = nixpkgs.haskellPackages.override {
     }; in
 let
   rtsBuildInputs = with nixpkgs; [
-    clang_10 # for native/wasm building
-    lld_10 # for wasm building
+    llvmPackages_10.clang # for native/wasm building
+    llvmPackages_10.lld
     llvmPackages_10.bintools
     rustc-nightly
     cargo-nightly
@@ -32,13 +32,19 @@ let
   ];
 
   llvmEnv = ''
+    # When compiling to wasm, we want to have more control over the flags,
+    # so we do not use the nix-provided wraper in clang
+    export WASM_CLANG="clang-10"
+    export WASM_LD=wasm-ld
+    # because we use the unwrapped clang, we have to pass in some flags/paths
+    # that otherwise the wrapped clang would take care for us
+    export WASM_CLANG_LIB="${nixpkgs.llvmPackages_10.clang-unwrapped.lib}"
+
     # When compiling natively, we want to use `clang` (which is a nixpkgs
     # provided wrapper that sets various include paths etc).
     # But for some reason it does not handle building for Wasm well, so
     # there we use plain clang-10. There is no stdlib there anyways.
     export CLANG="${nixpkgs.clang_10}/bin/clang"
-    export WASM_CLANG="clang-10"
-    export WASM_LD=wasm-ld
   '';
 in
 
