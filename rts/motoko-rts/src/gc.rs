a36ca2d17441f8d779e4e592da80ee14d86b722d
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-07-06 09:57:00 +0300

    Rename growing -> growth

--- rts/motoko-rts/src/gc.rs
+++ rts/motoko-rts/src/gc.rs
@@ -8,20 +8,20 @@ unsafe fn should_do_gc() -> bool {
     use core::cmp::{max, min};
 
     // A factor of last heap size. We allow at most this much allocation before doing GC.
-    const HEAP_GROWING_FACTOR: f32 = 1.5;
+    const HEAP_GROWTH_FACTOR: f32 = 1.5;
 
-    // On small heaps `last_hp * HEAP_GROWING_FACTOR` will be quite small. To avoid doing redundant
-    // collections in such cases we only check `last_hp * HEAP_GROWING_FACTOR` if at least this
+    // On small heaps `last_hp * HEAP_GROWTH_FACTOR` will be quite small. To avoid doing redundant
+    // collections in such cases we only check `last_hp * HEAP_GROWTH_FACTOR` if at least this
     // much is allocated.
     const SMALL_HEAP_DELTA: u32 = 10 * 1024 * 1024; // 10 MiB
 
-    // Regardless of other parameters (`HEAP_GROWING_FACTOR`, `SMALL_HEAP_DELTA`) we want to do GC
+    // Regardless of other parameters (`HEAP_GROWTH_FACTOR`, `SMALL_HEAP_DELTA`) we want to do GC
     // if `HP` is more than this amount.
     const MAX_HP_FOR_GC: u32 = 3 * 1024 * 1024 * 1024; // 3 GiB
 
     let heap_limit = min(
         max(
-            (LAST_HP as f32 * HEAP_GROWING_FACTOR) as u32,
+            (LAST_HP as f32 * HEAP_GROWTH_FACTOR) as u32,
             SMALL_HEAP_DELTA,
         ),
         MAX_HP_FOR_GC,
