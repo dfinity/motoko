22f474084617a1f8f58a5cb725bc690bd8846b65
Gabor Greif <gabor@dfinity.org>
2021-07-26 13:35:13 +0200

    tweak

--- src/codegen/compile.ml
+++ src/codegen/compile.ml
@@ -5841,7 +5841,7 @@ module FuncDec = struct
       );
 
     (* The (above) upper half of this function must not depend on the
-       get_arg, get_k and get_r parameters, so hide them from above (cute trick) *)
+       get_future, get_k and get_r parameters, so hide them from above (cute trick) *)
     fun get_future get_k get_r ->
       let (set_cb_index, get_cb_index) = new_local env "cb_index" in
       (* store the tuple away *)
