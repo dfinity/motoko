67f28382a14fa1a4f52ac1cbbe00a557beaff9c9
Gabor Greif <gabor@dfinity.org>
2021-07-28 10:06:25 +0200

    reduce noise

--- src/mo_config/internal_error.ml
+++ src/mo_config/internal_error.ml
@@ -1,8 +1,9 @@
 
 let setup_handler () =
-  Printexc.record_backtrace true;
-  Printexc.set_uncaught_exception_handler (fun exn rb ->
+  let open Printexc in
+  record_backtrace true;
+  set_uncaught_exception_handler (fun exn rb ->
     Printf.eprintf "OOPS! You've triggered a compiler bug.\n";
     Printf.eprintf "Please report this at https://github.com/dfinity/motoko/issues/new with the following details:\n\nMotoko %s\n\n" Source_id.banner;
-    Printexc.default_uncaught_exception_handler exn rb
+    default_uncaught_exception_handler exn rb
   );
