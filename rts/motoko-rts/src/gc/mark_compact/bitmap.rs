adcf324867637ba6e196d9d870aa639be4f3fdf1
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-08-10 11:39:28 +0300

    Refactor bitmap api

--- rts/motoko-rts/src/gc/mark_compact/bitmap.rs
+++ rts/motoko-rts/src/gc/mark_compact/bitmap.rs
@@ -5,51 +5,81 @@ use crate::ALLOC;
 use core::alloc::{GlobalAlloc, Layout};
 use core::convert::TryFrom;
 
-/// Current bitmap
-static mut BITMAP_PTR: *mut u8 = core::ptr::null_mut();
+pub struct Bitmap {
+    ptr: *mut u8,
 
-/// Layout of the memory pointed by `BITMAP_PTR`. Used to get bitmap size and for deallocating.
-static mut BITMAP_LAYOUT: Layout = unsafe { Layout::from_size_align_unchecked(0, 1) };
-
-pub unsafe fn alloc_bitmap(heap_size: Bytes<u32>) {
-    // We will have at most this many objects in the heap, each requiring a bit
-    let n_bits = heap_size.to_words().0;
-
-    // Each byte will hold 8 bits.
-    let bitmap_bytes = (n_bits + 7) / 8;
-
-    // Also round allocation up to 8-bytes to make iteration efficient. We want to be able to read
-    // 64 bits in a single read and check as many bits as possible with a single `word != 0`.
-    let bitmap_bytes = Bytes(((bitmap_bytes + 7) / 8) * 8);
-
-    BITMAP_LAYOUT = Layout::from_size_align_unchecked(bitmap_bytes.as_usize(), 1);
-    BITMAP_PTR = ALLOC.alloc(BITMAP_LAYOUT);
-
-    assert!(!BITMAP_PTR.is_null());
+    /// Layout of the memory pointed by `ptr`. Used to get bitmap size and for deallocating.
+    layout: Layout,
 }
 
-pub unsafe fn free_bitmap() {
-    ALLOC.dealloc(BITMAP_PTR, BITMAP_LAYOUT);
-    // TODO: How is `null_mut` compiled to Wasm? 0 is a valid address and won't cause a trap
-    BITMAP_PTR = core::ptr::null_mut();
+impl Bitmap {
+    // NB. Does not take a page allocator, allocates using global allocator
+    pub unsafe fn new(heap_size: Bytes<u32>) -> Bitmap {
+        // We will have at most this many objects in the heap, each requiring a bit
+        let n_bits = heap_size.to_words().0;
+
+        // Each byte will hold 8 bits.
+        let bitmap_bytes = (n_bits + 7) / 8;
+
+        // Also round allocation up to 8-bytes to make iteration efficient. We want to be able to read
+        // 64 bits in a single read and check as many bits as possible with a single `word != 0`.
+        let bitmap_bytes = Bytes(((bitmap_bytes + 7) / 8) * 8);
+
+        let layout = Layout::from_size_align_unchecked(bitmap_bytes.as_usize(), 1);
+        let ptr = ALLOC.alloc(layout);
+
+        assert!(!ptr.is_null());
+
+        Bitmap { ptr, layout }
+    }
+
+    pub unsafe fn free(self) {
+        ALLOC.dealloc(self.ptr, self.layout);
+    }
+
+    pub unsafe fn get_bit(&mut self, idx: u32) -> bool {
+        let byte_idx = idx / 8;
+        let byte = *self.ptr.add(byte_idx as usize);
+        let bit_idx = idx % 8;
+        (byte >> bit_idx) & 0b1 == 0b1
+    }
+
+    pub unsafe fn set_bit(&mut self, idx: u32) {
+        let byte_idx = idx / 8;
+        let byte = *self.ptr.add(byte_idx as usize);
+        let bit_idx = idx % 8;
+        let new_byte = byte | (0b1 << bit_idx);
+        *self.ptr.add(byte_idx as usize) = new_byte;
+    }
+
+    pub unsafe fn bitmap_iter(&self) -> BitmapIter {
+        let bitmap_bytes = self.layout.size();
+
+        debug_assert_eq!(bitmap_bytes % 8, 0);
+
+        let blob_len_64bit_words = bitmap_bytes / 8;
+
+        let current_word = if blob_len_64bit_words == 0 {
+            0
+        } else {
+            *(self.ptr as *const u64)
+        };
+
+        // `try_from` below disappears on Wasm as usize and u32 are the same
+        BitmapIter {
+            bitmap: self,
+            size: u32::try_from(blob_len_64bit_words).unwrap(),
+            current_word_idx: 0,
+            current_word,
+            bits_left: 64,
+        }
+    }
 }
 
-pub unsafe fn get_bit(idx: u32) -> bool {
-    let byte_idx = idx / 8;
-    let byte = *BITMAP_PTR.add(byte_idx as usize);
-    let bit_idx = idx % 8;
-    (byte >> bit_idx) & 0b1 == 0b1
-}
+pub struct BitmapIter<'a> {
+    /// The bitmap we're iterating
+    bitmap: &'a Bitmap,
 
-pub unsafe fn set_bit(idx: u32) {
-    let byte_idx = idx / 8;
-    let byte = *BITMAP_PTR.add(byte_idx as usize);
-    let bit_idx = idx % 8;
-    let new_byte = byte | (0b1 << bit_idx);
-    *BITMAP_PTR.add(byte_idx as usize) = new_byte;
-}
-
-pub struct BitmapIter {
     /// Size of the bitmap, in 64-bit words words. Does not change after initialization.
     size: u32,
 
@@ -66,28 +96,6 @@ pub struct BitmapIter {
     bits_left: u32,
 }
 
-pub unsafe fn iter_bits() -> BitmapIter {
-    let bitmap_bytes = BITMAP_LAYOUT.size();
-
-    debug_assert_eq!(bitmap_bytes % 8, 0);
-
-    let blob_len_64bit_words = bitmap_bytes / 8;
-
-    let current_word = if blob_len_64bit_words == 0 {
-        0
-    } else {
-        *(BITMAP_PTR as *const u64)
-    };
-
-    // `try_from` below disappears on Wasm as usize and u32 are the same
-    BitmapIter {
-        size: u32::try_from(blob_len_64bit_words).unwrap(),
-        current_word_idx: 0,
-        current_word,
-        bits_left: 64,
-    }
-}
-
 /// This value marks the end-of-stream in `BitmapIter`. Using this value instead of `None` for
 /// end-of-stream reduces Wasm instructions executed by ~2.7% in some cases.
 //
@@ -98,7 +106,7 @@ pub unsafe fn iter_bits() -> BitmapIter {
 // stack and can't do GC)
 pub const BITMAP_ITER_END: u32 = 1024 * 1024 * 1024;
 
-impl BitmapIter {
+impl<'a> BitmapIter<'a> {
     /// Returns the next bit, or `BITMAP_ITER_END` if there are no more bits set.
     pub fn next(&mut self) -> u32 {
         debug_assert!(self.current_word_idx <= self.size);
@@ -129,7 +137,7 @@ impl BitmapIter {
                 return BITMAP_ITER_END;
             }
             self.current_word =
-                unsafe { *(BITMAP_PTR as *const u64).add(self.current_word_idx as usize) };
+                unsafe { *(self.bitmap.ptr as *const u64).add(self.current_word_idx as usize) };
             self.bits_left = 64;
         }
     }
