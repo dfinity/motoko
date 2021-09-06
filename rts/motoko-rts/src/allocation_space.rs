3dcbf64794a941c784ef6ae0f56bf279bee3d2fe
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-09-06 16:09:18 +0300

    Implement alloc_blob, alloc_array, stubs for RTS stats

--- rts/motoko-rts/src/allocation_space.rs
+++ rts/motoko-rts/src/allocation_space.rs
@@ -1,8 +1,9 @@
 //! Defines mutator allocation interface
 
 use crate::page_alloc::ic::IcPageAlloc;
+use crate::rts_trap_with;
 use crate::space::Space;
-use crate::types::{Value, Words};
+use crate::types::{size_of, Array, Blob, Bytes, Value, Words, TAG_ARRAY, TAG_BLOB};
 
 // TODO: We should use `MaybeUninit` here, but `MaybeUninit::assume_init_ref` does not exist in the
 // Rust version we're using.. Need to update rustc.
@@ -15,6 +16,35 @@ pub unsafe fn init() {
 #[no_mangle]
 pub unsafe fn alloc_words(n: Words<u32>) -> Value {
     ALLOCATION_SPACE.as_mut().unwrap().alloc_words(n)
+}
+
+/// Helper for allocating blobs
+#[no_mangle]
+pub unsafe fn alloc_blob(size: Bytes<u32>) -> Value {
+    let ptr = alloc_words(size_of::<Blob>() + size.to_words());
+    // NB. Cannot use `as_blob` here as we didn't write the header yet
+    let blob = ptr.get_ptr() as *mut Blob;
+    (*blob).header.tag = TAG_BLOB;
+    (*blob).len = size;
+    ptr
+}
+
+/// Helper for allocating arrays
+#[no_mangle]
+pub unsafe fn alloc_array(len: u32) -> Value {
+    // Array payload should not be larger than half of the memory
+    if len > 1 << (32 - 2 - 1) {
+        // 2 for word size, 1 to divide by two
+        rts_trap_with("Array allocation too large");
+    }
+
+    let skewed_ptr = alloc_words(size_of::<Array>() + Words(len));
+
+    let ptr: *mut Array = skewed_ptr.get_ptr() as *mut Array;
+    (*ptr).header.tag = TAG_ARRAY;
+    (*ptr).len = len;
+
+    skewed_ptr
 }
 
 pub unsafe fn free_and_update_allocation_space(new: Space<IcPageAlloc>) {
--- rts/motoko-rts/src/gc.rs
+++ rts/motoko-rts/src/gc.rs
@@ -2,12 +2,13 @@ pub mod copying;
 pub mod mark_compact;
 
 #[cfg(feature = "ic")]
+use crate::types::Bytes;
+
+#[cfg(feature = "ic")]
 static mut LAST_TOTAL_ALLOC: u32 = 0;
 
 #[cfg(feature = "ic")]
 unsafe fn should_do_gc<P: crate::page_alloc::PageAlloc>(heap: &crate::space::Space<P>) -> bool {
-    use crate::types::Bytes;
-
     use core::cmp::{max, min};
 
     // A factor of last heap size. We allow at most this much allocation before doing GC.
@@ -31,4 +32,32 @@ unsafe fn should_do_gc<P: crate::page_alloc::PageAlloc>(heap: &crate::space::Spa
     );
 
     heap.total_alloc() as u64 >= heap_limit
+}
+
+#[cfg(feature = "ic")]
+#[no_mangle]
+unsafe extern "C" fn get_max_live_size() -> Bytes<u32> {
+    // TODO
+    Bytes(0)
+}
+
+#[cfg(feature = "ic")]
+#[no_mangle]
+unsafe extern "C" fn get_reclaimed() -> Bytes<u64> {
+    // TODO
+    Bytes(0)
+}
+
+#[cfg(feature = "ic")]
+#[no_mangle]
+unsafe extern "C" fn get_total_allocations() -> Bytes<u64> {
+    // TODO
+    Bytes(0)
+}
+
+#[cfg(feature = "ic")]
+#[no_mangle]
+unsafe extern "C" fn get_heap_size() -> Bytes<u32> {
+    // TODO
+    Bytes(0)
 }
