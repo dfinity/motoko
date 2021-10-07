05dc9119555a965b013df8558d44407c4909d599
Gabor Greif <gabor@dfinity.org>
2021-10-07 13:20:19 +0200

    sort cases

--- src/ir_def/construct.ml
+++ src/ir_def/construct.ml
@@ -91,12 +91,12 @@ let primE prim es =
     | ICStableWrite _ -> T.unit
     | IcUrlOfBlob -> T.text
     | ActorOfIdBlob t -> t
+    | BinPrim (t, _) -> t
     | CastPrim (t1, t2) -> t2
     | RelPrim _ -> T.bool
     | SerializePrim _ -> T.blob
     | SystemCyclesAvailablePrim -> T.nat64
     | SystemCyclesAcceptPrim -> T.nat64
-    | BinPrim (t, _) -> t
     | _ -> assert false (* implement more as needed *)
   in
   let effs = List.map eff es in
