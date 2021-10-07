c1cd9cd3e0be7cd57e3bd1394a1e0a648ac0e257
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-10-07 16:39:57 +0300

    Generate package checksums of vendored packages

--- vendor.py
+++ vendor.py
@@ -1,7 +1,13 @@
+import os
 import ssl
 import subprocess
 import sys
 import urllib.request
+
+def sha256sum(path):
+    output = subprocess.run(['sha256sum', path], capture_output=True).stdout
+    print(path + " output: " + str(output))
+    return output.split()[0]
 
 package_name = sys.argv[1]
 package_version = sys.argv[2]
@@ -16,11 +22,17 @@ urllib.request.urlretrieve(package_url, package_file_name)
 
 print(f'Done downloading package: {package_file_name}')
 
+package_checksum = sha256sum(package_file_name)
+
+print(f'Package checksum: {package_checksum}')
+
 subprocess.run(['tar', '-xvf', package_file_name])
 
-checksum_json_file_path = f'{package_name}-{package_version}/.cargo-checksum.json'
+package_dir_path = f'{package_name}-{package_version}'
+
+checksum_json_file_path = f'{package_dir_path}/.cargo-checksum.json'
 
 print(f'Creating file "{checksum_json_file_path}"')
 
 with open(checksum_json_file_path, 'w') as cargo_checksum:
-    cargo_checksum.write('{"files":{}}')
+    cargo_checksum.write('{"package":"{%s}","files":{}}' % package_checksum)
