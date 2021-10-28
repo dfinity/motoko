c6edfb252e5823f6ae1a8e01a630b9aa879a1f46
Gabor Greif <gabor@dfinity.org>
2021-10-28 14:31:17 +0200

    clarify why the range of floats can be on the fast path
    namely that these fit in 31 bits (with sign). Specifically: after shifting
    left by one, followed by an arithmetic shift right by one, we'll arrive
    at the same number.

--- rts/motoko-rts/src/bigint.rs
+++ rts/motoko-rts/src/bigint.rs
@@ -250,6 +250,9 @@ unsafe extern "C" fn bigint_of_int64(j: i64) -> Value {
 #[cfg(feature = "ic")]
 #[no_mangle]
 unsafe extern "C" fn bigint_of_float64(j: f64) -> Value {
+    // handle fast path: these numbers (when rounded towards zero)
+    // can be represented as `Int` without resorting to heap allocation,
+    // i.e. >= 0xc0000000 and <= 0x3fffffff
     if j < 1073741824.0 && j > -1073741825.0 {
         return Value::from_signed_scalar(j as i32);
     }
