64454d3b455ea5f68f05e1fb2ecfc0c504cb881c
Joachim Breitner <mail@joachim-breitner.de>
2021-10-31 15:20:55 +0100

    Set wrong hash

--- nix/drun.nix
+++ nix/drun.nix
@@ -14,7 +14,7 @@ pkgs:
       # To automate this, try running the following in the nix/ directory
       # nix run -f https://github.com/Mic92/nix-update/archive/master.tar.gz -c nix-update --version=skip drun
 
-      cargoSha256 = "0656lxdlr05cjkla1blvpqlxywk7shasiwmycz10nqykdrs4gfgf";
+      cargoSha256 = "0656lxdlr05cjkla1blvpqlxywk7sha0iwmycz10nqykdrs4gfgf";
 
       nativeBuildInputs = with pkgs; [
         pkg-config
