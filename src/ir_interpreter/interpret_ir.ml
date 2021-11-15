dbc42c331305a7d48361634abc39cfce3004e97f
Gabor Greif <gabor@dfinity.org>
2021-11-15 15:29:52 +0100

    ooops
    the hash needs to be compared in the regular way

--- src/ir_interpreter/interpret_ir.ml
+++ src/ir_interpreter/interpret_ir.ml
@@ -363,7 +363,7 @@ and interpret_exp_mut env exp (k : V.value V.cont) =
       | SameReference, [v1; v2] ->
         k (V.Bool (v1 == v2))
       | SameVariantTag _, [v1; v2] ->
-        k (V.Bool (fst (V.as_variant v1) == fst (V.as_variant v2)))
+        k (V.Bool (fst (V.as_variant v1) = fst (V.as_variant v2)))
       | BreakPrim id, [v1] -> find id env.labs v1
       | RetPrim, [v1] -> Option.get env.rets v1
       | ThrowPrim, [v1] -> Option.get env.throws v1
