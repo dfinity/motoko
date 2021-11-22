d4b8b367bdd88ea64e546ffe5bb9161819f710cb
Claudio Russo <claudio@dfinity.org>
2021-11-22 13:46:46 +0000

    revise add-tag script for dfx 0.8.4 onwards

--- add-tags.sh
+++ add-tags.sh
@@ -35,6 +35,12 @@ do
   git -C "$3" tag -f "dfx-$tag" "$base_rev"
 done
 
+read -p "Are you sure you want to push these tags? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
 
 git -C "$2" push --tags
 git -C "$3" push --tags
