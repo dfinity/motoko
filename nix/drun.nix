2222570d5801925728c7ed5af5fb03928bcd10dd
Nix hash updater <nobody@example.com>
2021-12-08 09:09:21 +0000

    Updating nix hashes

--- nix/drun.nix
+++ nix/drun.nix
@@ -14,7 +14,7 @@ pkgs:
       # To automate this, try running the following in the nix/ directory
       # nix run -f https://github.com/Mic92/nix-update/archive/master.tar.gz -c nix-update --version=skip drun
 
-      cargoSha256 = "sha256-7rlHdG7TYwvCZ77yqBXUZ3LfKb6brqDolKyATFunphg=";
+      cargoSha256 = "sha256-keJ3V5LhaEPprkX/xjYKAZlML9uaWMfBHu8uopsIStQ=";
 
       nativeBuildInputs = with pkgs; [
         pkg-config
