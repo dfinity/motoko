bc2c34039294733ee85a80e8d31e844ea1b2f3c2
Gabor Greif <gabor@dfinity.org>
2021-12-13 21:52:03 +0100

    Transition to `llvm_13` (#2959)
    * transition to llvm_13
    
    * try the staging branch
    
    * bump
    
    * try master branch
    
    * darwin seems fixed
    
    * refresher

--- default.nix
+++ default.nix
@@ -25,11 +25,11 @@ let haskellPackages = nixpkgs.haskellPackages.override {
     }; in
 let
   rtsBuildInputs = with nixpkgs; [
-    # pulls in clang (wrapped) and clang-12 (unwrapped)
-    llvmPackages_12.clang
+    # pulls in clang (wrapped) and clang-13 (unwrapped)
+    llvmPackages_13.clang
     # pulls in wasm-ld
-    llvmPackages_12.lld
-    llvmPackages_12.bintools
+    llvmPackages_13.lld
+    llvmPackages_13.bintools
     rustc-nightly
     cargo-nightly
     wasmtime
@@ -42,16 +42,16 @@ let
   llvmEnv = ''
     # When compiling to wasm, we want to have more control over the flags,
     # so we do not use the nix-provided wrapper in clang
-    export WASM_CLANG="clang-12"
+    export WASM_CLANG="clang-13"
     export WASM_LD=wasm-ld
     # because we use the unwrapped clang, we have to pass in some flags/paths
     # that otherwise the wrapped clang would take care for us
-    export WASM_CLANG_LIB="${nixpkgs.llvmPackages_12.clang-unwrapped.lib}"
+    export WASM_CLANG_LIB="${nixpkgs.llvmPackages_13.clang-unwrapped.lib}"
 
     # When compiling natively, we want to use `clang` (which is a nixpkgs
     # provided wrapper that sets various include paths etc).
     # But for some reason it does not handle building for Wasm well, so
-    # there we use plain clang-12. There is no stdlib there anyways.
+    # there we use plain clang-13. There is no stdlib there anyways.
     export CLANG="${nixpkgs.clang_12}/bin/clang"
   '';
 in
