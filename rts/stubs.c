c8dd87e1a075f346467231fd02a4097a03964b0d
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-12-15 14:36:30 +0300

    Update comments

--- rts/stubs.c
+++ rts/stubs.c
@@ -4,14 +4,15 @@
 __attribute__ ((noreturn)) void rts_trap(char *msg, uint32_t len);
 
 // Stubbery for musl. Functions below are used by some of the musl functions we
-// compile. musl implementations use system calls. We don't want them to be
-// used and we can't provide sensible implementations for them, so we provide
-// stubs below that just trap when called.
+// compile.
+
+// musl implementation uses system calls so we provide our own implementation
+void abort(void) { rts_trap("abort", 5); }
+
+// Functions below should not be called so we trap when they're called.
 
 int wctomb(char *s, wchar_t wc) { rts_trap("wctomb", 6); }
 
 int fputs(const char *s, void *f) { rts_trap("fputs", 5); }
-
-void abort(void) { rts_trap("abort", 5); }
 
 char *strerror(int e) { rts_trap("strerror", 8); }
