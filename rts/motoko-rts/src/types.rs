45517175655a4428efe9b8d45d41ee27fa0d89da
Gabor Greif <gabor@dfinity.org>
2021-12-15 16:55:11 +0100

    fixups

--- rts/motoko-rts/src/types.rs
+++ rts/motoko-rts/src/types.rs
@@ -180,14 +180,14 @@ impl PtrOrScalar {
 impl Value {
     /// Create a value from a pointer
     pub const fn from_ptr(ptr: usize) -> Self {
-        // Cannot use `assert_eq` in const yet, so using `assert`
+        // Cannot use `debug_assert_eq` in const yet, so using `debug_assert`
         debug_assert!(ptr & 0b1 == 0b0);
         Value(skew(ptr) as u32)
     }
 
     /// Create a value from a scalar
     pub const fn from_scalar(value: u32) -> Self {
-        // Cannot use `assert_eq` in const yet, so using `assert`
+        // Cannot use `debug_assert_eq` in const yet, so using `debug_assert`
         debug_assert!(value >> 31 == 0);
         Value(value << 1)
     }
