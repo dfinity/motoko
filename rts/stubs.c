a2704727bf5849f0ab1d1c9ce75aa56e545466a3
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-12-15 14:20:01 +0300

    RTS: trap in stubbed libc functions
    See the comments in the code.
    
    Fixes #2248

--- rts/stubs.c
+++ rts/stubs.c
@@ -3,15 +3,15 @@
 
 __attribute__ ((noreturn)) void rts_trap(char *msg, uint32_t len);
 
-/* Stubbery for musl */
+// Stubbery for musl. Functions below are used by some of the musl functions we
+// compile. musl implementations use system calls. We don't want them to be
+// used and we can't provide sensible implementations for them, so we provide
+// stubs below that just trap when called.
 
-// NOTE (osa): This function previously did not return even though it should
-// (return type declared as `int`). Returning `0` here for now but we should
-// probably make sure is never used (maybe trap).
-int wctomb(char *s, wchar_t wc) { *s = '\0'; return 0; }
+int wctomb(char *s, wchar_t wc) { rts_trap("wctomb", 6); }
 
-int fputs(const char *s, void *f) { return 0; }
+int fputs(const char *s, void *f) { rts_trap("fputs", 5); }
 
 void abort(void) { rts_trap("abort", 5); }
 
-char *strerror(int e) { return ""; }
+char *strerror(int e) { rts_trap("strerror", 8); }
