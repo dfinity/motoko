6deb20772f98de3619e2ddfadf8317af7ced0fbe
Joachim Breitner <mail@joachim-breitner.de>
2022-01-02 19:47:55 +0100

    Maybe v16 builds on osx?

--- default.nix
+++ default.nix
@@ -318,7 +318,7 @@ rec {
         src = test_src dir;
         buildInputs =
           deps ++
-          (with nixpkgs; [ wabt bash perl getconf moreutils nodejs-17_x sources.esm ]) ++
+          (with nixpkgs; [ wabt bash perl getconf moreutils nodejs-16_x sources.esm ]) ++
           [ filecheck
             wasmtime
           ] ++
@@ -465,7 +465,7 @@ rec {
         buildInputs = commonBuildInputs nixpkgs ++ [
           nixpkgs.ocamlPackages.js_of_ocaml
           nixpkgs.ocamlPackages.js_of_ocaml-ppx
-          nixpkgs.nodejs-17_x
+          nixpkgs.nodejs-16_x
         ];
         buildPhase = ''
           patchShebangs .
