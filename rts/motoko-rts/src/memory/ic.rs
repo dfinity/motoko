f6d70fb4a289e31be8aafbab4bb2b9c2eec539f4
Claudio Russo <claudio@dfinity.org>
2022-01-29 19:45:57 +0000

    remove redundant cast

--- rts/motoko-rts/src/memory/ic.rs
+++ rts/motoko-rts/src/memory/ic.rs
@@ -95,7 +95,7 @@ unsafe fn grow_memory(ptr: u64) {
     debug_assert!(ptr <= 2 * u64::from(core::u32::MAX));
     let page_size = u64::from(WASM_PAGE_SIZE.as_u32());
     let total_pages_needed = ((ptr + page_size - 1) / page_size) as usize;
-    let current_pages = wasm32::memory_size(0) as usize;
+    let current_pages = wasm32::memory_size(0);
     if total_pages_needed > current_pages {
         if wasm32::memory_grow(0, total_pages_needed - current_pages) == core::usize::MAX {
             rts_trap_with("Cannot grow memory");
