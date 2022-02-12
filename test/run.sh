22ebffeecd535fb74560f4a86fd9314af345253d
Gabor Greif <gabor@dfinity.org>
2022-02-12 13:58:00 +0100

    normalise

--- test/run.sh
+++ test/run.sh
@@ -81,6 +81,7 @@ function normalize () {
     sed 's/trap at 0x[a-f0-9]*/trap at 0x___:/g' |
     sed 's/^\(         [0-9]\+:\).*!/\1 /g' | # wasmtime backtrace locations
     sed 's/Ignore Diff:.*/Ignore Diff: (ignored)/ig' |
+    sed 's/Motoko (source .*)/Motoko (source XXX)/ig' |
     sed 's/compiler (source .*)/compiler (source XXX)/ig' |
     # Normalize canister id prefixes in debug prints
     sed 's/\[Canister [0-9a-z\-]*\]/debug.print:/g' |
