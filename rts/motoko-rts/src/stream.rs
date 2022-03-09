e85f1c667b02c08f9778755244a851ebb26ec100
Gabor Greif <gabor@dfinity.org>
2022-03-09 13:19:06 +0100

    flesh it out a bit more

--- rts/motoko-rts/src/stream.rs
+++ rts/motoko-rts/src/stream.rs
@@ -1,12 +1,14 @@
 //! The implementation of streaming serialisation
 //!
-//! When serialising Motoko stable variables to stable memory we used to first completely.
+//! When serialising Motoko stable variables to stable memory we used to first completely
 //! fill up an in-heap buffer and then copy that wholesale into stable memory. This can be
 //! disadvategous for two reasons:
 //!  - double copying
-//!  - heap congestion (especially for the compactig collector)
+//!  - heap congestion (especially for the compacting collector)
 //!
-//! Instead now we'll 
+//! Instead now we'll only allocate a small(ish) blob that will serve as a temporary storage
+//! for bytes in transit, while bigger chunks will flush this staging area before being written
+//! directly to destination.
 //!
 //!
 
@@ -18,7 +20,9 @@
 //
 // We reuse the opaque nature of blobs (to Motoko) and stick Rust-related information
 // into the leading bytes:
-// - tag and len are blob metadata
-// - ptr64 and limit64 are the next and past-end pointers into stable memory
-// - flusher is the 
+// - `tag` and `len` are blob metadata
+// - `ptr64` and `limit64` are the next and past-end pointers into stable memory
+// - `filled` and `cache` are the number of bytes consumed from the blob, and the
+//   staging area of the stream, respectively
+// - `flusher` is the function to be called when `len - filled` approaches zero.
 
