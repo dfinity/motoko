3d21b9568ce57d4fd33fba30aa771b710a5396d0
Matthew Hammer <matthew.hammer@gmail.com>
2022-04-01 15:37:17 -0600

    refine suggestion; revert another earlier change.

--- src/lowering/desugar.ml
+++ src/lowering/desugar.ml
@@ -71,7 +71,7 @@ and exp' at note = function
   | S.ToCandidE (ot, es) ->
     begin match !ot with
     | T.Tup ts1 ->
-      I.PrimE (I.SerializePrim ts1, [tupE (exps es)])
+      I.PrimE (I.SerializePrim [T.Tup ts1], [tupE (exps es)])
     | _ -> assert false
     end
   | S.FromCandidE (ot, e) ->
@@ -156,11 +156,10 @@ and exp' at note = function
       I.PrimE (I.CastPrim (T.seq ts1, T.seq ts2), [exp e])
     | _ -> assert false
     end
-  | S.CallE ({it=S.AnnotE ({it=S.PrimE "serialize";_}, _);note;_}, _,
-             {it=S.TupE es; _}) ->
+  | S.CallE ({it=S.AnnotE ({it=S.PrimE "serialize";_}, _);note;_}, _, e) ->
     begin match note.S.note_typ with
     | T.Func (T.Local, T.Returns, [], ts1, ts2) ->
-      I.PrimE (I.SerializePrim ts1, exps es)
+      I.PrimE (I.SerializePrim ts1, [exp e])
     | _ -> assert false
     end
   | S.CallE ({it=S.AnnotE ({it=S.PrimE "deserialize";_}, _);note;_}, _, e) ->
