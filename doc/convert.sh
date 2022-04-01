cd8186e580ecde117e13659adda4110595a40594
Claudio Russo <claudio@dfinity.org>
2022-04-01 14:24:02 +0100

    convert from adoc to markdown

---
+++ doc/convert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+for f in $(find $1 -name '*.adoc'); do
+  echo $f
+  asciidoctor -b docbook -a leveloffset=+1 -o $2/$(basename $f .adoc).xml $f
+  pandoc -t gfm  --no-highlight --wrap=none -f docbook $2/$(basename $f .adoc).xml \
+  > $2/$(basename $f .adoc).md || true
+  echo $f
+done
