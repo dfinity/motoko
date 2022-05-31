dfinity/motoko

commit 6887b32bd957cf2ee4eb226e47aa2161c055d562

bugfix: fix issue #3265 (typing of  annotated, recursive records) (#3268)
Fixes #3265 (adding repro from #3262 as regression test)

src/lowering/desugar.ml
@@ -534,23 +534,27 @@ and build_obj at s self_id dfs obj_typ =
       let self = var self_id.it obj_typ in
       (letE self e (varE self)).it
 
-and exp_field ef =
+and exp_field obj_typ ef =
+  let _, fts = T.as_obj_sub [] obj_typ in
   let S.{mut; id; exp = e} = ef.it in
-  let typ = e.note.S.note_typ in
-  match mut.it with
-  | S.Var ->
-    let id' = fresh_var id.it (T.Mut typ) in
-    let d = varD id' (exp e) in
-    let f = { it = { I.name = id.it; I.var = id_of_var id'}; at = no_region; note = T.Mut typ } in
-    (d, f)
-  | S.Const ->
-    let id' = fresh_var id.it typ in
-    let d = letD id' (exp e) in
-    let f = { it = { I.name = id.it; I.var = id_of_var id'}; at = no_region; note = typ } in
+  let typ =
+    match T.lookup_val_field_opt id.it fts with
+    | Some typ -> typ
+    | None -> e.note.S.note_typ
+  in
+  let id' = fresh_var id.it typ in
+  let d = match mut.it with
+    | S.Var -> varD id' (exp e)
+    | S.Const -> letD id' (exp e) in
+  let f =
+    { it = { I.name = id.it; I.var = id_of_var id'};
+      at = no_region;
+      note = typ }
+  in
     (d, f)
 
 and obj obj_typ efs =
-  let (ds, fs) = List.map exp_field efs |> List.split in
+  let (ds, fs) = List.map (exp_field obj_typ) efs |> List.split in
   let obj_e = newObjE T.Object fs obj_typ in
   I.BlockE(ds, obj_e)
 

test/run/issue3265.mo
@@ -1,0 +1,5 @@
+type List<X> = ?(X, List<X>);
+func nil<X>() : List<X> = null;
+func sub(x:List<None>):List<Nat>{x};
+let rec1 : { var myList : Int } = { var myList = (0 : Nat)};
+let rec2 : { var myList : List<Nat> } = { var myList = nil<None>() };

commit d2f2f531549988acf9190083d71db7e9d8e76e27

bugfix: fix bug introduced in #3268 (lowering of recursive records) (#3277)
Fix bug that snuck in with over-eager refactoring in #3268

src/lowering/desugar.ml
@@ -537,20 +537,26 @@ and build_obj at s self_id dfs obj_typ =
 and exp_field obj_typ ef =
   let _, fts = T.as_obj_sub [] obj_typ in
   let S.{mut; id; exp = e} = ef.it in
-  let typ =
-    match T.lookup_val_field_opt id.it fts with
-    | Some typ -> typ
-    | None -> e.note.S.note_typ
-  in
-  let id' = fresh_var id.it typ in
-  let d = match mut.it with
-    | S.Var -> varD id' (exp e)
-    | S.Const -> letD id' (exp e) in
-  let f =
-    { it = { I.name = id.it; I.var = id_of_var id'};
-      at = no_region;
-      note = typ }
-  in
+  match mut.it with
+  | S.Var ->
+    let typ = match T.lookup_val_field_opt id.it fts with
+      | Some typ -> typ
+      | None -> T.Mut e.note.S.note_typ
+    in
+    assert (T.is_mut typ);
+    let id' = fresh_var id.it typ in
+    let d = varD id' (exp e) in
+    let f = { it = { I.name = id.it; I.var = id_of_var id'}; at = no_region; note = typ } in
+    (d, f)
+  | S.Const ->
+    let typ = match T.lookup_val_field_opt id.it fts with
+      | Some typ -> typ
+      | None -> e.note.S.note_typ
+    in
+    assert (not (T.is_mut typ));
+    let id' = fresh_var id.it typ in
+    let d = letD id' (exp e) in
+    let f = { it = { I.name = id.it; I.var = id_of_var id'}; at = no_region; note = typ } in
     (d, f)
 
 and obj obj_typ efs =

test/fail/issue-3265-fail.mo
@@ -1,0 +1,12 @@
+// simplest example of failure (negative version)
+let rec1 : { var x : Nat } = { var x = (0 : Int)}; // reject
+let rec1a : { var x : Int } = { x = (0 : Nat)}; // reject
+let rec1b : { x : Int } = { var x = (0 : Nat)}; // reject
+
+// repro from #3265 (negative version)
+type List<X> = ?(X, List<X>);
+func nil<X>() : List<X> = null;
+func sub(x : List<None>) : List<Nat>{x};
+let rec2 : { var myList : List<None> } = { var myList = nil<Nat>() }; // reject
+let rec2a : { myList : List<Nat> } = { var myList = nil<None>() }; // reject
+let rec2b : { var myList : List<Nat> } = { myList = nil<None>() }; // reject

test/fail/ok/issue-3265-fail.tc.ok
@@ -1,0 +1,20 @@
+issue-3265-fail.mo:2.41-2.48: type error [M0096], expression of type
+  Int
+cannot produce expected type
+  Nat
+issue-3265-fail.mo:3.33-3.46: type error [M0149], expected mutable 'var' field x of type
+  Int
+but found immutable field (insert 'var'?)
+issue-3265-fail.mo:4.29-4.46: type error [M0150], expected immutable field x of type
+  Int
+but found mutable 'var' field (delete 'var'?)
+issue-3265-fail.mo:10.57-10.67: type error [M0096], expression of type
+  List<Nat> = ?(Nat, List<Nat>)
+cannot produce expected type
+  ?(None, List<None>)
+issue-3265-fail.mo:11.40-11.64: type error [M0150], expected immutable field myList of type
+  List<Nat>
+but found mutable 'var' field (delete 'var'?)
+issue-3265-fail.mo:12.44-12.64: type error [M0149], expected mutable 'var' field myList of type
+  List<Nat>
+but found immutable field (insert 'var'?)

test/fail/ok/issue-3265-fail.tc.ret.ok
@@ -1,0 +1,1 @@
+Return code 1

test/run/issue3265.mo
@@ -1,5 +1,8 @@
+// simplest example of failure
+let rec1 : { var x : Int } = { var x = (0 : Nat)};
+
+// repro from #3265
 type List<X> = ?(X, List<X>);
 func nil<X>() : List<X> = null;
-func sub(x:List<None>):List<Nat>{x};
-let rec1 : { var myList : Int } = { var myList = (0 : Nat)};
+func sub(x : List<None>) : List<Nat>{x};
 let rec2 : { var myList : List<Nat> } = { var myList = nil<None>() };