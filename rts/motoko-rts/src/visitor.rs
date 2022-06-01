d10949a956c08f3bf11d5e0eb383ac80c48ce278
Gabor Greif <gabor@dfinity.org>
2022-06-01 22:00:04 +0200

    explain `visit_pointer_fields`

--- rts/motoko-rts/src/visitor.rs
+++ rts/motoko-rts/src/visitor.rs
@@ -3,6 +3,23 @@ use crate::types::*;
 
 /// A visitor that passes field addresses of fields with pointers to dynamic heap to the given
 /// callback
+///
+/// Arguments:
+///
+/// * `ctx`: any context passed to the `visit_*` callbacks
+/// * `obj`: the heap object to be visited (note: its heap tag may be invalid)
+/// * `tag`: the heap object's logical tag (or start of array object's suffix slice)
+/// * `heap_base`: start address of the dynamic heap
+/// * `visit_ptr_field`: callback for individual fields
+/// * `visit_field_range`: callback for determining the suffix slice
+///   Arguments:
+///   * `&mut C`: passed context
+///   * `u32`: start index of array suffix slice being visited
+///   * `*mut Array`: home object of the slice (its heap tag may be invalid)
+///   Returns:
+///   * `u32`: start of the suffix slice of fields not to be passed to `visit_ptr_field`;
+///            it is the callback's responsibility to deal with the spanned slice
+
 pub unsafe fn visit_pointer_fields<C, F, G>(
     ctx: &mut C,
     obj: *mut Obj,
