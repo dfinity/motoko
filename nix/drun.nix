3b2d3df12299543bb0623d42ce041bd6ff3579b0
Nix hash updater <nobody@example.com>
2022-06-12 00:19:24 +0000

    Updating nix hashes

--- nix/drun.nix
+++ nix/drun.nix
@@ -15,7 +15,7 @@ pkgs:
       # installed. You will normally not be bothered to perform
       # the command therein manually.
 
-      cargoSha256 = "sha256-EbYM5DxJILstvNjQS4Jr9RvCxsT71XfcZ4lWzBiJaTU=";
+      cargoSha256 = "sha256-fzFkMJKUErbURpVVnt08krCfK4LjQ9aoSs7AeJR5Xtc=";
 
       nativeBuildInputs = with pkgs; [
         pkg-config
