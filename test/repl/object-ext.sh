14f22fa674b38a06572d1fdceeb1e8ecff75bfe2
Gabor Greif <gabor@dfinity.org>
2022-06-24 14:44:01 +0200

    exercise `module` as base

--- test/repl/object-ext.sh
+++ test/repl/object-ext.sh
@@ -2,4 +2,7 @@
 # Tests that fields completely override base fields
 moc -i <<__END__
 { x = -25 in { x = 42 : Nat } };
+
+module X {public let a = 7};
+{ y = -25 in { y = 3; z = "H" } and X };
 __END__
