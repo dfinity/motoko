ffc4fe6b971a8eb0b3447f3ed5d5ab8640c5177b
Claudio Russo <claudio@dfinity.org>
2022-07-20 17:31:11 +0100

    fix repl tests

--- test/repl/object-ext.sh
+++ test/repl/object-ext.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Tests that fields completely override base fields
 moc -i <<__END__
-{ x = -25 in { x = 42 : Nat } };
+{ { x = 42 : Nat }  |  x = -25 };
 
 module X {public let a = 7};
-{ y = -25 in { y = 3; z = "H" } and X };
+{ { y = 3; z = "H" } and X | y = -25 };
 __END__
