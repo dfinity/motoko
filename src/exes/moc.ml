3712c386aa7c18b2e671bd462a01ac260ae504b3
Gabor Greif <gabor@dfinity.org>
2022-08-22 14:45:07 +0200

    Update src/exes/moc.ml
    Co-authored-by: Claudio Russo <claudio@dfinity.org>

--- src/exes/moc.ml
+++ src/exes/moc.ml
@@ -152,7 +152,7 @@ let argspec = [
 
   "--max-stable-pages",
   Arg.Set_int Flags.max_stable_pages,
-  "<n>  set maximum number of pages available for library `ExperimentStableMemory.mo` (default " ^ (Int.to_string Flags.max_stable_pages_default) ^ ")";
+  "<n>  set maximum number of pages available for library `ExperimentalStableMemory.mo` (default " ^ (Int.to_string Flags.max_stable_pages_default) ^ ")";
 
   "--experimental-references",
   Arg.Unit (fun () -> Flags.experimental_references := true),
