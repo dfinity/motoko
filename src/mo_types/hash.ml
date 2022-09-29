15ec82ca91c1d8775a0839775484a4dd46d726fe
Claudio Russo <claudio@dfinity.org>
2022-09-29 12:44:05 +0100

    use string indexing syntax

--- src/mo_types/hash.ml
+++ src/mo_types/hash.ml
@@ -3,11 +3,10 @@ We hash field names using the following hash function,
 which results in values [0..2^31-1]
 *)
 
-(* TODO: optimise *)
 let hash : string -> int32 = fun s ->
   let open Int32 in
   let sum = ref zero in
   for i = 0 to String.length s -1 do
-    sum := add (mul (!sum) (of_int 223)) (of_int (Char.code (String.get s i)))
+    sum := add (mul (!sum) (of_int 223)) (of_int (Char.code (s.[i])))
   done;
   logand (shift_right_logical minus_one 1) (!sum)
