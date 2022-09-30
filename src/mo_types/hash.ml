3f59c891f5cd70dcf801719545d1fe1f2d8e242c
Claudio Russo <claudio@dfinity.org>
2022-09-30 13:05:29 +0100

    Update src/mo_types/hash.ml

--- src/mo_types/hash.ml
+++ src/mo_types/hash.ml
@@ -6,7 +6,7 @@ which results in values [0..2^31-1]
 let hash : string -> int32 = fun s ->
   let open Int32 in
   let sum = ref zero in
-  for i = 0 to String.length s -1 do
+  for i = 0 to String.length s - 1 do
     sum := add (mul (!sum) (of_int 223)) (of_int (Char.code (s.[i])))
   done;
   logand (shift_right_logical minus_one 1) (!sum)
