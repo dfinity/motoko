a740be7541b64a5127f81eaabe0453360d7474f2
luc-blaeser <luc.blaeser@dfinity.org>
2022-10-18 10:43:22 +0200

    minor cleanup

--- rts/motoko-rts/src/gc/generational.rs
+++ rts/motoko-rts/src/gc/generational.rs
@@ -210,7 +210,7 @@ impl<'a, M: Memory> GenerationalGC<'a, M> {
         }
 
         if self.is_compaction_beneficial() {
-            self.thread_backward_phase();
+            self.thread_initial_phase();
             self.move_phase();
         }
 
@@ -339,7 +339,7 @@ impl<'a, M: Memory> GenerationalGC<'a, M> {
         }
     }
 
-    unsafe fn thread_backward_phase(&mut self) {
+    unsafe fn thread_initial_phase(&mut self) {
         self.thread_all_backward_pointers();
 
         // For static roots, also forward pointers are threaded.
