54fd3f4d9d06ee08a900b77d5bd48eb34073940a
Gabor Greif <gabor@dfinity.org>
2022-11-09 19:30:25 +0100

    only do the defines for M1

--- nix/drun.nix
+++ nix/drun.nix
@@ -21,18 +21,20 @@ pkgs:
       cd ../drun-vendor.tar.gz
       ls -l librocksdb-sys/build.rs
       patch librocksdb-sys/build.rs << EOF
-@@ -118,6 +118,8 @@
+@@ -118,6 +118,10 @@
          config.define("OS_MACOSX", Some("1"));
          config.define("ROCKSDB_PLATFORM_POSIX", Some("1"));
          config.define("ROCKSDB_LIB_IO_POSIX", Some("1"));
-+        config.define("isSSE42()", Some("0"));
-+        config.define("isPCLMULQDQ()", Some("0"));
++        if target.contains("aarch64") {
++            config.define("isSSE42()", Some("0"));
++            config.define("isPCLMULQDQ()", Some("0"));
++        }
      } else if target.contains("android") {
          config.define("OS_ANDROID", Some("1"));
          config.define("ROCKSDB_PLATFORM_POSIX", Some("1"));
 EOF
 
-      sed -i -e s/08d86b53188dc6f15c8dc09d8aadece72e39f145e3ae497bb8711936a916335a/b099df5e4401ea37f9c04060cfc19a9f2d78e8f3ff90ce80377ad6f0164532c1/g librocksdb-sys/.cargo-checksum.json
+      sed -i -e s/08d86b53188dc6f15c8dc09d8aadece72e39f145e3ae497bb8711936a916335a/536e44802de57cc7d3690c90c80f154f770f48e82b82756c36443b8b47c9b5e7/g librocksdb-sys/.cargo-checksum.json
 
       cd -
       '';
