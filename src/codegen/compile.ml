f5dd884ad9eb11eb7dc2d2334d6fd48983b12b32
luc-blaeser <luc.blaeser@dfinity.org>
2022-11-10 11:29:09 +0100

    dummy change

--- src/codegen/compile.ml
+++ src/codegen/compile.ml
@@ -10,7 +10,7 @@ grouping. Every module has a high-level prose comment explaining the concept;
 this keeps documentation close to the code (a lesson learned from Simon PJ).
 *)
 
-open Ir_def
+ open Ir_def
 open Mo_values
 open Mo_types
 open Mo_config
