ade9aa0b0cc055b241b09f5bb622a90bfb48f9f9
luc-blaeser <luc.blaeser@dfinity.org>
2022-11-16 10:52:01 +0100

    Update run.sh

--- crash/run.sh
+++ crash/run.sh
@@ -1,6 +1,14 @@
 ulimit -c unlimited
 cargo run
-find / -name core.*
+echo "search in local directory"
+pwd
+find -name core.*
+echo "search in /var/lib/systemd/coredump"
+ls -la /var/lib/systemd/coredump
+echo "search in /var/crash"
+ls -la /var/lib/systemd/coredump
+echo "search in /var/lib/apport/coredump"
+ls -la /var/lib/apport/coredump
 if [ -e core.* ]
 then
     gzip core.*
