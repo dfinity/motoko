39b23fd5c9b780a9b2ba7cebbde63a57ba08acf1
luc-blaeser <luc.blaeser@dfinity.org>
2022-11-16 13:38:58 +0100

    Update run.sh

--- crash/run.sh
+++ crash/run.sh
@@ -3,6 +3,10 @@ cargo build
 pwd
 target/debug/crash
 ls -lar /*
+echo "List /var/"
+ls -lar /var/
+echo "List /var/lib/systemd/coredump"
+ls -lar /var/lib/systemd/coredump
 if [ -e core.* ]
 then
     gzip core.*
