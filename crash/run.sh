d55a954a0d7354304d82bea7f209da6d0f3ac301
luc-blaeser <luc.blaeser@dfinity.org>
2022-11-17 13:13:49 +0100

    Update run.sh

--- crash/run.sh
+++ crash/run.sh
@@ -5,6 +5,7 @@ cat /proc/sys/kernel/core_pattern
 cargo run
 ls -la
 echo "Core dump copied to /build/dumps"
+mkdir -p /build/dumps
 cp core.* /build/dumps
 ls -la /build/dumps
 echo "Core dump output location"
