273833f32558c75c7f396744bd92f5b14f6cdd52
Gabor Greif <gabor@dfinity.org>
2022-11-21 23:10:41 +0100

    format

--- src/languageServer/imports.ml
+++ src/languageServer/imports.ml
@@ -9,7 +9,10 @@ type import = string * string
 
 let parse_with mode lexbuf parser =
   let tokenizer, _ = Lexer.tokenizer mode lexbuf in
-  Ok (Parsing.parse Lexer_lib.mode 0 (parser lexbuf.Lexing.lex_curr_p) tokenizer lexbuf)
+  Ok
+    (Parsing.parse Lexer_lib.mode 0
+       (parser lexbuf.Lexing.lex_curr_p)
+       tokenizer lexbuf)
 
 let parse_string s =
   try
