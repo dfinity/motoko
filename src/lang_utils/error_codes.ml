21d8a699d5843822c0a98d73d7657c47a14671af
Kento Sugama <kento.sugama@dfinity.org>
2022-11-21 16:08:02 -0800

    fix error code

--- src/lang_utils/error_codes.ml
+++ src/lang_utils/error_codes.ml
@@ -184,4 +184,5 @@ let error_codes : (string * string option) list =
     "M0178", None; (* Bases of record extensions must be either objects or modules *)
     "M0179", None; (* Mutable (var) fields from bases must be overwritten explicitly *)
     "M0180", None; (* Shared function has unexpected type parameters *)
+    "M0181", Some([%blob "error_codes/M0181.adoc"]); (* Let else binding has a non-None type else block *)
   ]
