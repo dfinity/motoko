3214be62697e65e143abc67a08ebb284a173484f
luc-blaeser <luc.blaeser@dfinity.org>
2022-12-12 16:05:00 +0100

    Revert "switch for extra memory sanity checks in generational GC"
    This reverts commit 96d2c920135c6d7f1f078541e779a7b6afc0dfa7.

--- rts/motoko-rts/src/gc/generational.rs
+++ rts/motoko-rts/src/gc/generational.rs
@@ -7,7 +7,7 @@
 
 pub mod mark_stack;
 pub mod remembered_set;
-#[cfg(feature = "memory_check")]
+#[cfg(debug_assertions)]
 mod sanity_checks;
 pub mod write_barrier;
 
@@ -51,13 +51,13 @@ unsafe fn generational_gc<M: Memory>(mem: &mut M) {
     };
     let strategy = decide_strategy(&heap.limits);
 
-    #[cfg(feature = "memory_check")]
+    #[cfg(debug_assertions)]
     let forced_gc = strategy.is_none();
 
     let strategy = strategy.unwrap_or(Strategy::Young);
     let mut gc = GenerationalGC::new(heap, strategy);
 
-    #[cfg(feature = "memory_check")]
+    #[cfg(debug_assertions)]
     sanity_checks::verify_snapshot(&gc.heap, false);
 
     gc.run();
@@ -67,7 +67,7 @@ unsafe fn generational_gc<M: Memory>(mem: &mut M) {
     update_statistics(&old_limits, new_limits);
     update_strategy(strategy, new_limits);
 
-    #[cfg(feature = "memory_check")]
+    #[cfg(debug_assertions)]
     if !forced_gc {
         sanity_checks::check_memory(&gc.heap.limits, &gc.heap.roots);
         sanity_checks::take_snapshot(&mut gc.heap);
