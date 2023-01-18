89978ae6905ad426016d5e8f6c06155df783783e
luc-blaeser <luc.blaeser@dfinity.org>
2023-01-18 17:26:41 +0100

    Optimize barrier

--- rts/motoko-rts/src/gc/incremental/barriers.rs
+++ rts/motoko-rts/src/gc/incremental/barriers.rs
@@ -5,7 +5,7 @@
 use motoko_rts_macros::ic_mem_fn;
 
 use crate::{
-    gc::incremental::pre_write_barrier,
+    gc::incremental::{pre_write_barrier, Phase, PHASE},
     memory::Memory,
     types::{is_skewed, Value},
 };
@@ -25,7 +25,11 @@ pub unsafe fn write_with_barrier<M: Memory>(mem: &mut M, location: *mut Value, v
     debug_assert!(!is_skewed(location as u32));
     debug_assert_ne!(location, core::ptr::null_mut());
     pre_write_barrier(mem, *location);
-    *location = value.forward_if_possible();
+    if let Phase::Update(_) = &PHASE {
+        *location = value.forward_if_possible();
+    } else {
+        *location = value;
+    }
 }
 
 /// Allocation barrier to be called after a new object allocation.
