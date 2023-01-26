9dfce571dd2049c6a48fbe6e0787112737d53d9b
luc-blaeser <luc.blaeser@dfinity.org>
2023-01-26 10:30:32 +0100

    Code refactoring

--- rts/motoko-rts/src/types.rs
+++ rts/motoko-rts/src/types.rs
@@ -20,7 +20,7 @@
 // [2]: https://doc.rust-lang.org/stable/reference/type-layout.html#the-c-representation
 
 use crate::gc::generational::write_barrier::post_write_barrier;
-use crate::gc::incremental::pre_write_barrier;
+use crate::gc::incremental::barriers::write_with_barrier;
 use crate::memory::Memory;
 use crate::tommath_bindings::{mp_digit, mp_int};
 use core::ops::{Add, AddAssign, Div, Mul, Sub, SubAssign};
@@ -569,8 +569,7 @@ impl Array {
     pub unsafe fn set_pointer<M: Memory>(self: *mut Self, idx: u32, value: Value, mem: &mut M) {
         debug_assert!(value.is_ptr());
         let slot_addr = self.element_address(idx) as *mut Value;
-        pre_write_barrier(mem, *slot_addr);
-        *slot_addr = value.forward_if_possible();
+        write_with_barrier(mem, slot_addr, value);
         post_write_barrier(mem, slot_addr as u32);
     }
 
