e3145e7ce788fd2f62acf5b19e2dae1fe84451ec
Gabor Greif <gabor@dfinity.org>
2023-02-08 20:24:30 +0100

    Update rts/motoko-rts/src/memory/ic.rs

--- rts/motoko-rts/src/memory/ic.rs
+++ rts/motoko-rts/src/memory/ic.rs
@@ -103,7 +103,7 @@ unsafe fn grow_memory(ptr: u64) {
     let total_pages_needed = (ptr >> 16) as usize + 1 - (ptr >> 32) as usize;
     let current_pages = wasm32::memory_size(0);
     if total_pages_needed > current_pages {
-        #[allow(clippy::collapsible_if)] // faster by 1% if not colapsed with &&
+        #[allow(clippy::collapsible_if)] // faster by 1% if not collapsed with &&
         if wasm32::memory_grow(0, total_pages_needed - current_pages) == core::usize::MAX {
             rts_trap_with("Cannot grow memory");
         }
