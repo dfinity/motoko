c11e30d7e6261adbfcadbf7b26481b5fc09ca04a
Claudio Russo <claudio@dfinity.org>
2023-02-10 00:05:36 +0000

    optimise alloc slightly

--- rts/motoko-rts/src/memory/ic.rs
+++ rts/motoko-rts/src/memory/ic.rs
@@ -91,13 +91,13 @@ unsafe fn grow_memory(ptr: u64) {
     let page_size = u64::from(WASM_PAGE_SIZE.as_u32());
     let total_pages_needed = ((ptr + page_size - 1) / page_size) as usize;
     let current_pages = wasm32::memory_size(0);
+    if total_pages_needed > 65535 {
+        // last page is reserved for Rust/C stack guard
+        rts_trap_with("Cannot grow memory");
+    }
     if total_pages_needed > current_pages {
         #[allow(clippy::collapsible_if)] // faster by 1% if not colapsed with &&
         if wasm32::memory_grow(0, total_pages_needed - current_pages) == core::usize::MAX {
-            rts_trap_with("Cannot grow memory");
-        }
-        if wasm32::memory_size(0) == 65536 {
-            // last page is reserved for Rust/C stack guard
             rts_trap_with("Cannot grow memory");
         }
     }
