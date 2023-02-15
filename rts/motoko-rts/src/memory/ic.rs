24062565f90bdefef0d847db46f5df80fc6c2f27
Claudio Russo <claudio@dfinity.org>
2023-02-15 11:03:45 +0000

    perf: allocator fast path (alternative testing memory size). (#3790)
    Experiment with comparing `new_hp` against `memory_size`. Variation of #3090.
    
    - simpler conceptually/less churn
    - still rejects failed `memory_grow` (doesn't assume it goes to 65536 pages)
    - avoids grow when used heap within physical memory
    - no need to pregrow as with xor trick.
    
    I think I am shaving the yak, but let's see what CI reports for perf.

--- rts/motoko-rts/src/memory/ic.rs
+++ rts/motoko-rts/src/memory/ic.rs
@@ -38,8 +38,6 @@ unsafe extern "C" fn init(align: bool) {
         get_heap_base()
     };
     LAST_HP = HP;
-    // make sure that the page HP points to is allocated too
-    grow_memory(u64::from(HP))
 }
 
 #[no_mangle]
@@ -77,8 +75,7 @@ impl Memory for IcMemory {
         let new_hp = old_hp + delta;
 
         // Grow memory if needed
-        if (old_hp ^ new_hp) / WASM_PAGE_SIZE.as_u32() as u64 != 0 {
-            // Page boundary crossed, make sure that pages are allocated
+        if new_hp > ((wasm32::memory_size(0) as u64) << 16) {
             grow_memory(new_hp)
         }
 
@@ -88,9 +85,8 @@ impl Memory for IcMemory {
     }
 }
 
-/// Page allocation. Ensures that the memory up to, and including, the given pointer is allocated,
+/// Page allocation. Ensures that the memory up to, but excluding, the given pointer is allocated,
 /// with the slight exception of not allocating the extra page for address 0xFFFF_0000.
-/// Enforced precondition: `ptr` must be a pointer less than or equal to 0xFFFF_0000.
 #[inline(never)]
 unsafe fn grow_memory(ptr: u64) {
     debug_assert_eq!(0xFFFF_0000, usize::MAX - WASM_PAGE_SIZE.as_usize() + 1);
@@ -98,7 +94,8 @@ unsafe fn grow_memory(ptr: u64) {
         // spare the last wasm memory page
         rts_trap_with("Cannot allocate memory")
     };
-    let total_pages_needed = (ptr >> 16) as usize + (ptr < 0xFFFF_0000) as usize;
+    let page_size = u64::from(WASM_PAGE_SIZE.as_u32());
+    let total_pages_needed = ((ptr + page_size - 1) / page_size) as usize;
     let current_pages = wasm32::memory_size(0);
     if total_pages_needed > current_pages {
         if wasm32::memory_grow(0, total_pages_needed - current_pages) == core::usize::MAX {
