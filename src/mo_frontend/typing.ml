d5dd69375159e80cd7a1d54bf3b770f31fac2d8b
Kento Sugama <kento.sugama@dfinity.org>
2023-02-22 12:01:48 -0800

    simplify refutablility warning

--- src/mo_frontend/typing.ml
+++ src/mo_frontend/typing.ml
@@ -2648,11 +2648,10 @@ and infer_dec_valdecs env dec : Scope.t =
     Scope.{empty with val_env = T.Env.singleton id.it obj_typ}
   | LetD (pat, exp, fail) ->
     let t = infer_exp {env with pre = true} exp in
-    let consequence = match fail with
-      | None when is_import dec -> local_error
-      | None -> warn
-      | Some _ -> fun _ _ _ -> Format.kasprintf ignore in
-    let ve' = check_pat_exhaustive consequence env t pat in
+    let ve' = match fail with
+      | None -> check_pat_exhaustive (if is_import dec then local_error else warn) env t pat
+      | Some _ -> check_pat env t pat
+    in
     Scope.{empty with val_env = ve'}
   | VarD (id, exp) ->
     let t = infer_exp {env with pre = true} exp in
