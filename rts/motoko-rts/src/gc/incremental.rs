70f92a482e7e3bed09b43ac5b28102ad39f18291
luc-blaeser <luc.blaeser@dfinity.org>
2023-02-28 14:39:05 +0100

    Resolve merge conflicts

--- rts/motoko-rts/src/gc/incremental.rs
+++ rts/motoko-rts/src/gc/incremental.rs
@@ -8,7 +8,7 @@ pub mod barriers;
 #[ic_mem_fn(ic_only)]
 unsafe fn initialize_incremental_gc<M: Memory>(_mem: &mut M) {
     use crate::memory::ic;
-    ic::initialize_memory(true);
+    ic::initialize_memory();
 }
 
 #[ic_mem_fn(ic_only)]
