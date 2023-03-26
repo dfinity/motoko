dfinity/motoko

commit 0bea8a5e225b615693eb9929e2b198bf7a937205

perf: identify const variants (#3878)
Variants with constant payloads should be constant and end up in the static heap. This will speed up all tree-like data structures that have unit-payload leaves (not to speak of allocation wins!). See https://github.com/dfinity/motoko-base/issues/543 for motivation.

For below program
``` Motoko
import P = "mo:⛔️";

type Tree = { #leaf; #node : (Tree, Tree) };

func leaf() : (Tree, (Nat, Nat)) = (#leaf, (5, 42));

P.debugPrint(debug_show leaf());
```
the function `leaf()` now looks like
``` wasm
  (func $leaf (type 5) (param $clos i32)
    i32.const 2097267
    i32.const 2097279
    global.set 18
    global.set 17)
```
looks as expected. It also runs happily
```
$ wasmtime tree.wasm 
(#leaf, (5, 42))
```

src/codegen/compile.ml
@@ -138,6 +138,7 @@ module Const = struct
     | Obj of (string * t) list
     | Unit
     | Array of t list (* also tuples, but not nullary *)
+    | Tag of (string * t)
     | Lit of lit
 
   (* A constant known value together with a vanilla pointer.
@@ -1749,6 +1750,13 @@ module Variant = struct
   let test_is env l =
     get_variant_tag ^^
     compile_eq_const (hash_variant_label env l)
+
+  let vanilla_lit env i ptr =
+    E.add_static env StaticBytes.[
+        I32 Tagged.(int_of_tag Variant);
+        I32 (hash_variant_label env i);
+        I32 ptr
+      ]
 
 end (* Variant *)
 
@@ -7129,6 +7137,9 @@ module StackRep = struct
     | Const.Array cs ->
       let ptrs = List.map (materialize_const_t env) cs in
       Arr.vanilla_lit env ptrs
+    | Const.Tag (i, c) ->
+      let ptr = materialize_const_t env c in
+      Variant.vanilla_lit env i ptr
     | Const.Lit l -> materialize_lit env l
 
   let adjust env (sr_in : t) sr_out =
@@ -10307,8 +10318,12 @@ and compile_const_exp env pre_ae exp : Const.t * (E.t -> VarEnv.t -> unit) =
   | PrimE (ArrayPrim (Const, _), es)
   | PrimE (TupPrim, es) ->
     let (cs, fills) = List.split (List.map (compile_const_exp env pre_ae) es) in
-    Const.t_of_v (Const.Array cs),
+    Const.(t_of_v (Array cs)),
     (fun env ae -> List.iter (fun fill -> fill env ae) fills)
+  | PrimE (TagPrim i, [e]) ->
+    let (arg_ct, fill) = compile_const_exp env pre_ae e in
+    Const.(t_of_v (Tag (i, arg_ct))),
+    fill
 
   | _ -> assert false
 

src/ir_def/check_ir.ml
@@ -867,6 +867,8 @@ let rec check_exp env (exp:Ir.exp) : unit =
       check e1.note.Note.const "constant DotPrim on non-constant subexpression"
     | PrimE (ProjPrim _, [e1]) ->
       check e1.note.Note.const "constant ProjPrim on non-constant subexpression"
+    | PrimE (TagPrim _, [e1]) ->
+      check e1.note.Note.const "constant TagPrim with non-constant subexpression"
     | BlockE (ds, e) ->
       List.iter (fun d -> match d.it with
         | VarD _ | RefD _ -> check false "VarD/RefD in constant BlockE"

src/ir_passes/const.ml
@@ -124,8 +124,7 @@ let rec exp lvl (env : env) e : Lbool.t =
     | PrimE (TupPrim, es)
     | PrimE (ArrayPrim (Const, _), es) ->
       all (List.map (fun e -> exp lvl env e) es)
-    | PrimE (DotPrim _, [e1])
-    | PrimE (ProjPrim _, [e1]) ->
+    | PrimE (DotPrim _, [e1] | ProjPrim _, [e1] | TagPrim _, [e1]) ->
       exp lvl env e1
     | LitE _ ->
       surely_true

commit 698a5e2d77c3ee7c0f547d20de97df165f1e3adc

bugfix: compiler crash when matching on constant variants (#3901)
This fixes a regression about `switch` on constant variants that crashes the compiler (the repro and symptoms are in https://github.com/dfinity/motoko/pull/3901/commits/e138d932b5de102c1910a25c45bf278890006a6e). The regression was introduced in #3878.

We now additionally allow certain refutable patterns in constant expression matches (`TagE` for now).

Fixes #3899.

src/codegen/compile.ml
@@ -7892,7 +7892,7 @@ module PatCode = struct
   jump-label for the fail case. But many patterns cannot fail, in particular
   function arguments that are simple variables. In these cases, we do not want
   to create the block and the (unused) jump label. So we first generate the
-  code, either as plain code (CannotFail) or as code with hole for code to fun
+  code, either as plain code (CannotFail) or as code with hole for code to run
   in case of failure (CanFail).
   *)
 
@@ -7937,8 +7937,10 @@ module PatCode = struct
   let orElses : patternCode list -> patternCode -> patternCode =
     List.fold_right orElse
 
+  let patternFailTrap env = E.trap_with env "pattern failed"
+
   let orPatternFailure env pcode =
-    with_fail (E.trap_with env "pattern failed") pcode
+    with_fail (patternFailTrap env) pcode
 
   let orsPatternFailure env pcodes =
     orPatternFailure env (orElses pcodes definiteFail)
@@ -10194,6 +10196,17 @@ and compile_dec env pre_ae how v2en dec : VarEnv.t * G.t * (VarEnv.t -> scope_wr
   (fun (pre_ae, alloc_code, mk_code, wrap) ->
        G.(pre_ae, with_region dec.at alloc_code, fun ae body_code ->
           with_region dec.at (mk_code ae) ^^ wrap body_code)) @@
+
+  (* Set up some helpers, for exclusive usage by the "constant expressions" special case below *)
+  let const_exp_helper =
+    lazy begin
+        let[@warning "-8"] LetD (p, e) = dec.it in
+        const_exp_matches_pat env p e
+      end in
+  let is_compile_time_matchable () =
+    let lazy const_exp_matches = const_exp_helper in
+    Option.is_some const_exp_matches in
+
   match dec.it with
   (* A special case for public methods *)
   (* This relies on the fact that in the top-level mutually recursive group, no shadowing happens. *)
@@ -10206,9 +10219,15 @@ and compile_dec env pre_ae how v2en dec : VarEnv.t * G.t * (VarEnv.t -> scope_wr
     G.( pre_ae1, nop, (fun ae -> fill env ae; nop), unmodified)
 
   (* A special case for constant expressions *)
-  | LetD (p, e) when Ir_utils.is_irrefutable p && e.note.Note.const ->
-    let extend, fill = compile_const_dec env pre_ae dec in
-    G.( extend pre_ae, nop, (fun ae -> fill env ae; nop), unmodified)
+  | LetD (p, e) when e.note.Note.const && is_compile_time_matchable () ->
+    let is_compile_time_bottom =
+      let lazy const_exp_matches = const_exp_helper in
+      not (Option.get const_exp_matches) in
+    if is_compile_time_bottom then (* refuted *)
+      (pre_ae, G.nop, (fun _ -> PatCode.patternFailTrap env), unmodified)
+    else (* not refuted *)
+      let extend, fill = compile_const_dec env pre_ae dec in
+      G.(extend pre_ae, nop, (fun ae -> fill env ae; nop), unmodified)
 
   | LetD (p, e) ->
     let (pre_ae1, alloc_code, pre_code, sr, fill_code) = compile_unboxed_pat env pre_ae how p in
@@ -10361,6 +10380,15 @@ and compile_const_decs env pre_ae decs : (VarEnv.t -> VarEnv.t) * (E.t -> VarEnv
         (fun env ae -> fill1 env ae; fill2 env ae) in
   go pre_ae decs
 
+and const_exp_matches_pat env pat exp : bool option =
+  assert exp.note.Note.const;
+  match exp.it with
+  | _ when Ir_utils.is_irrefutable pat -> Some true
+  | PrimE (TagPrim _, _) ->
+     let c, _ = compile_const_exp env VarEnv.empty_ae exp in
+     (try ignore (destruct_const_pat VarEnv.empty_ae pat c); Some true with Invalid_argument _ -> Some false)
+  | _ -> None
+
 and destruct_const_pat ae pat const : VarEnv.t = match pat.it with
   | WildP -> ae
   | VarP v -> VarEnv.add_local_const ae v const
@@ -10371,13 +10399,21 @@ and destruct_const_pat ae pat const : VarEnv.t = match pat.it with
       | Some (_, c) -> destruct_const_pat ae pf.it.pat c
       | None -> assert false
     ) ae pfs
-  | AltP (p1, p2) -> destruct_const_pat ae p1 const
+  | AltP (p1, p2) ->
+    begin
+      try destruct_const_pat ae p1 const with
+        Invalid_argument _ -> destruct_const_pat ae p2 const
+    end
   | TupP ps ->
     let cs = match const with (_ , Const.Array cs) -> cs | (_, Const.Unit) -> [] | _ -> assert false in
     List.fold_left2 destruct_const_pat ae ps cs
   | LitP _ -> raise (Invalid_argument "LitP in static irrefutable pattern")
   | OptP _ -> raise (Invalid_argument "OptP in static irrefutable pattern")
-  | TagP _ -> raise (Invalid_argument "TagP in static irrefutable pattern")
+  | TagP (i, p) ->
+     match const with
+     | (_, Const.Tag (ic, c)) when i = ic -> destruct_const_pat ae p c
+     | (_, Const.Tag _) -> raise (Invalid_argument "TagP mismatch")
+     | _ -> assert false
 
 and compile_const_dec env pre_ae dec : (VarEnv.t -> VarEnv.t) * (E.t -> VarEnv.t -> unit) =
   (* This returns a _function_ to extend the VarEnv, instead of doing it, because

test/run/ok/refuted-const-variant.run.ok
@@ -1,0 +1,1 @@
+refuted-const-variant.mo:2.6-2.14: execution error, value #bummer does not match pattern

test/run/ok/refuted-const-variant.run.ret.ok
@@ -1,0 +1,1 @@
+Return code 1

test/run/ok/refuted-const-variant.tc.ok
@@ -1,0 +1,4 @@
+refuted-const-variant.mo:2.6-2.14: warning [M0145], this pattern of type
+  {#bummer}
+does not cover value
+  #bummer

test/run/ok/refuted-const-variant.wasm-run.ok
@@ -1,0 +1,10 @@
+pattern failed
+Error: failed to run main module `_out/refuted-const-variant.wasm`
+
+Caused by:
+    0: failed to invoke command default
+    1: wasm trap: unreachable
+       wasm backtrace:
+         0: init
+         1: _start
+       

test/run/ok/refuted-const-variant.wasm-run.ret.ok
@@ -1,0 +1,1 @@
+Return code 134

test/run/refuted-const-variant.mo
@@ -1,0 +1,5 @@
+// a failing pattern match that can be compiled to a trap
+let (#const b) = #bummer;
+
+//SKIP run-low
+//SKIP run-ir

test/run/variants.mo
@@ -55,3 +55,7 @@ assert (debug_show (#foo (#bar)) == "#foo(#bar)");
 
 assert ([#Monday, #Tuesday, #Wednesday, #Thursday, #Friday, #Saturday, #Sunday].size() == 7);
 
+// check compilation of pattern matching on constant variants
+let (#const a) = #const 42;
+
+assert a == 42;