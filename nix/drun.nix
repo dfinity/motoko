fcb4e94b80abb643da0a8c8ef9a5548d0b1850ae
luc-blaeser <luc.blaeser@dfinity.org>
2023-05-01 14:57:50 +0200

    Revert "Updating nix hashes"
    This reverts commit 01c0f8f62f8ceacacc501740b0800f641f18b4b4.

--- nix/drun.nix
+++ nix/drun.nix
@@ -15,7 +15,7 @@ pkgs:
       # installed. You will normally not be bothered to perform
       # the command therein manually.
 
-      cargoSha256 = "";
+      cargoSha256 = "sha256-yzI2mGzExqkcW0I7yM8iLdQ34CMN/jRinuT6L9dJBsw=";
 
       patchPhase = ''
 
