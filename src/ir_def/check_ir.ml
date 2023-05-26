78e75834f091d0995e03a20faf56ba999aaac8b3
Claudio Russo <claudio@dfinity.org>
2023-05-26 16:00:23 +0100

    skip ic-ref-run while I don't have it

--- src/ir_def/check_ir.ml
+++ src/ir_def/check_ir.ml
@@ -271,7 +271,7 @@ and check_typ_field env s tf : unit =
      "typ field in non-typ_field flavor";
     check_con env c
   | t, Some T.Actor when not (T.is_shared_func t) ->
-    error env no_region "actor field %s must have shared function type" tf.T.lab
+    error env no_region "actor field %s must have shared function type, found %s" tf.T.lab (T.string_of_typ t)
   | t, _ -> check_typ env t
 
 and check_typ_binds_acyclic env cs ts  =
