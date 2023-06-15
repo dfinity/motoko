0db11eaba30bf6a50ffbbaed857d52eb7dd9cc2d
matthewhammer <matthew@dfinity.org>
2023-06-15 08:28:23 -0600

    re-run script on more tests.

--- test/run-drun/test-feature.sh
+++ test/run-drun/test-feature.sh
@@ -1,1 +1,10 @@
-for i in region0-stable-mem-*.mo stable-mem-*.mo; do echo $i; if ../run.sh -d $i ; then echo; echo yay; echo; else echo `date` $i >> failures.log ; echo; echo "Failure."; echo; fi; done
+for i in stable-region-*.mo region0-stable-mem-*.mo stable-mem-*.mo; do
+    echo $i;
+    if ../run.sh -d $i ; then
+	echo `date` $i >> success.log ;
+	echo; echo "Success."; echo;
+    else
+	echo `date` $i >> failures.log ;
+	echo; echo "Failure."; echo;
+    fi;
+done
