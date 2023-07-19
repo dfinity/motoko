f39c93342a8234b25355f31c30bf7ec276b04876
luc-blaeser <luc.blaeser@dfinity.org>
2023-07-19 14:34:47 +0200

    Retrigger CI build
    This reverts commit 36105b5e895bb3320a77ccb9394764ccb1857b95.

--- default.nix
+++ default.nix
@@ -193,7 +193,7 @@ rec {
         name = "motoko-rts-deps";
         src = subpath ./rts;
         sourceRoot = "rts/motoko-rts-tests";
-        sha256 = "sha256-XDgs1TRGJb3PkP3mKVYLZXq0BFVeRVYc7hS/BOJuQXU=";
+        sha256 = "sha256-DRX2IJzJ5rDGoutUssqO4TaHwvKO3rUR7MUpWIIKvKU=";
         copyLockfile = true;
       };
 
