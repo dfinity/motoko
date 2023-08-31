48ca9a1a277547f69d5efc0786c382a0f8951f21
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2023-08-31 14:29:26 -0600

    Experiment: add doc comments to generated Candid files (#4178)
    * Set up experiment: add doc comments to generated Candid files
    
    * Progress
    
    * Progress
    
    * Progress
    
    * Progress
    
    * Progress
    
    * Remove extraneous file
    
    * Progress
    
    * Simplify
    
    * Fix 'parse_tests'
    
    * Misc refactoring
    
    * Fix warnings
    
    * Pass Motoko trivia table to 'MakeState()'
    
    * Add trivia lookup function
    
    * Misc
    
    * Refactor trivia printing logic (still provisional)
    
    * Pass 'E.prog' source location to corresponding 'I.prog'
    
    * Progress
    
    * Simplify
    
    * Misc refactor
    
    * Fix formatting
    
    * Use 'actor' source region in place of 'prog'
    
    * Print field doc comments when available
    
    * Print doc comments for actors, fields, and decs when source region is available
    
    * Use '/// ' line prefix
    
    * Use 'prog.at' for top-level source region
    
    * Add todo for context
    
    * Implement 'string_of_trivia_info'
    
    * Update 'trivia.mli'
    
    * Remove unused / duplicate logic
    
    * Add 'docs_of_trivia_info' with comment syntax information
    
    * Use original line/block comment syntax in Candid file
    
    * Add todo comments
    
    * Refactor 'depr' -> 'src' in type field
    
    * Use 'no_region' in place of 'None' for src.region
    
    * Update code comments
    
    * Pass source regions to Candid methods
    
    * Simplify 'object_of_scope' logic
    
    * Refactor to pass around 'src' in place of 'region * opt string' in 'typing.ml'
    
    * Move actor comment to before 'service' field
    
    * Include doc comments directly before actor
    
    * Convert block comments to line comments
    
    * Include 'id_region' to keep original source locations for error M1024
    
    * Refactor 'visibility_src' for clarity
    
    * Update moc.js test file
    
    * Finish 'visibility_src' refactor
    
    * Remove commented code

--- src/ir_def/construct.ml
+++ src/ir_def/construct.ml
@@ -220,7 +220,7 @@ let optE e =
 
 let tagE i e =
  { it = PrimE (TagPrim i, [e]);
-   note = Note.{ def with typ = T.Variant [{T.lab = i; typ = typ e; depr = None}]; eff = eff e };
+   note = Note.{ def with typ = T.Variant [{T.lab = i; typ = typ e; src = T.empty_src}]; eff = eff e };
    at = no_region;
  }
 
