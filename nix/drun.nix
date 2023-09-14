c6fccab4994aa9d092262183c155bcfb3d7fb185
Nix hash updater <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-14 09:21:20 +0000

    Updating nix hashes

--- nix/drun.nix
+++ nix/drun.nix
@@ -19,7 +19,10 @@ pkgs:
         lockFile = "${pkgs.sources.ic}/Cargo.lock";
         outputHashes = {
           "derive_more-0.99.8-alpha.0" = "sha256-tEsfYC9oCAsDjinCsUDgRg3q6ruvayuA1lRmsEP9cys=";
-          "ic-btc-interface-0.1.0" = "sha256-VecEMFjoeiRi0VgJ9CeDoOzdyJbJNiZ5MBmiV1+b7As=";
+          "ic-btc-interface-0.1.0" = "sha256-7X82NzllKEpXU+fOtc4GiGwnKNCB6DZZf7MFftnr5cI=";
+          "ic-btc-test-utils-0.1.0" = "sha256-VecEMFjoeiRi0VgJ9CeDoOzdyJbJNiZ5MBmiV1+b7As=";
+          "ic-btc-validation-0.1.0" = "sha256-DRrZ0MX6Qykh+IbEpmWPYvT5XxsiED0jPp6l0Ci+Leo=";
+          "icrc1-test-env-0.1.0" = "sha256-+R9mClFb7I7dSYbKHnDVwyTN4QrMRDxiDNx/smDJe3g=";
           "jsonrpc-0.12.1" = "sha256-3FtdZlt2PqVDkE5iKWYIp1eiIELsaYlUPRSP2Xp8ejM=";
           "libssh2-sys-0.2.23" = "sha256-9Hb7CnPF+lxrVO1NAhS7EXcPVWZutJXr6UWxpptzk4U=";
           "lmdb-rkv-0.14.99" = "sha256-dqxWgtNZ/qRukQLiDawjHRwdPgfzm/sVcWwudwX9rD8=";
