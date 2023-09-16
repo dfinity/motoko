2f5845b344e0a62bb48158c90a18c4721896dc6f
Claudio Russo <claudio@dfinity.org>
2023-09-16 02:51:34 +0100

    inline allocation too

--- src/codegen/compile.ml
+++ src/codegen/compile.ml
@@ -1687,7 +1687,7 @@ module Tagged = struct
       let ext = if Numerics.Nat16.(to_int (popcnt (of_int n))) = 1 then increment else 0l in
       Int32.(logor ext (logand page_mask (shift_left minus_one (16 - Numerics.Nat16.(to_int (clz (of_int n))))))) in
 
-    Func.share_code0 Func.Always env name [I32Type] (fun env ->
+    Func.share_code0 Func.Never env name [I32Type] (fun env ->
       let set_object, get_object = new_local env "new_object" in
       let size_in_bytes = Int32.(mul size Heap.word_size) in
       let half_page_size = Int32.div page_size 2l in
