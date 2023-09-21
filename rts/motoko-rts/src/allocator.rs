29093e0c7dca852bea789492313bd9bb089ab61b
Claudio Russo <claudio@dfinity.org>
2023-09-22 00:11:06 +0100

    support aligment > 4 (probably untested)

--- rts/motoko-rts/src/allocator.rs
+++ rts/motoko-rts/src/allocator.rs
@@ -9,12 +9,29 @@ pub struct HeapAllocator;
 
 unsafe impl GlobalAlloc for HeapAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        if !(layout.align() == 4 || layout.align() == 2 || layout.align() == 1) {
-            panic!("unsupported allocator alignment");
-        };
-        let blob = alloc_blob::<ic::IcMemory>(&mut ic::IcMemory, Bytes(layout.size() as u32))
-            .as_blob_mut();
-        blob.payload_addr()
+        let size = layout.size();
+        let align = layout.align();
+        // align is a power of 2
+        debug_assert!(align.count_ones() == 1);
+        if !(align <= 4) {
+            // payload_addr() is always word-aligned, so also nibble and byte aligned)
+            let blob =
+                alloc_blob::<ic::IcMemory>(&mut ic::IcMemory, Bytes(size as u32)).as_blob_mut();
+            let address = blob.payload_addr();
+            debug_assert!(address as usize % align == 0);
+            address
+        } else {
+            // for other alignments (all powers of 2), we need to round up the size and
+            // round up payload_addr to nearest aligned address
+            let size_up = size + (align - 1);
+            let blob =
+                alloc_blob::<ic::IcMemory>(&mut ic::IcMemory, Bytes(size_up as u32)).as_blob_mut();
+            let payload_addr = blob.payload_addr() as usize;
+            let address = (payload_addr + (align - 1)) & (!(align - 1));
+            debug_assert!(address % align == 0);
+            debug_assert!(address + size - 1 <= payload_addr + size_up - 1);
+            address as *mut u8
+        }
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
