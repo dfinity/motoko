189197222cd7f5c45b28264a46f6b1645d905cb5
Claudio Russo <claudio@dfinity.org>
2023-11-01 11:16:56 +0000

    Update src/codegen/compile.ml
    to trigger CI

--- src/codegen/compile.ml
+++ src/codegen/compile.ml
@@ -328,7 +328,7 @@ module E = struct
 
     features : FeatureSet.t ref; (* Wasm features using wasmtime naming *)
 
-    (* StableMemory *)
+    (* requires stable memory, and emulation on wasm targets *)
     requires_stable_memory : bool ref;
   }
 
