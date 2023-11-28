ff5c79666faf5433240b6cf66bb6c6443e8e83ab
luc-blaeser <luc.blaeser@dfinity.org>
2023-11-28 13:53:42 +0100

    Write-through memory

--- rts/motoko-rts/src/stabilization/reader_writer.rs
+++ rts/motoko-rts/src/stabilization/reader_writer.rs
@@ -1,89 +1,17 @@
-//! Buffered read/write access on stable memory.
+//! Read/write access on stable memory.
+//! Experiment: No buffering.
 //! Supporting Cheney's to-space in stable memory.
 
-use core::{
-    cmp::min,
-    mem::{size_of, zeroed},
-};
+use core::mem::{size_of, zeroed};
 
-use crate::{
-    mem_utils::memcpy_bytes,
-    stable_mem::{ic0_stable64_read, ic0_stable64_write, PAGE_SIZE},
-    types::Bytes,
-};
+use crate::stable_mem::{ic0_stable64_read, ic0_stable64_write};
 
 use super::grant_stable_space;
 
-const FREE_PAGE_INDEX: u64 = u64::MAX;
-
-struct StableMemoryPage {
-    page_index: u64,
-    modified: bool,
-    data: [u8; PAGE_SIZE as usize],
-}
-
-impl StableMemoryPage {
-    fn new() -> StableMemoryPage {
-        StableMemoryPage {
-            page_index: FREE_PAGE_INDEX,
-            modified: false,
-            data: [0u8; PAGE_SIZE as usize],
-        }
-    }
-
-    fn is_free(&self) -> bool {
-        self.page_index == FREE_PAGE_INDEX
-    }
-
-    fn cached_data(&mut self) -> *mut u8 {
-        &mut self.data as *mut u8
-    }
-
-    fn stable_memory_offset(&self) -> u64 {
-        self.page_index * PAGE_SIZE
-    }
-
-    fn swap_out(&mut self) {
-        debug_assert!(!self.is_free());
-        if self.modified {
-            unsafe {
-                ic0_stable64_write(
-                    self.stable_memory_offset(),
-                    self.cached_data() as u64,
-                    PAGE_SIZE,
-                );
-            }
-        }
-        self.page_index = FREE_PAGE_INDEX;
-        self.modified = false;
-    }
-
-    fn swap_in(&mut self, page_index: u64) {
-        debug_assert!(self.is_free());
-        Self::grow_if_needed(page_index);
-        self.page_index = page_index;
-        debug_assert!(!self.modified);
-        unsafe {
-            ic0_stable64_read(
-                self.cached_data() as u64,
-                self.stable_memory_offset(),
-                PAGE_SIZE,
-            )
-        }
-    }
-
-    fn grow_if_needed(page_index: u64) {
-        grant_stable_space((page_index + 1) * PAGE_SIZE);
-    }
-}
-
-// At most two pages are needed to cache the current streamed `scan`` and `free` position.
-const CACHE_SIZE: usize = 2;
-
-/// Buffered streamed reader/writer on stable memory.
-/// Optimized for Cheney's algorithm.
+/// Streamed reader/writer on stable memory.
+/// Experiment: No buffering.
 ///
-/// The buffer supports two location-independent streams:
+/// The memory supports two location-independent streams:
 /// * Streamed reading and updating, used for scanning and patching pointers.
 /// * Streamed writing, used for allocating new objects.
 pub struct StableMemorySpace {
@@ -94,8 +22,6 @@ pub struct StableMemorySpace {
     scan_address: u64,
     /// Used for writing.
     free_address: u64,
-    // TODO: Use a way to not allocate a 128K empty data segment.
-    cache: [StableMemoryPage; CACHE_SIZE],
     closed: bool,
 }
 
@@ -123,32 +49,17 @@ pub trait WriteStream {
     fn raw_write(&mut self, data_address: usize, length: usize);
 }
 
-enum AccessMode {
-    /// Read at `scan`-position.
-    Read,
-    /// Update at `scan`-position.
-    Update,
-    /// Write at `free`-position.
-    Write,
-}
-
 impl StableMemorySpace {
     pub fn open(start_address: u64) -> StableMemorySpace {
         StableMemorySpace {
             base_address: start_address,
             scan_address: start_address,
             free_address: start_address,
-            cache: [StableMemoryPage::new(), StableMemoryPage::new()],
             closed: false,
         }
     }
 
     pub fn close(&mut self) {
-        for page in &mut self.cache {
-            if !page.is_free() {
-                page.swap_out();
-            }
-        }
         self.closed = true;
         debug_assert!(self.scan_address <= self.free_address);
     }
@@ -158,69 +69,6 @@ impl StableMemorySpace {
     pub fn written_length(&self) -> u64 {
         debug_assert!(self.base_address <= self.free_address);
         self.free_address - self.base_address
-    }
-
-    fn can_evict(&self, page_index: u64) -> bool {
-        page_index != self.scan_address / PAGE_SIZE && page_index != self.free_address / PAGE_SIZE
-    }
-
-    fn lookup(&mut self, page_index: u64) -> &mut StableMemoryPage {
-        debug_assert!(!self.can_evict(page_index));
-        for position in 0..self.cache.len() {
-            let cached_page_index = self.cache[position].page_index;
-            if cached_page_index == page_index {
-                return &mut self.cache[position];
-            }
-            if self.can_evict(cached_page_index) {
-                let page = &mut self.cache[position];
-                if !page.is_free() {
-                    page.swap_out();
-                }
-            }
-        }
-        for page in &mut self.cache {
-            if page.is_free() {
-                page.swap_in(page_index);
-                return page;
-            }
-        }
-        unreachable!()
-    }
-
-    fn chunked_access(&mut self, mode: AccessMode, main_memory_address: usize, length: usize) {
-        debug_assert!(!self.closed);
-        let mut main_memory_address = main_memory_address;
-        let mut stable_memory_address = match mode {
-            AccessMode::Read | AccessMode::Update => self.scan_address,
-            AccessMode::Write => self.free_address,
-        };
-        let access_end = stable_memory_address + length as u64;
-        while stable_memory_address < access_end {
-            let page_index = stable_memory_address / PAGE_SIZE;
-            let page_offset = stable_memory_address % PAGE_SIZE;
-            let page_end = (page_index + 1) * PAGE_SIZE;
-            let chunk_end = min(access_end, page_end);
-            let chunk_size = chunk_end - stable_memory_address;
-            let page = self.lookup(page_index);
-            let cache_address = page.cached_data() as usize + page_offset as usize;
-            let (destination, source) = match mode {
-                AccessMode::Read => (main_memory_address, cache_address),
-                AccessMode::Update | AccessMode::Write => (cache_address, main_memory_address),
-            };
-            unsafe {
-                memcpy_bytes(destination, source, Bytes(chunk_size as u32));
-            }
-            match mode {
-                AccessMode::Read => {}
-                AccessMode::Update | AccessMode::Write => page.modified = true,
-            }
-            stable_memory_address += chunk_size;
-            match mode {
-                AccessMode::Read | AccessMode::Update => self.scan_address = stable_memory_address,
-                AccessMode::Write => self.free_address = stable_memory_address,
-            }
-            main_memory_address += chunk_size as usize;
-        }
     }
 }
 
@@ -240,7 +88,9 @@ impl ScanStream for StableMemorySpace {
 
     fn raw_read(&mut self, data_address: usize, length: usize) {
         debug_assert!(self.scan_address + length as u64 <= self.free_address);
-        self.chunked_access(AccessMode::Read, data_address, length);
+        unsafe {
+            ic0_stable64_read(data_address as u64, self.scan_address, length as u64);
+        }
     }
 
     fn rewind(&mut self, length: usize) {
@@ -264,7 +114,9 @@ impl ScanStream for StableMemorySpace {
     fn raw_update(&mut self, data_address: usize, length: usize) {
         debug_assert!(length as u64 <= self.scan_address);
         self.scan_address -= length as u64;
-        self.chunked_access(AccessMode::Update, data_address, length);
+        unsafe {
+            ic0_stable64_write(self.scan_address, data_address as u64, length as u64);
+        }
     }
 }
 
@@ -276,7 +128,10 @@ impl WriteStream for StableMemorySpace {
     }
 
     fn raw_write(&mut self, data_address: usize, length: usize) {
-        self.chunked_access(AccessMode::Write, data_address, length);
+        unsafe {
+            grant_stable_space(self.free_address + length as u64);
+            ic0_stable64_write(self.free_address, data_address as u64, length as u64);
+        }
     }
 }
 
