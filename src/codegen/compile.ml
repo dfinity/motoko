d5f0c92ecd1cd5594e1c75dcd6feced009b2696a
luc-blaeser <luc.blaeser@dfinity.org>
2024-02-12 10:54:58 +0100

    Manual merge conflict resolution

--- src/codegen/compile.ml
+++ src/codegen/compile.ml
@@ -10568,7 +10568,8 @@ and compile_prim_invocation (env : E.t) ae p es at =
 
   | OtherPrim "call_perform_status", [] ->
     SR.UnboxedWord64 Type.Nat32,
-    IC.get_call_perform_status env
+    IC.get_call_perform_status env ^^
+    TaggedSmallWord.msb_adjust Type.Nat32
 
   | OtherPrim "call_perform_message", [] ->
     SR.Vanilla,
