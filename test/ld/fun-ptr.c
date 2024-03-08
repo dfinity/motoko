ee9fc7b09aa583a3d7f250f0b848903d2aabb538
luc-blaeser <luc.blaeser@dfinity.org>
2024-03-08 18:55:26 +0100

    Adjust linker tests

--- test/ld/fun-ptr.c
+++ test/ld/fun-ptr.c
@@ -4,14 +4,20 @@ int f0(int x, int y)
     return x + y;
 }
 
+// Clang/LLVM bug for Wasm `memory64`: &f0 generates a 32-bit value that is incompatible to void*
+void* result_f1 = (void*)1; // &f0
+
 __attribute__ ((visibility("default")))
 int (*f1(void)) (int x, int y)
 {
-    return &f0;
+    return result_f1;
 }
+
+// Clang/LLVM bug for Wasm `memory64`: &f0 generates a 32-bit value that is incompatible to void*
+void* result_f2 = (void*)2; // &f1
 
 __attribute__ ((visibility("default")))
 void *f2()
 {
-    return &f1;
+    return result_f2;
 }
