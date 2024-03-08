6af8bed2a6df201bd1e86c043c6be19a504e0e36
Claudio Russo <claudio@dfinity.org>
2024-03-08 20:12:02 +0000

    extracted small repro

--- src/codegen/compile.ml
+++ src/codegen/compile.ml
@@ -58,7 +58,7 @@ module TaggingScheme = struct
      Flags.sanity_check will check tags, but not further locate them.
   *)
 
-  let debug = true (* should never be true in master! *)
+  let debug = false (* should never be true in master! *)
 
   type bit = I | O
   let _ = (I,O) (* silence warning on unused constructors *)
@@ -1848,7 +1848,7 @@ module BitTagged = struct
          get_n ^^
          compile_bitand_const tag_mask ^^
          compile_eq_const (TaggingScheme.tag_of_typ ty) ^^
-         E.else_trap_with env "unexpected tag" ^^
+         E.else_trap_with env (prim_fun_name ty "unexpected tag"^Int.to_string line) ^^
          get_n))
     else G.nop
 
@@ -12014,6 +12014,7 @@ and compile_exp_with_hint (env : E.t) ae sr_hint exp =
     compile_exp_as env ae sr e ^^
     code
   | FuncE (x, sort, control, typ_binds, args, res_tys, e) ->
+    let x = x^"@"^Source.string_of_region exp.at in (* TBD *)
     let captured = Freevars.captured exp in
     let return_tys = match control with
       | Type.Returns -> res_tys
