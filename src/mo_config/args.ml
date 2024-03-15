aea7ad5ce9a3870b88fa37d78d2c3f21e48e366b
rvanasa <ryan.vandersmith@dfinity.org>
2024-03-15 09:39:06 -0600

    Remove extraneous semicolon

--- src/mo_config/args.ml
+++ src/mo_config/args.ml
@@ -19,7 +19,7 @@ let string_map flag r desc =
 let package_args = [
   string_map "--package" Flags.package_urls "<package-name> <package-path> specify a <package-name> <package-path> pair, separated by a space";
   "--actor-idl", Arg.String (fun fp -> Flags.actor_idl_path := Some fp), "<idl-path>   path to actor IDL (Candid) files";
-  string_map "--actor-alias" Flags.actor_aliases "<alias> <principal>  actor import alias";
+  string_map "--actor-alias" Flags.actor_aliases "<alias> <principal>  actor import alias"
   ]
 
 let error_args = [
