1ba5c2719336b2e2423bdd58129fe22b0785acb5
rvanasa <ryan.vandersmith@dfinity.org>
2024-03-19 11:33:27 -0600

    Add optimized 'Array' and 'Blob' types

--- rts/motoko-rts/src/custom.rs
+++ rts/motoko-rts/src/custom.rs
@@ -1,5 +1,7 @@
 // Custom RTS function utilities
 
+use core::marker::PhantomData;
+
 use alloc::vec::Vec;
 use motoko_rts_macros::{motoko, tuple_macro};
 
@@ -40,117 +42,113 @@ pub enum MotokoError {
     Custom(u64),
 }
 
-type MotokoResult<T> = Result<T, MotokoError>;
-
-/// Wrapper for representing a `Vec<u8>` value as a `Blob` in Motoko.
-#[derive(Clone, Debug)]
-pub struct BlobVec(pub Vec<u8>);
+pub type Result<T, E = MotokoError> = core::result::Result<T, E>;
 
 /// Trait for converting a Motoko value to a Rust value.
 pub trait FromValue: Sized {
-    unsafe fn from_value(value: Value, mem: &mut impl Memory) -> MotokoResult<Self>;
+    unsafe fn from_value(value: Value, mem: &mut impl Memory) -> Result<Self>;
 }
 
 /// Trait for converting a Rust value to a Motoko value.
 pub trait IntoValue {
-    unsafe fn into_value(self, mem: &mut impl Memory) -> MotokoResult<Value>;
+    unsafe fn into_value(self, mem: &mut impl Memory) -> Result<Value>;
 }
 
 impl FromValue for Value {
-    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> Result<Self> {
         Ok(value)
     }
 }
 impl IntoValue for Value {
-    unsafe fn into_value(self, _mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, _mem: &mut impl Memory) -> Result<Value> {
         Ok(self)
     }
 }
 
 impl FromValue for u8 {
-    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> Result<Self> {
         Ok(u8_from_nat8(value))
     }
 }
 impl IntoValue for u8 {
-    unsafe fn into_value(self, _mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, _mem: &mut impl Memory) -> Result<Value> {
         Ok(nat8_from_u8(self))
     }
 }
 impl FromValue for u16 {
-    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> Result<Self> {
         Ok(u16_from_nat16(value))
     }
 }
 impl IntoValue for u16 {
-    unsafe fn into_value(self, _mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, _mem: &mut impl Memory) -> Result<Value> {
         Ok(nat16_from_u16(self))
     }
 }
 impl FromValue for u32 {
-    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> Result<Self> {
         Ok(u32_from_nat32(value))
     }
 }
 impl IntoValue for u32 {
-    unsafe fn into_value(self, _mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, _mem: &mut impl Memory) -> Result<Value> {
         Ok(nat32_from_u32(self))
     }
 }
 impl FromValue for u64 {
-    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> Result<Self> {
         Ok(u64_from_nat64(value))
     }
 }
 impl IntoValue for u64 {
-    unsafe fn into_value(self, _mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, _mem: &mut impl Memory) -> Result<Value> {
         Ok(nat64_from_u64(self))
     }
 }
 
 impl FromValue for i8 {
-    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> Result<Self> {
         Ok(i8_from_int8(value))
     }
 }
 impl IntoValue for i8 {
-    unsafe fn into_value(self, _mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, _mem: &mut impl Memory) -> Result<Value> {
         Ok(int8_from_i8(self))
     }
 }
 impl FromValue for i16 {
-    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> Result<Self> {
         Ok(i16_from_int16(value))
     }
 }
 impl IntoValue for i16 {
-    unsafe fn into_value(self, _mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, _mem: &mut impl Memory) -> Result<Value> {
         Ok(int16_from_i16(self))
     }
 }
 impl FromValue for i32 {
-    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> Result<Self> {
         Ok(i32_from_int32(value))
     }
 }
 impl IntoValue for i32 {
-    unsafe fn into_value(self, _mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, _mem: &mut impl Memory) -> Result<Value> {
         Ok(int32_from_i32(self))
     }
 }
 impl FromValue for i64 {
-    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> Result<Self> {
         Ok(i64_from_int64(value))
     }
 }
 impl IntoValue for i64 {
-    unsafe fn into_value(self, _mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, _mem: &mut impl Memory) -> Result<Value> {
         Ok(int64_from_i64(self))
     }
 }
 
 impl FromValue for bool {
-    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> Result<Self> {
         // Do we want a function in `compile.ml` here?
         let raw_value = value.get_raw();
         assert!(
@@ -162,40 +160,13 @@ impl FromValue for bool {
     }
 }
 impl IntoValue for bool {
-    unsafe fn into_value(self, _mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, _mem: &mut impl Memory) -> Result<Value> {
         Ok(Value::from_raw(self as u32))
     }
 }
 
-impl FromValue for BlobVec {
-    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
-        match value.tag() {
-            TAG_BLOB => {
-                let blob = value.as_blob();
-                let len = blob.len().as_u32();
-                Ok(BlobVec((0..len).into_iter().map(|i| blob.get(i)).collect()))
-            }
-            tag => Err(MotokoError::UnexpectedTag(tag)),
-        }
-    }
-}
-impl IntoValue for BlobVec {
-    unsafe fn into_value(self, mem: &mut impl Memory) -> MotokoResult<Value> {
-        let vec = self.0;
-        let value = alloc_blob(mem, Bytes(vec.len() as u32));
-        let blob = value.as_blob_mut();
-        let mut dest = blob.payload_addr();
-        for item in vec.into_iter() {
-            *dest = item;
-            dest = dest.add(1);
-        }
-        Ok(allocation_barrier(value))
-        // ^^ Is this allocation barrier necessary?
-    }
-}
-
 impl<T: FromValue> FromValue for Vec<T> {
-    unsafe fn from_value(value: Value, mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(value: Value, mem: &mut impl Memory) -> Result<Self> {
         match value.tag() {
             TAG_ARRAY => {
                 let array = value.as_array();
@@ -210,11 +181,11 @@ impl<T: FromValue> FromValue for Vec<T> {
     }
 }
 impl<T: IntoValue> IntoValue for Vec<T> {
-    unsafe fn into_value(self, mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, mem: &mut impl Memory) -> Result<Value> {
         let value = alloc_array(mem, self.len() as u32);
         let array = value.as_array();
         let mut dest = array.payload_addr();
-        for item in self.into_iter() {
+        for item in self {
             *dest = item.into_value(mem)?;
             dest = dest.add(1);
         }
@@ -223,12 +194,12 @@ impl<T: IntoValue> IntoValue for Vec<T> {
 }
 
 impl FromValue for () {
-    unsafe fn from_value(_value: Value, _mem: &mut impl Memory) -> MotokoResult<Self> {
+    unsafe fn from_value(_value: Value, _mem: &mut impl Memory) -> Result<Self> {
         Ok(())
     }
 }
 impl IntoValue for () {
-    unsafe fn into_value(self, _mem: &mut impl Memory) -> MotokoResult<Value> {
+    unsafe fn into_value(self, _mem: &mut impl Memory) -> Result<Value> {
         Ok(unit())
     }
 }
@@ -238,7 +209,7 @@ impl IntoValue for () {
 macro_rules! tuple_impl {
     ($len:expr; $($name:ident = $index:tt),+) => {
         impl<$($name: FromValue),+> FromValue for ($($name),+) {
-            unsafe fn from_value(value: Value, mem: &mut impl Memory) -> MotokoResult<Self> {
+            unsafe fn from_value(value: Value, mem: &mut impl Memory) -> Result<Self> {
                 const LENGTH: u32 = $len;
                 match value.tag() {
                     TAG_ARRAY => {
@@ -253,7 +224,7 @@ macro_rules! tuple_impl {
             }
         }
         impl<$($name: IntoValue),+> IntoValue for ($($name),+) {
-            unsafe fn into_value(self, mem: &mut impl Memory) -> MotokoResult<Value> {
+            unsafe fn into_value(self, mem: &mut impl Memory) -> Result<Value> {
                 const LENGTH: u32 = $len;
                 let value = alloc_array(mem, LENGTH);
                 let array = value.as_array();
@@ -265,29 +236,154 @@ macro_rules! tuple_impl {
     };
 }
 
+// Data structures
+
+/// Wrapper for representing a `Vec<u8>` value as a `Blob` in Motoko.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct BlobVec(Vec<u8>);
+
+impl BlobVec {
+    pub fn vec(self) -> Vec<u8> {
+        self.0
+    }
+}
+
+impl FromValue for BlobVec {
+    unsafe fn from_value(value: Value, _mem: &mut impl Memory) -> Result<Self> {
+        match value.tag() {
+            TAG_BLOB => {
+                let blob = value.as_blob();
+                let len = blob.len().as_u32();
+                Ok(BlobVec((0..len).into_iter().map(|i| blob.get(i)).collect()))
+            }
+            tag => Err(MotokoError::UnexpectedTag(tag)),
+        }
+    }
+}
+impl IntoValue for BlobVec {
+    unsafe fn into_value(self, mem: &mut impl Memory) -> Result<Value> {
+        self.vec().into_value(mem)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Blob(Value);
+
+impl Blob {
+    pub unsafe fn from_vec(vec: Vec<u8>, mem: &mut impl Memory) -> Result<Self> {
+        let value = alloc_blob(mem, Bytes(vec.len() as u32));
+        let blob = value.as_blob_mut();
+        let mut dest = blob.payload_addr();
+        for item in vec.into_iter() {
+            *dest = item;
+            dest = dest.add(1);
+        }
+        Ok(Blob(allocation_barrier(value)))
+    }
+}
+
+impl From<Value> for Blob {
+    fn from(value: Value) -> Self {
+        Blob(value)
+    }
+}
+impl From<Blob> for Value {
+    fn from(value: Blob) -> Self {
+        value.0
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Array<T>(Value, PhantomData<T>);
+
+impl<T> Array<T> {
+    pub unsafe fn from_vec(vec: Vec<T>, mem: &mut impl Memory) -> Result<Self>
+    where
+        T: IntoValue,
+    {
+        let value = alloc_array(mem, vec.len() as u32);
+        let array = value.as_array();
+        let mut dest = array.payload_addr();
+        for item in vec {
+            *dest = item.into_value(mem)?;
+            dest = dest.add(1);
+        }
+        Ok(Array::from(allocation_barrier(value)))
+    }
+
+    pub unsafe fn concat(slice: impl AsRef<[Array<T>]>, mem: &mut impl Memory) -> Array<T> {
+        let slice = slice.as_ref();
+        let length: u32 = slice.iter().map(|array| array.len()).sum();
+        let value = alloc_array(mem, length as u32);
+        let array = value.as_array();
+        let mut dest = array.payload_addr();
+        for array in slice {
+            for i in 0..array.len() {
+                *dest = array.get(i);
+                dest = dest.add(1);
+            }
+        }
+        Array::from(allocation_barrier(value))
+    }
+
+    pub unsafe fn len(&self) -> u32 {
+        self.0.as_array().len()
+    }
+
+    pub unsafe fn get(&self, index: u32) -> Value {
+        self.0.as_array().get(index)
+    }
+}
+
+impl<T> From<Value> for Array<T> {
+    fn from(value: Value) -> Self {
+        Array(value, PhantomData::default())
+    }
+}
+impl<T> From<Array<T>> for Value {
+    fn from(value: Array<T>) -> Self {
+        value.0
+    }
+}
+
+// impl<T> FromIterator<T> for Result<Array<Value>> {
+//     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+//        let vec= Vec::from_iter(iter);
+//     }
+// }
+
 // Temporary examples
 
 #[motoko]
-unsafe fn empty() {}
+fn empty() {}
 
 #[motoko]
-unsafe fn identity(value: Value) -> Value {
+fn identity(value: Value) -> Value {
     value
 }
 
 #[motoko]
-unsafe fn div_rem(a: u32, b: u32) -> (u32, u32) {
+fn div_rem(a: u32, b: u32) -> (u32, u32) {
     (a / b, a % b)
 }
 
 #[motoko]
-unsafe fn array_concat(a: Vec<Value>, b: Vec<Value>) -> Vec<Value> {
-    [a, b].concat()
+unsafe fn array_concat_fast(
+    a: Array<Value>,
+    b: Array<Value>,
+    #[memory] mem: &mut impl Memory,
+) -> Array<Value> {
+    Array::concat([a, b], mem)
 }
 
 #[motoko]
-unsafe fn blob_modify(mut blob: BlobVec) -> BlobVec {
-    blob.0.push('!' as u8);
+fn array_concat_slow(a: Vec<Value>, b: Vec<Value>) -> Vec<Value> {
+    [a, b].concat() // slower but more usable within the Rust ecosystem
+}
+
+#[motoko]
+fn blob_modify(mut blob: BlobVec) -> BlobVec {
+    blob.vec().push('!' as u8);
     blob
 }
 
@@ -305,12 +401,12 @@ unsafe fn manual_alloc(#[memory] mem: &mut impl Memory) -> Value {
 }
 
 #[motoko]
-unsafe fn bool_swap(a: bool, b: bool) -> (bool, bool) {
+fn bool_swap(a: bool, b: bool) -> (bool, bool) {
     (b, a)
 }
 
 #[motoko]
-unsafe fn check_numbers(
+fn check_numbers(
     a: u8,
     b: i8,
     c: u16,
