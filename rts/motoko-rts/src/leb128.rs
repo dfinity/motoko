afbcffb82f699cfaed14cb9cc97cedf844df006d
luc-blaeser <luc.blaeser@dfinity.org>
2024-04-03 09:22:51 +0200

    Adjust LEB128 encoding for combined mode

--- rts/motoko-rts/src/leb128.rs
+++ rts/motoko-rts/src/leb128.rs
@@ -1,5 +1,7 @@
 //! LEB1128 encoding. Reference: https://en.wikipedia.org/wiki/LEB128
 
+use motoko_rts_macros::uses_enhanced_orthogonal_persistence;
+
 use crate::buf::{read_byte, Buf};
 
 #[no_mangle]
@@ -48,9 +50,20 @@ pub unsafe fn leb128_decode_checked(buf: *mut Buf) -> Option<usize> {
 
         result |= ((byte & 0b0111_1111) as usize) << shift;
 
+
+        let overflow = if uses_enhanced_orthogonal_persistence!() {
+            // The 10th byte needs to be the last, and it must contribute at most 1 bit, otherwise we
+            // have an overflow.
+            shift == 63 && (byte & 0b1111_1110) != 0
+        } else {
+            // The 5th byte needs to be the last, and it must contribute at most 4 bits, otherwise we
+            // have an overflow.
+            shift == 28 && (byte & 0b1111_0000) != 0
+        };
+
         // The 10th byte needs to be the last, and it must contribute at most 1 bit, otherwise we
         // have an overflow
-        if shift == 63 && (byte & 0b1111_1110) != 0 {
+        if overflow {
             return None;
         }
 
@@ -81,7 +94,13 @@ pub unsafe fn sleb128_decode_checked(buf: *mut Buf) -> Option<isize> {
 
         // Overflow check ported from Wasm reference implementation:
         // https://github.com/WebAssembly/spec/blob/f9770eb75117cac0c878feaa5eaf4a4d9dda61f5/interpreter/binary/decode.ml#L89-L98
-        if shift == 63 && (byte & 0b0111_1111 != 0 && byte & 0b0111_1111 != 0b0111_1111) {
+        let overflow = if uses_enhanced_orthogonal_persistence!() {
+            shift == 63 && (byte & 0b0111_1111 != 0 && byte & 0b0111_1111 != 0b0111_1111)
+        } else {
+            shift == 28 && (byte & 0b0111_1000 != 0 && byte & 0b0111_1000 != 0b0111_1000)
+        };
+
+        if overflow {
             return None;
         }
 
@@ -93,7 +112,7 @@ pub unsafe fn sleb128_decode_checked(buf: *mut Buf) -> Option<isize> {
     };
 
     // Sign extend
-    if shift < 64 && last_byte & 0b0100_0000 != 0 {
+    if shift < usize::BITS && last_byte & 0b0100_0000 != 0 {
         result |= !0 << shift;
     }
 
