9ed2fed69c0eb0804e194608ecc0432ceb63fd3c
Jessie Mongeon <jessie.mongeon@dfinity.org>
2024-04-18 13:56:02 -0500

    fix links

--- nix/releaseVersion.nix
+++
@@ -1,9 +0,0 @@
-# Extracts the first version number in Changelog.md
-{ pkgs, officialRelease ? false }:
-let version =
-  builtins.head (builtins.head (builtins.filter (x : x != null) (
-    builtins.map (builtins.match "## ([0-9.]+).*") (
-      pkgs.lib.splitString "\n" (builtins.readFile ../Changelog.md)
-    )
-  ))); in
-if officialRelease then version else "${version}+"
