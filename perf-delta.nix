8ffebaf2247c63d92824f630eafaa2ca4140d7fc
luc-blaeser <luc.blaeser@dfinity.org>
2024-05-03 14:58:19 +0200

    More trace output in perf delta script

--- perf-delta.nix
+++ perf-delta.nix
@@ -37,11 +37,16 @@ let
         do
           # ignore all errors
           echo -n $file
-          if timeout 10s moc $file --omit-metadata motoko:compiler --force-gc --compacting-gc -no-check-ir -ref-system-api -o $file.wasm 2>/dev/null
-          then echo " ok"
-          else echo " failed (ignored)"
-          fi
+          moc $file --omit-metadata motoko:compiler --force-gc --compacting-gc -no-check-ir -ref-system-api -o $file.wasm
+
+
+          #if timeout 10s moc $file --omit-metadata motoko:compiler --force-gc --compacting-gc -no-check-ir -ref-system-api -o $file.wasm 2>/dev/null
+          #then echo " ok"
+          #else echo " failed (ignored)"
+          #fi
         done
+
+        find . -name \*.wasm -print
 
         if ! test -n "$(find . -name \*.wasm -print -quit)"
         then
