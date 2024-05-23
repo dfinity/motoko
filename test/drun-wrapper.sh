008fbc6afdf647fa97e72e6b32e7dca2036c5539
Martin Raszyk <martin.raszyk@dfinity.org>
2024-05-23 18:21:40 +0200

    queries

--- test/drun-wrapper.sh
+++ test/drun-wrapper.sh
@@ -31,6 +31,7 @@ BASE64ID="///////AAAABAQ=="
 INSTANCE_ID=$(curl -X POST -H 'Content-Type: application/json' http://localhost:8000/instances -d '{"nns": {"state_config": "New", "instruction_config": "Production", "dts_flag": "Enabled"}, "sns": {"state_config": "New", "instruction_config": "Production", "dts_flag": "Enabled"}, "ii": null, "fiduciary": null, "bitcoin": null, "system": [], "application": [{"state_config": "New", "instruction_config": "Production", "dts_flag": "Enabled"}]}' | jq -r ".Created.instance_id")
 PIC_URL="http://localhost:8000/instances/${INSTANCE_ID}"
 PIC_INGRESS="${PIC_URL}/update/execute_ingress_message"
+PIC_QUERY="${PIC_URL}/read/query"
 
 CREATE_PAYLOAD=$(echo "(record {})" | didc encode | xxd -r -p | base64 | tr -d '\n')
 
@@ -52,9 +53,13 @@ UPGRADE_PAYLOAD=$(echo "(record {
   arg=blob\"DIDL\00\00\";
 })" | didc encode | xxd -r -p | base64 | tr -d '\n')
 
+INGRESS_CMD='q#curl -X POST -H "Content-Type: application/json" "'"${PIC_INGRESS}"'" -d "{\"sender\": \"BA==\", \"canister_id\": \"'"${BASE64ID}"'\", \"effective_principal\": \"None\", \"method\": \"# . "$1" . q#\", \"payload\": \"# . "$2" . q#\"}"#'
+QUERY_CMD='q#curl -X POST -H "Content-Type: application/json" "'"${PIC_QUERY}"'" -d "{\"sender\": \"BA==\", \"canister_id\": \"'"${BASE64ID}"'\", \"effective_principal\": \"None\", \"method\": \"# . "$1" . q#\", \"payload\": \"# . "$2" . q#\"}"#'
+UPGRADE_CMD='q#curl -X POST -H "Content-Type: application/json" "'"${PIC_INGRESS}"'" -d "{\"sender\": \"BA==\", \"canister_id\": \"\", \"effective_principal\": {\"CanisterId\": \"'"${BASE64ID}"'\"}, \"method\": \"# . "install_code" . q#\", \"payload\": \"# . "'"${UPGRADE_PAYLOAD}"'" . q#\"}"#'
+
 if [ -n "$2" ]
 then
-  eval $(LANG=C perl -ne 'print q#curl -X POST -H "Content-Type: application/json" "'"${PIC_INGRESS}"'" -d "{\"sender\": \"BA==\", \"canister_id\": \"'"${BASE64ID}"'\", \"effective_principal\": \"None\", \"method\": \"# . "$2" . q#\", \"payload\": \"# . "$3" . q#\"}"# . " && " if m,^//CALL (ingress|query) ([^ ]*) (.*),;print q#curl -X POST -H "Content-Type: application/json" "'"${PIC_INGRESS}"'" -d "{\"sender\": \"BA==\", \"canister_id\": \"\", \"effective_principal\": {\"CanisterId\": \"'"${BASE64ID}"'\"}, \"method\": \"# . "install_code" . q#\", \"payload\": \"# . "'"${UPGRADE_PAYLOAD}"'" . q#\"}"# . " && " if m,^//CALL upgrade,;' $2) true
+  eval $(LANG=C perl -ne 'print '"${INGRESS_CMD}"' . " && " if m,^//CALL ingress ([^ ]*) (.*),;print '"${QUERY_CMD}"' . " && " if m,^//CALL query ([^ ]*) (.*),;print '"${UPGRADE_CMD}"' . " && " if m,^//CALL upgrade,;' $2) true
 fi
 
 PIC_CYCLES="${PIC_URL}/read/get_cycles"
