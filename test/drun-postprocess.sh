d6311443665a3b384c7673f57ca04ec94500f043
Martin Raszyk <martin.raszyk@dfinity.org>
2024-05-23 22:35:52 +0200

    fix

--- test/drun-postprocess.sh
+++ test/drun-postprocess.sh
@@ -3,7 +3,7 @@
 sed "s/^....-..-.. ..:..:............ UTC: \[Canister tqzl2-p7777-77776-aaaaa-cai\]/debug.print: /" | \
 sed "s/^{\"Ok\":{\"Reply\":\"\(.*\)\"}} ingress$/ingress Completed: Reply: \1/" | \
 sed "s/^{\"Ok\":{\"Reject\":\"\(.*\)\"}} ingress$/ingress Completed: Reject: \1/" | \
-sed "s/^{\"Err\":\(.*\)} ingress$/Err: \1/" | \
+sed "s/^{\"Err\":\(.*\)} ingress$/ingress Err: \1/" | \
 sed "s/^{\"Ok\":{\"Reply\":\"\(.*\)\"}} query$/Ok: Reply: \1/" | \
 sed "s/^{\"Ok\":{\"Reject\":\"\(.*\)\"}} query$/Ok: Reject: \1/" | \
 sed "s/^{\"Err\":\(.*\)} query$/Err: \1/"
