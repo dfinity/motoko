fbe345d3c1f854ed3f1bacdcec9f47d905841132
Leonid Vasilev <leonid.vasilev@serokell.io>
2024-05-24 00:08:58 +0400

    motoko-san: support labels, breaks, and continues

--- src/viper/common.ml
+++ src/viper/common.ml
@@ -3,3 +3,8 @@ exception Unsupported of Source.region * string
 
 let unsupported at sexp =
   raise (Unsupported (at, (Wasm.Sexpr.to_string 80 sexp)))
+
+let rec map_last ~f = function
+  | [] -> []
+  | [ x ] -> [ f x ]
+  | x :: xs -> x :: map_last ~f xs
