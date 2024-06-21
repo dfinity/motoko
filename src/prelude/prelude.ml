afa5831710a166d0a13daaf14056a7f048c7b844
Claudio Russo <claudio@dfinity.org>
2024-06-21 15:59:52 +0100

    Update src/prelude/prelude.ml

--- src/prelude/prelude.ml
+++ src/prelude/prelude.ml
@@ -3,6 +3,6 @@ let internals = [%blob "prelude/internals.mo"]
 let timers_api = [%blob "prelude/timers-api.mo"]
 let prim_module' = [%blob "prelude/prim.mo"]
 let prim_module ~timers:required =
-   if required
-   then prim_module' ^ timers_api
-   else prim_module'
+  if required
+  then prim_module' ^ timers_api
+  else prim_module'
