132b4b48a6515bcf3131b66bf0a95192bfa0a517
Gabor Greif <gabor@dfinity.org>
2024-07-25 10:13:10 +0200

    feat: `finally` to also clean up after trapping continuations (#4507)
    Introduces an optional `finally` clause to `try` expressions that can be used for disciplined (structured) resource management. The clause comprises a unit-valued expression (or block) that may not perform sends (or similar). It gets executed for all control paths that leave the `try`-expression, i.e. `return`, `break`, etc. code paths. For nested `try` expressions the cleanups are performed in innermost to outermost order.
    
    For last-resort cleanups (i.e. code paths trapping after an `await`) the replica invokes the callback registered as `ic0.call_on_cleanup`, and this will result in the execution of (exclusively) the `finally` blocks in (dynamic) scope. This is a new mechanism in Motoko, which was not possible to achieve earlier, and puts certain resource deallocations (e.g. of acquired locks) under the programmer's control.
    
    -----------------
    TODO:
    - [ ] [invoke cleanup from the interpreter](https://github.com/dfinity/motoko/pull/4507/files/51bb8a1933bbf2c509447032f1ba26b8f91fa4e0#r1658678631)
    - [ ] make `catch` optional (non-trapping when missing) — #4600
      - well, this was not really needed as pattern-match failure would just re-`throw`, so we were fine. Optimised, though.
    - [x] remaining FIXMEs (`arrange.ml`, etc.)
    - [x] `Triv` in `try` (respect edges)
      - [x] fix fallout
    - [x] forbid control-flow edges out of `finally` blocks
    - [x] remove all aliasing from IR
      - [ ] use `meta` (would `assert` currently, thus causing minor code duplication) — could be follow-up PR
    - [x] handle `Throw` and regular continuations in `await.ml` (instead of desugaring)
      - [x] adjust (AST, IR)-interpreter
      - ❗️this is necessary when there is no `catch`, as the exception will escape
    - how are label continuations invoked in the backend? (I need to know for the _last-resort callback_)
      - have a new `context` key sort `Cleanup`, stack up continuations of finally blocks only
      - extend the `kr` in `await.ml` to `krc` (`CPSAwait`), pass `Cleanup` continuations as `c`
      - add new primitive to compiler to set the last-resort field (not needed)
      - in `async.ml` lower `CPSAwait` to that too
      - [x] adapt `check_ir.ml`, etc.
    - [x] tests with `await*`
    - [x] fix up the syntax part
    - `do` ... `finally` (when there is no `catch` clause necessary)
      - ~means: `TryE` needs `cases list option`~
    - type checking for the `finally` clause
      -  [x] unit result
      -  [x] trivial effect (actually Claudio made this capability-based)
      -  [ ] that the `try` block has `await` effect — nope!
    - [x] other type than unit for `try`
    - `OtherPrim "call_raw"` + tests — nope!
    - `cps_asyncE` similarly to `CallPrim/3` — nope!
    - highlighting of `finally`
      - [x] for `emacs`
      - [x] for VSCode → https://github.com/dfinity/vscode-motoko/pull/288
      - [x] GitHub highlighter
    - [x] Changelog (breaking change, new keyword)
    - [x] Docs
    - [ ] adapt best practices: https://internetcomputer.org/docs/current/developer-docs/security/security-best-practices/inter-canister-calls#recommendation
    - [ ] adapt Motoko examples where locking happens

--- src/ir_def/construct.ml
+++ src/ir_def/construct.ml
@@ -15,7 +15,7 @@ let nextN = "next"
 
 (* Identifiers *)
 
-type var = (string * T.typ)
+type var = id * T.typ
 
 let var id typ = (id, typ)
 
@@ -117,8 +117,7 @@ let primE prim es =
     | OtherPrim "is_controller" -> T.bool
     | _ -> assert false (* implement more as needed *)
   in
-  let effs = List.map eff es in
-  let eff = List.fold_left max_eff T.Triv effs in
+  let eff = List.(map eff es |> fold_left max_eff T.Triv) in
   { it = PrimE (prim, es);
     at = no_region;
     note = Note.{ def with typ; eff }
@@ -183,22 +182,20 @@ let ic_rejectE e =
     note = Note.{ def with typ = T.unit; eff = eff e }
   }
 
-let ic_callE f e k r =
-  let es = [f; e; k; r] in
-  let effs = List.map eff es in
-  let eff = List.fold_left max_eff T.Triv effs in
+let ic_callE f e k r c =
+  let es = [f; e; k; r; c] in
+  let eff = List.(map eff es |> fold_left max_eff T.Triv) in
   { it = PrimE (ICCallPrim, es);
     at = no_region;
-    note = Note.{ def with typ = T.unit; eff = eff }
+    note = Note.{ def with typ = T.unit; eff }
   }
 
-let ic_call_rawE p m a k r =
-  let es = [p; m; a; k; r] in
-  let effs = List.map eff es in
-  let eff = List.fold_left max_eff T.Triv effs in
+let ic_call_rawE p m a k r c =
+  let es = [p; m; a; k; r; c] in
+  let eff = List.(map eff es |> fold_left max_eff T.Triv) in
   { it = PrimE (ICCallRawPrim, es);
     at = no_region;
-    note = Note.{ def with typ = T.unit; eff = eff }
+    note = Note.{ def with typ = T.unit; eff }
   }
 
 (* tuples *)
@@ -245,9 +242,8 @@ let blockE decs exp =
   match decs' with
   | [] -> exp
   | _ ->
-    let es = List.map dec_eff decs' in
     let typ = typ exp in
-    let eff =  List.fold_left max_eff (eff exp) es in
+    let eff =  List.(map dec_eff decs' |> fold_left max_eff (eff exp)) in
     { it = BlockE (decs', exp);
       at = no_region;
       note = Note.{ def with typ; eff }
@@ -313,7 +309,7 @@ let funcE name sort ctrl typ_binds args typs exp =
   }
 
 let callE exp1 typs exp2 =
-  let typ =  match T.promote (typ exp1) with
+  let typ = match T.promote (typ exp1) with
     | T.Func (_sort, control, _, _, ret_tys) ->
       T.codom control (fun () -> List.hd typs) (List.map (T.open_ typs) ret_tys)
     | T.Non -> T.Non
@@ -412,7 +408,7 @@ let switch_variantE exp1 cases typ1 =
     at = no_region;
     note = Note.{ def with
       typ = typ1;
-      eff = List.fold_left max_eff (eff exp1) (List.map (fun (l,p,e) -> eff e) cases)
+      eff = List.(map (fun (l,p,e) -> eff e) cases |> fold_left max_eff (eff exp1))
     }
   }
 
@@ -436,14 +432,13 @@ let switch_textE exp1 cases (pat, exp2) typ1 =
     note = Note.{
       def with
       typ = typ1;
-      eff = List.fold_left max_eff (eff exp1) (List.map (fun c -> eff c.it.exp) cs)
+      eff = List.(map (fun c -> eff c.it.exp) cs |> fold_left max_eff (eff exp1))
     }
   }
 
 
 let tupE exps =
-  let effs = List.map eff exps in
-  let eff = List.fold_left max_eff T.Triv effs in
+  let eff = List.(map eff exps |> fold_left max_eff T.Triv) in
   { it = PrimE (TupPrim, exps);
     at = no_region;
     note = Note.{ def with typ = T.Tup (List.map typ exps); eff };
@@ -632,17 +627,18 @@ let contT typ ans_typ = T.(Func (Local, Returns, [], as_seq typ, as_seq ans_typ)
 
 let err_contT ans_typ =  T.(Func (Local, Returns, [], [catch], as_seq ans_typ))
 
+let bail_contT = T.(contT unit unit) (* when `await`ing *)
+
+let clean_contT = bail_contT (* last-resort replica callback *)
+
 let answerT typ : T.typ =
   match typ with
   | T.Func (T.Local, T.Returns, [], ts1, ts2) -> T.seq ts2
   | _ -> assert false
 
-let cpsT typ ans_typ = T.(Func (Local, Returns, [], [contT typ ans_typ; err_contT ans_typ], as_seq ans_typ))
-
 (* Sequence expressions *)
 
-let seqE es =
-  match es with
+let seqE = function
   | [e] -> e
   | es -> tupE es
 
@@ -668,7 +664,7 @@ let forall tbs e =
  let cs = List.map (fun tb -> tb.it.con) tbs in
  match e.it, e.note.Note.typ with
  | FuncE (n, s, c1, [], xs, ts, exp),
-   T.Func ( _, c2, [], ts1, ts2) ->
+   T.Func (_, c2, [], ts1, ts2) ->
    { e with
      it = FuncE(n, s, c1, tbs, xs, ts, exp);
      note = Note.{ e.note with
@@ -678,6 +674,14 @@ let forall tbs e =
      }
    }
  | _ -> assert false
+
+(* changing display name of e.g. local lambda *)
+let named displ e =
+  match e.it with
+  | FuncE (_, s, c1, [], xs, ts, exp)
+    -> { e with it = FuncE (displ, s, c1, [], xs, ts, exp) }
+  | _ -> assert false
+
 
 (* Lambda application (monomorphic) *)
 
