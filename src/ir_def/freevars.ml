58c81b9830ec3aaf076dce64fdfb4ab2adbbf9ab
Gabor Greif <gabor@dfinity.org>
2024-08-14 17:43:42 +0200

    elim FIXMEs

--- src/ir_def/freevars.ml
+++ src/ir_def/freevars.ml
@@ -119,7 +119,7 @@ let rec exp e : f = match e.it with
   | ActorE (ds, fs, u, _)  -> actor ds fs u
   | NewObjE (_, fs, _)  -> fields fs
   | TryE (e, cs, cl)    -> exp e ++ cases cs ++ (match cl with Some (v, _) -> id v | _ -> M.empty)
-  | SelfCallE (_FIXME, _, e1, e2, e3, e4) -> under_lambda (exp e1) ++ exps [e2; e3; e4]
+  | SelfCallE (par, _, e1, e2, e3, e4) -> under_lambda (exp e1) ++ exps [par; e2; e3; e4]
 
 and actor ds fs u = close (decs ds +++ fields fs +++ system u)
 
