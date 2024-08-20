edb23df2c72e5db9792c9ba8dc659d4de39b2059
Luc Blaeser <112870813+luc-blaeser@users.noreply.github.com>
2024-08-20 15:51:07 +0200

    Merge Preparation: Latest IC with Integrated EOP  (#4638)
    * Adjust to new system API
    
    * Port to latest IC 64-bit system API
    
    * Update to new IC with Wasm64
    
    * Updating nix hashes
    
    * Update IC dependency (Wasm64 enabled)
    
    * Update expected test results
    
    * Fix migration test
    
    * Use latest `drun`
    
    * Adjust expected test results
    
    * Updating nix hashes
    
    * Update expected test results
    
    * Fix `drun` nix build for Linux
    
    * Disable DTS in `drun`, refactor `drun` patches
    
    * Update expected test results for new `drun`
    
    * Limiting amount of stable memory accessed per graph copy increment
    
    * Reformat
    
    * Manual merge conflict resolution
    
    * Manual merge conflict resolution
    
    * Adjust expected test result
    
    ---------
    
    Co-authored-by: Nix hash updater <41898282+github-actions[bot]@users.noreply.github.com>

--- rts/motoko-rts/src/stabilization/graph_copy/limit.rs
+++ rts/motoko-rts/src/stabilization/graph_copy/limit.rs
@@ -1,7 +1,18 @@
-use crate::{constants::KB, stabilization::ic0_performance_counter};
+use crate::{
+    constants::{GB, KB},
+    stabilization::ic0_performance_counter,
+};
 
-/// Instruction limit for the graph copy increment.
-/// Monitoring the message instruction counter.
+/// Maximum amount of memory that is processed per increment.
+/// The IC configures a maximum of 2 GB of stable memory that can
+/// be accessed per message. We keep a conservative reserve of 1 GB.
+const MEMORY_PROCESSING_LIMIT_PER_INCREMENT: u64 = GB as u64;
+
+/// Execution limit for the graph copy increment.
+/// Monitoring the message instruction counter and
+/// the amount of processed memory.
+/// The latter is relevant to avoid exceeding the limit
+/// of how much stable memory be accessed in a message.
 /// Optimization: Avoiding frequent repeated calls to
 /// `ic0_performance_counter()` as this incurs 200
 /// instructions itself.
@@ -18,11 +29,13 @@ use crate::{constants::KB, stabilization::ic0_performance_counter};
 /// Once the limit has been exceeded, the heuristics
 /// continuously returns exceeded until the monitoring
 /// is reset.
-pub struct InstructionLimit {
+pub struct ExecutionLimit {
     /// Instruction counter at the beginning of the measurement.
-    start: u64,
+    initial_instruction_counter: u64,
     /// Limit of processed instructions since measurment start.
-    limit: u64,
+    instruction_limit: u64,
+    /// Amount of processed memory before the measurement.
+    initial_processed_memory: u64,
     // Only used for sporadic synchronization heuristics:
     /// Number of `is_exceeded` calls since the last instruction
     /// counter synchronization.
@@ -34,7 +47,7 @@ pub struct InstructionLimit {
     exceeded: bool,
 }
 
-impl InstructionLimit {
+impl ExecutionLimit {
     /// Threshold on the number of `is_exceeded` calls since the
     /// last instruction counter synchronization.
     const CALL_THRESHOLD: usize = 1_000;
@@ -42,10 +55,11 @@ impl InstructionLimit {
     /// instruction counter synchronization.
     const MEMORY_THRESHOLD: u64 = 256 * KB as u64;
 
-    pub fn new(limit: u64) -> InstructionLimit {
-        InstructionLimit {
-            start: Self::instruction_counter(),
-            limit,
+    pub fn new(instruction_limit: u64) -> ExecutionLimit {
+        ExecutionLimit {
+            initial_instruction_counter: Self::instruction_counter(),
+            instruction_limit,
+            initial_processed_memory: 0,
             call_counter: 0,
             last_processed: 0,
             exceeded: false,
@@ -53,6 +67,13 @@ impl InstructionLimit {
     }
 
     pub fn is_exceeded(&mut self, processed_memory: u64) -> bool {
+        debug_assert!(self.initial_processed_memory <= processed_memory);
+        // Check the memory limit.
+        if processed_memory - self.initial_processed_memory > MEMORY_PROCESSING_LIMIT_PER_INCREMENT
+        {
+            return true;
+        }
+        // Check the instruction limit.
         // Sporadic instruction counter synchronization, see above.
         self.call_counter += 1;
         if processed_memory >= self.last_processed.saturating_add(Self::MEMORY_THRESHOLD)
@@ -63,15 +84,16 @@ impl InstructionLimit {
             self.last_processed = processed_memory;
             // Check actual instruction counter.
             let current = Self::instruction_counter();
-            debug_assert!(self.start <= current);
-            let elapsed = current - self.start;
-            self.exceeded = elapsed > self.limit;
+            debug_assert!(self.initial_instruction_counter <= current);
+            let elapsed = current - self.initial_instruction_counter;
+            self.exceeded = elapsed > self.instruction_limit;
         }
         self.exceeded
     }
 
-    pub fn reset(&mut self, limit: u64) {
-        *self = Self::new(limit);
+    pub fn reset(&mut self, instruction_limit: u64, processed_memory: u64) {
+        *self = Self::new(instruction_limit);
+        self.initial_processed_memory = processed_memory;
     }
 
     fn instruction_counter() -> u64 {
