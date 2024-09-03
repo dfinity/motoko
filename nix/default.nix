735ea5f3c935b703c901f43e09270fc7fc54f93a
luc-blaeser <luc.blaeser@dfinity.org>
2024-09-03 14:34:18 +0200

    Further simplify Wasm linking

--- nix/default.nix
+++ nix/default.nix
@@ -95,9 +95,7 @@ let
         in rec {
           rustc-nightly = rust-channel.rust.override {
             targets = [
-               "wasm32-unknown-emscripten"
                "wasm32-wasi"
-               "i686-unknown-linux-gnu"
             ];
             extensions = ["rust-src"];
           };
