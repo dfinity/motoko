bd845da9e4a4f87b803a64e26f77476cf3662c52
Claudio Russo <claudio@dfinity.org>
2024-11-29 13:50:26 +0000

    Update nix/default.nix
    revert default.nix change to adtgen

--- nix/default.nix
+++ nix/default.nix
@@ -77,6 +77,9 @@ let
                   sha256 = "1kp72yv4k176i94np0m09g10cviqp2pnpm7jmiq6ik7fmmbknk7c";
                 };
               };
+
+              # No testing of atdgen, as it pulls in python stuff, tricky on musl
+              atdgen = super.ocamlPackages.atdgen.overrideAttrs { doCheck = false; };
             };
           }
         )
