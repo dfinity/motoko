309a2316c4692cacff50228e46b7ba2c726c9fbd
Gabor Greif <gabor@dfinity.org>
2024-12-03 18:00:37 +0100

    fix `cps_asyncE` for the `AsyncE` (freestanding) case

--- src/ir_passes/await.ml
+++ src/ir_passes/await.ml
@@ -455,8 +455,8 @@ and c_exp' context exp k =
       cps_asyncE T.Fut typ1 (match par_opt with
                              | Some par when T.(sub (typ par) (Obj (Object, [{ lab = "cycles"; typ = nat; src = empty_src}])))
                                -> optE par
-                             | None -> primE ICCyclesPrim []
-                             | Some _ -> nullE ()) (typ exp1)
+                             (*| Some _ -> FIXME: pass 0 cycles: optE (recE [cycles = 0])*)
+                             | _ -> nullE ()) (typ exp1)
         (forall [tb] ([k_ret; k_fail; k_clean] -->*
           (c_exp context' exp1 (ContVar k_ret)))) in
     let k' = meta (typ cps_async)
