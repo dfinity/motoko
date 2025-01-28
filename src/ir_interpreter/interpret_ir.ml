ae41e7b2a725641f3aca85b61ddaabd5e79fc04b
Gabor Greif <gabor@dfinity.org>
2025-01-28 21:03:20 +0100

    Update src/ir_interpreter/interpret_ir.ml
    Co-authored-by: Claudio Russo <claudio@dfinity.org>

--- src/ir_interpreter/interpret_ir.ml
+++ src/ir_interpreter/interpret_ir.ml
@@ -527,7 +527,7 @@ and interpret_exp_mut env exp (k : V.value V.cont) =
         let env' = { env with labs = V.Env.empty; rets = Some k'; throws = Some r }
         in interpret_exp env' exp1 k')
       k
-  | AsyncE (_, Type.Cmp, _, exp1, _) ->
+  | AsyncE (_FIXME, Type.Cmp, _, exp1, _) ->
     assert env.flavor.has_await;
     k (V.Comp (fun k' r ->
       let env' = { env with labs = V.Env.empty; rets = Some k'; throws = Some r }
