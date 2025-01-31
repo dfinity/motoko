d48ca0ff1006fbe74308adac062ae52dc7891db8
Claudio Russo <claudio@dfinity.org>
2025-01-31 19:14:20 +0000

    Apply suggestions from code review

--- src/mo_frontend/source_token.ml
+++ src/mo_frontend/source_token.ml
@@ -137,8 +137,8 @@ let to_parser_token :
   | DISALLOWED -> Ok Parser.DISALLOWED
   | LET -> Ok Parser.LET
   | VAR -> Ok Parser.VAR
-  | RPAR -> Ok Parser.RPAR
   | LPAR -> Ok Parser.LPAR
+  | RPAR -> Ok Parser.RPAR
   | LBRACKET -> Ok Parser.LBRACKET
   | RBRACKET -> Ok Parser.RBRACKET
   | LCURLY -> Ok Parser.LCURLY
