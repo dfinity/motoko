53fcfa86330ff39fb9c06eb86cd742fe94464e25
Claudio Russo <claudio@dfinity.org>
2025-03-13 15:11:57 +0000

    Update src/ir_passes/erase_typ_field.ml

--- src/ir_passes/erase_typ_field.ml
+++ src/ir_passes/erase_typ_field.ml
@@ -56,7 +56,8 @@ let transform prog =
     | Any -> Any
     | Non -> Non
     | Pre -> Pre
-    | Named (n , t) -> t_typ t (*Named (n, t_typ t) (* erase instead *) *)
+    | Named (n , t) -> (* erased! *)
+      t_typ t 
     | Typ c -> assert false (* second class *)
 
   and t_bind tb =
