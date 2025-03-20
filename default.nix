5c9311e087e77d969cd5e7a144f2c0cce738a9db
Gabor Greif <gabor@dfinity.org>
2025-03-20 15:29:27 +0100

    try `doInstallCheck`

--- default.nix
+++ default.nix
@@ -156,14 +156,21 @@ let ocaml_exe = name: bin: rts:
         ldd $(which patchelf)
         ls -l $out/bin/*
         chmod +w $out/bin/*
-        patchelf --set-interpreter "/nix/store/034vzdhdia1f3dn6zv65xxishb1nnzff-musl-1.2.3/lib/ld-musl-aarch64.so.1" $out/bin/* || true
+        #patchelf --set-interpreter "/nix/store/034vzdhdia1f3dn6zv65xxishb1nnzff-musl-1.2.3/lib/ld-musl-aarch64.so.1" $out/bin/* || true
         ldd $out/bin/* || true
         file $out/bin/*
         ls -l $out/bin/*
         chmod a-w $out/bin/*
         ls -l $out/bin/*
-        $out/bin/* --help >/dev/null
+        #$out/bin/* --help >/dev/null
         echo /installPhase
+      '';
+
+      doInstallCheck = true;
+      installCheckPhase = ''
+        ls -l $out/bin/*
+        $out/bin/* --help >/dev/null
+        echo /installCheckPhase
       '';
     };
 in
