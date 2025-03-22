db777538f7a52304df1e9e8da2d039cf9168f58c
Gabor Greif <gabor@dfinity.org>
2025-03-22 02:10:51 +0100

    just jump to 20

--- default.nix
+++ default.nix
@@ -322,7 +322,7 @@ rec {
     };
 
     testDerivationDeps =
-      (with nixpkgs; [ wabt bash perl getconf moreutils nodejs_22 ]) ++
+      (with nixpkgs; [ wabt bash perl getconf moreutils nodejs_20 ]) ++
       [ filecheck wasmtime ];
 
 
@@ -553,7 +553,7 @@ rec {
         buildInputs = commonBuildInputs nixpkgs ++ [
           nixpkgs.ocamlPackages.js_of_ocaml
           nixpkgs.ocamlPackages.js_of_ocaml-ppx
-          nixpkgs.nodejs_22
+          nixpkgs.nodejs_20
           nixpkgs.nodePackages.terser
         ];
         buildPhase = ''
