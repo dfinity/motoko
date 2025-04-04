6597fb493b60f7a2f1649723ab38a04408018dd4
Claudio Russo <claudio@dfinity.org>
2025-04-04 02:30:25 +0200

    feat: preserve named types in semantic types and when translating between Motoko and Candid (#4943)
    Inspired by discussion with  @giorgiopiatti-dfinity 
    
    
    Both Motoko and Candid support syntactically named types for more informative type  (especially function) signatures.
    
    Eg. 
    ```
    (x: Int, y:Int) -> (z:int) 
    ```
    vs.
    ```
    (Int, Int) -> (Int)
    ```
    These are purely syntactic and erased from the syntax before type checking. They are not preserved by type checking nor by translation to Candid.
    
    This PR attempts to preserve name types throughout front-end type checking to produce better Candid and more informative types. Named types are erased, along with type fields, in early IR pass erase_typ_fields, so most ir passes are unaffected.
    
    This requires fairly extensive surgery to ignore names in various operations on types, e.g. sub-typing and type equivalence.
    
    Example Candid output:
    
    names.mo:
    ```
    actor {
    
      /// named
      public func named(x:Nat, y:Nat) : async (r : Nat) {
        x + y
      };
    
      /// anon
      public func anon(_ : Nat, _ : Nat) : async Nat {
        0
      };
    
      /// record
      public func record({x:Nat; y:Nat}) : async {r:Nat} {
        {r = x + y}
      };
    
      /// function arg/ret
      public func f(named : shared (x:Nat, y:Nat) -> async (r : Nat)) :
         async (unamed : shared (Nat, Nat) -> async Nat) {
        named
      };
    
      /// actor arg/ret
      public func g(A :
        actor {
          named : shared (x:Nat, y:Nat) -> async (r : Nat);
          anon : shared (Nat, Nat) -> async Nat
         }) :
         async (actor {
          named : shared (Nat, Nat) -> async Nat;
          anon : shared (x:Nat, y:Nat) -> async (r : Nat)
         }) {
        A
      };
    
      /// escape candid keywords
      public func escape(int : Int, bool : Bool, service : actor {}) :
       async (int : Int, bool : Bool, service : actor {}) {
        (int, bool, service)
      }
    }
    ```
    now produces Candid:
    
    ```
    service : {
      /// anon
      anon: (nat, nat) -> (nat);
      /// escape candid keywords
      escape: ("int": int, "bool": bool, "service": service {}) -> 
        ("int": int, "bool":  bool, "service": service { });
      /// function arg/ret
      f: (named: func (x: nat, y: nat) -> (r: nat)) -> 
           (unamed: func (nat, nat) -> (nat));
      /// actor arg/ret
      g: (A:
       service {
         anon: (nat, nat) -> (nat);
         named: (x: nat, y: nat) -> (r: nat);
       }) ->
       (service {
          anon: (x: nat, y: nat) -> (r: nat);
          named: (nat, nat) -> (nat);
        });
      /// named
      named: (x: nat, y: nat) -> (r: nat);
      /// record
      "record": (record {
                   x: nat;
                   y: nat;
                 }) -> (record {r: nat;});
    }
    ```
    In comparison, master branch produces:
    
    ```
    service : {
      /// anon
      anon: (nat, nat) -> (nat);
      /// escape candid keywords
      escape: (int, bool, service {
                          }) -> (int, bool, service {
                                            });
      /// function arg/ret
      f: (func (nat, nat) -> (nat)) -> (func (nat, nat) -> (nat));
      /// actor arg/ret
      g: (service {
            anon: (nat, nat) -> (nat);
            named: (nat, nat) -> (nat);
          }) -> (service {
                   anon: (nat, nat) -> (nat);
                   named: (nat, nat) -> (nat);
                 });
      /// named
      named: (nat, nat) -> (nat);
      /// record
      "record": (record {
                   x: nat;
                   y: nat;
                 }) -> (record {r: nat;});
    }
    ```
    
    
    To avoid information overload, we suppress names under object fields, tags and options.
    
    TODO:
    [x] ~add space before around colon~ (current is consistent with current formatting of candid fields/tags)
    [x] suppress names under object fields, tags and options (too much information)
    
    NOTE: If we think this is providing too much info in ordinary error messages, a less  radical approach would be to suppress names on output of Motoko types (always/sometimes), and only use them when translating to Candid.

--- src/mo_types/type.ml
+++ src/mo_types/type.ml
@@ -1,6 +1,7 @@
 (* Representation *)
 type lab = string
 type var = string
+type name = string
 
 type control =
   | Returns        (* regular local function or one-shot shared function *)
@@ -56,6 +57,7 @@ and typ =
   | Any                                       (* top *)
   | Non                                       (* bottom *)
   | Typ of con                                (* type (field of module) *)
+  | Named of name * typ                       (* named type *)
   | Pre                                       (* pre-type *)
 
 and scope = typ
@@ -132,6 +134,7 @@ let tag = function
   | Non -> 12
   | Pre -> 13
   | Typ _ -> 14
+  | Named _ -> 15
 
 let compare_prim p1 p2 =
   let d = tag_prim p1 - tag_prim p2 in
@@ -220,6 +223,10 @@ let rec compare_typ (t1 : typ) (t2 : typ) =
   | Non, Non
   | Pre, Pre -> 0
   | Typ c1, Typ c2 -> Cons.compare c1 c2
+  | Named (n1, t1), Named (n2, t2) ->
+    (match String.compare n1 n2 with
+     | 0 -> compare_typ t1 t2
+     | ord -> ord)
   | _ -> Int.compare (tag t1) (tag t2)
 
 and compare_tb tb1 tb2 =
@@ -420,6 +427,7 @@ let rec shift i n t =
   | Non -> Non
   | Pre -> Pre
   | Typ c -> Typ c
+  | Named (name, t) -> Named (name, shift i n t)
 
 and shift_bind i n tb =
   {tb with bound = shift i n tb.bound}
@@ -471,6 +479,7 @@ let rec subst sigma t =
                       type parameter cannot mention that parameter
                       (but can mention other (closed) type constructors).
                     *)
+  | Named (name, t) -> Named (name, subst sigma t)
 
 and subst_bind sigma tb =
   { tb with bound = subst sigma tb.bound}
@@ -521,6 +530,7 @@ let rec open' i ts t =
   | Non -> Non
   | Pre -> Pre
   | Typ c -> Typ c
+  | Named (name, t) -> Named (name, open' i ts t)
 
 and open_bind i ts tb  =
   {tb with bound = open' i ts tb.bound}
@@ -565,12 +575,14 @@ let rec normalize = function
     | _ -> t
     )
   | Mut t -> Mut (normalize t)
+  | Named (_, t) -> normalize t
   | t -> t
 
 let rec promote = function
   | Con (con, ts) ->
     let Def (tbs, t) | Abs (tbs, t) = Cons.kind con
     in promote (reduce tbs t ts)
+  | Named (_, t) -> promote t
   | t -> t
 
 
@@ -730,6 +742,7 @@ let rec span = function
   | Mut t -> span t
   | Non -> Some 0
   | Typ _ -> Some 1
+  | Named (_, t) -> span t
 
 
 (* Collecting type constructors *)
@@ -761,6 +774,9 @@ let rec cons' inTyp t cs =
     else
       (* don't add c unless mentioned in Cons.kind c *)
       cons_kind' inTyp (Cons.kind c) cs
+  | Named (_ , t) ->
+    cons' inTyp t cs
+
 
 and cons_con inTyp c cs =
   if ConSet.mem c cs
@@ -815,6 +831,7 @@ let concrete t =
         List.for_all go (List.map (open_ ts) ts2)
       | Typ c -> (* assumes type defs are closed *)
         true (* so we can transmit actors with typ fields *)
+      | Named (_, t) -> go t
     end
   in go t
 
@@ -846,6 +863,7 @@ let serializable allow_mut t =
          | Object | Memory -> List.for_all (fun f -> go f.typ) fs)
       | Variant fs -> List.for_all (fun f -> go f.typ) fs
       | Func (s, c, tbs, ts1, ts2) -> is_shared_sort s
+      | Named (n, t) -> go t
     end
   in go t
 
@@ -880,6 +898,7 @@ let find_unshared t =
         if is_shared_sort s
         then None
         else Some t
+      | Named (n, t) -> go t
     end
   in go t
 
@@ -967,6 +986,10 @@ let rec rel_typ d rel eq t1 t2 =
     true
   | Non, _ when rel != eq ->
     true
+  | Named (_n, t1'), t2 ->
+    rel_typ d rel eq t1' t2
+  | t1, Named (_n, t2') ->
+    rel_typ d rel eq t1 t2'
   | Con (con1, ts1), Con (con2, ts2) ->
     (match Cons.kind con1, Cons.kind con2 with
     | Def (tbs, t), _ -> (* TBR this may fail to terminate *)
@@ -1156,6 +1179,8 @@ let rec compatible_typ co t1 t2 =
     compatible_typ co t12 t22
   | Func _, Func _ ->
     true
+  | Named _, _
+  | _, Named _ -> assert false
   | _, _ ->
     false
   end
@@ -1201,11 +1226,12 @@ let rec inhabited_typ co t =
   | Variant tfs -> List.exists (inhabited_field co) tfs
   | Var _ -> true  (* TODO(rossberg): consider bound *)
   | Con (c, ts) ->
-    match Cons.kind c with
+    (match Cons.kind c with
     | Def (tbs, t') -> (* TBR this may fail to terminate *)
       inhabited_typ co (open_ ts t')
     | Abs (tbs, t') ->
-      inhabited_typ co t'
+      inhabited_typ co t')
+  | Named _ -> assert false
   end
 
 and inhabited_field co tf = inhabited_typ co tf.typ
@@ -1229,6 +1255,7 @@ let rec singleton_typ co t =
   | Variant _ -> false
   | Var _ -> false
   | Con _ -> false
+  | Named _ -> assert false
   end
 
 and singleton_field co tf = singleton_typ co tf.typ
@@ -1324,6 +1351,14 @@ let rec combine rel lubs glbs t1 t2 =
         in
         set_kind c (Def ([], t'));
         t'
+    | Named (n1, t1'), Named (n2, t2') ->
+      if n1 = n2 then
+        Named (n1, combine rel lubs glbs t1' t2')
+      else
+        combine rel lubs glbs t1' t2'
+    | Named (_, t), t'
+    | t, Named (_, t') ->
+      combine rel lubs glbs t t'
     | _, _ ->
       if rel == lubs then Any else Non
 
@@ -1608,6 +1643,7 @@ and can_omit n t =
       List.for_all (go i') ts1 &&
       List.for_all (go i') ts2
     | Typ c -> true (* assumes type defs are closed *)
+    | Named (n, t) -> go i t
   in go n t
 
 let rec pp_typ_obj vs ppf o =
@@ -1627,11 +1663,19 @@ and pp_typ_variant vs ppf fs =
     fprintf ppf "@[<hv 2>{@;<0 0>%a@;<0 -2>}@]"
       (pp_print_list ~pp_sep:semi (pp_tag vs)) fs
 
+and pp_typ_item vs ppf t =
+  match t with
+  | Named (n, t) ->
+    fprintf ppf "@[<1>%s : %a@]" n (pp_typ' vs) t
+  | typ -> pp_typ' vs ppf t
+
 and pp_typ_nullary vs ppf t =
   match t with
+  | Named (n, t) ->
+    fprintf ppf "@[<1>(%s : %a)@]" n (pp_typ' vs) t
   | Tup ts ->
     fprintf ppf "@[<1>(%a%s)@]"
-      (pp_print_list ~pp_sep:comma (pp_typ' vs)) ts
+      (pp_print_list ~pp_sep:comma (pp_typ_item vs)) ts
       (if List.length ts = 1 then "," else "")
   | Pre -> pr ppf "???"
   | Any -> pr ppf "Any"
