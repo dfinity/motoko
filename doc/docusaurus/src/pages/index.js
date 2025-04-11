2f8feecbc442d18ef932d604728bae914f66a1e0
Jessie Mongeon <jessie.mongeon@dfinity.org>
2025-04-11 16:12:24 -0500

    revise ICP features pages

--- doc/docusaurus/src/pages/index.js
+++ doc/docusaurus/src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/meet-motoko">
+            to="/home">
             Docs
           </Link>
         </div>
