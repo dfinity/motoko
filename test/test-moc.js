176b6c12cd42828b9fcadca89020c081d5e9f90f
Claudio Russo <claudio@dfinity.org>
2025-04-16 17:43:14 +0100

    update some tests

--- test/test-moc.js
+++ test/test-moc.js
@@ -150,7 +150,7 @@ assert.deepStrictEqual(Motoko.check("bad.mo"), {
 // Run interpreter
 assert.deepStrictEqual(Motoko.run([], "actor.mo"), {
   stdout: "`ys6dh-5cjiq-5dc` :\n" +
-    "  actor {main : shared query () -> async A__0__actor2Emo<Text__0__prelude>}\n",
+    "  actor {main : shared query () -> async A<Text>}\n",
   stderr: "",
   result: { error: null },
 });
