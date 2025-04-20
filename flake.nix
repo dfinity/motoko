4c8c9656bfe4eb1d9a86fb5887c320c9881f0768
Bas van Dijk <bas@van.dijk.ch>
2025-04-20 14:09:28 +0200

    .

--- flake.nix
+++ flake.nix
@@ -2,9 +2,14 @@
   description = "The Motoko compiler";
 
   inputs = {
-    # Check the following to see how up-to-date Hydra is:
-    # https://hydra.nixos.org/job/nixpkgs/nixpkgs-24.11-darwin/llvm.aarch64-darwin
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    # The following is a recent commit on the release-24.11 branch. The reason we specify a commit
+    # here instead of "release-24.11" is that nixpkgs' CI system (Hydra) runs a bit behind on darwin
+    # which means that not all packages we depend on have been cached if we would use the HEAD of
+    # the branch. So we look at some important jobs on Hydra like the following to figure out the
+    # best commit to use:
+    # https://hydra.nixos.org/job/nixpkgs/nixpkgs-24.11-darwin/llvm.aarch64-darwin/latest
+    # Then click on the Part of: "evaluation ..."  -> Inputs -> nixpkgs Revision.
+    nixpkgs.url = "github:NixOS/nixpkgs/5051ae6744b993fcfab221e8bd38f8bc26f88393";
     flake-utils.url = "github:numtide/flake-utils";
 
     nix-update-flake.url = "github:Mic92/nix-update";
@@ -81,7 +86,7 @@
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
-          (self: super: { inherit ic-src; })
+          (self: super: { inherit ic-src libtommath-src; })
 
           # Selecting the ocaml version
           # Also update ocaml-version in src/*/.ocamlformat!
@@ -297,132 +302,7 @@
           doCheck = false;
         };
 
-      rts =
-        let
-          # Build Rust package cargo-vendor-tools
-          cargoVendorTools = pkgs.rustPlatform.buildRustPackage rec {
-            name = "cargo-vendor-tools";
-            src = ./rts/${name};
-            cargoLock = {
-              lockFile = ./rts/${name}/Cargo.lock;
-            };
-          };
-
-          # Path to vendor-rust-std-deps, provided by cargo-vendor-tools
-          vendorRustStdDeps = "${cargoVendorTools}/bin/vendor-rust-std-deps";
-
-          # SHA256 of Rust std deps
-          rustStdDepsHash = "sha256-U4BTr1CzFuOMdyLuhw5ry3/u8bkRiPmnMr4pLo3IdOQ=";
-
-          # Vendor directory for Rust std deps
-          rustStdDeps = pkgs.stdenvNoCC.mkDerivation {
-            name = "rustc-std-deps";
-
-            nativeBuildInputs = [ pkgs.curl ];
-
-            buildCommand = ''
-              mkdir $out
-              cd $out
-              ${vendorRustStdDeps} ${pkgs.rust-nightly} .
-            '';
-
-            outputHash = rustStdDepsHash;
-            outputHashAlgo = "sha256";
-            outputHashMode = "recursive";
-          };
-
-          # Vendor tarball of the RTS
-          rtsDeps = pkgs.rustPlatform.importCargoLock {
-            lockFile = ./rts/motoko-rts-tests/Cargo.lock;
-          };
-
-          # All dependencies needed to build the RTS, including Rust std deps, to
-          # allow `cargo -Zbuild-std`. (rust-lang/wg-cargo-std-aware#23)
-          allDeps = pkgs.symlinkJoin {
-            name = "merged-rust-deps";
-            paths = [
-              rtsDeps
-              rustStdDeps
-            ];
-          };
-        in
-
-        pkgs.stdenv.mkDerivation {
-          name = "moc-rts";
-
-          src = ./rts;
-
-          nativeBuildInputs = [ pkgs.makeWrapper pkgs.removeReferencesTo pkgs.cacert ];
-
-          buildInputs = with pkgs; [
-            llvmPackages_18.clang
-            llvmPackages_18.bintools
-            rust-nightly
-            wasmtime
-            rust-bindgen
-            python3
-          ] ++ pkgs.lib.optional pkgs.stdenv.isDarwin [
-            libiconv
-          ];
-
-          preBuild = ''
-            export CARGO_HOME=$PWD/cargo-home
-
-            # This replicates logic from nixpkgs’ pkgs/build-support/rust/default.nix
-            mkdir -p $CARGO_HOME
-            echo "Using vendored sources from ${rtsDeps}"
-            unpackFile ${allDeps}
-            cat > $CARGO_HOME/config <<__END__
-              [source."crates-io"]
-              "replace-with" = "vendored-sources"
-
-              [source."vendored-sources"]
-              "directory" = "$(stripHash ${allDeps})"
-            __END__
-
-
-            ${llvmEnv}
-            export TOMMATHSRC=${libtommath-src}
-          '';
-
-          doCheck = true;
-
-          checkPhase = ''
-            make test
-          '';
-
-          installPhase = ''
-            mkdir -p $out/rts
-            cp mo-rts-non-incremental.wasm $out/rts
-            cp mo-rts-non-incremental-debug.wasm $out/rts
-            cp mo-rts-incremental.wasm $out/rts
-            cp mo-rts-incremental-debug.wasm $out/rts
-            cp mo-rts-eop.wasm $out/rts
-            cp mo-rts-eop-debug.wasm $out/rts
-          '';
-
-          # This needs to be self-contained. Remove mention of nix path in debug
-          # message.
-          preFixup = ''
-            remove-references-to \
-              -t ${pkgs.rust-nightly} \
-              -t ${rtsDeps} \
-              -t ${rustStdDeps} \
-              $out/rts/mo-rts-non-incremental.wasm $out/rts/mo-rts-non-incremental-debug.wasm
-            remove-references-to \
-              -t ${pkgs.rust-nightly} \
-              -t ${rtsDeps} \
-              -t ${rustStdDeps} \
-              $out/rts/mo-rts-incremental.wasm $out/rts/mo-rts-incremental-debug.wasm
-            remove-references-to \
-              -t ${pkgs.rust-nightly} \
-              -t ${rtsDeps} \
-              -t ${rustStdDeps} \
-              $out/rts/mo-rts-eop.wasm $out/rts/mo-rts-eop-debug.wasm
-          '';
-
-          allowedRequisites = [ ];
-        };
+      rts = import ./nix/rts.nix pkgs llvmEnv;
 
       samples = pkgs.stdenv.mkDerivation {
         name = "samples";
@@ -437,45 +317,7 @@
         '';
       };
 
-      js =
-        let
-          mk = n:
-            pkgs.stdenv.mkDerivation {
-              name = "${n}.js";
-              src = ./src;
-              buildInputs = commonBuildInputs pkgs ++ [
-                pkgs.ocamlPackages.js_of_ocaml
-                pkgs.ocamlPackages.js_of_ocaml-ppx
-                pkgs.nodejs_20
-                pkgs.nodePackages.terser
-              ];
-              buildPhase = ''
-                patchShebangs .
-              '' + pkgs.lib.optionalString (rts != null) ''
-                ./rts/gen.sh ${rts}/rts/
-              '' + ''
-                make DUNE_OPTS="--profile=release" ${n}.js
-                terser ${n}.js -o ${n}.min.js -c -m
-              '';
-              installPhase = ''
-                mkdir -p $out
-                mkdir -p $out/bin
-                cp --verbose --dereference ${n}.js $out/bin
-                cp --verbose --dereference ${n}.min.js $out/bin
-              '';
-              doInstallCheck = true;
-              test = ./test + "/test-${n}.js";
-              installCheckPhase = ''
-                NODE_PATH=$out/bin node --experimental-wasm-memory64 $test
-              '';
-            };
-        in
-        {
-          moc = mk "moc";
-          moc_interpreter = mk "moc_interpreter";
-          didc = mk "didc";
-          recurseForDerivations = true;
-        };
+      js = import ./nix/moc.js.nix pkgs commonBuildInputs rts;
 
       filecheck = pkgs.runCommandNoCC "FileCheck" { } ''
         mkdir -p $out/bin
