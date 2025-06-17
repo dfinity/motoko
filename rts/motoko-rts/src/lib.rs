1dc1425158096678b3c5b70af289b92e1890871a
Bas van Dijk <bas@dfinity.org>
2025-06-17 22:24:51 +0000

    .

--- rts/motoko-rts/src/lib.rs
+++ rts/motoko-rts/src/lib.rs
@@ -7,7 +7,8 @@
     proc_macro_hygiene,
     // // We do not need simd but this flag enables `core::arch:wasm64`.
     // // See https://github.com/rust-lang/rust/issues/90599
-    simd_wasm64
+    simd_wasm64,
+    unsafe_extern_blocks
 )]
 #![allow(internal_features)]
 
