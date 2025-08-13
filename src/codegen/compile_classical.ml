31898c58df92dde577ad048e856740bb17a7ba05
Alex Uta <alexandru.uta@gmail.com>
2025-08-13 11:27:39 +0200

    Update src/codegen/compile_classical.ml
    format ocaml code
    
    Co-authored-by: Claudio Russo <claudio@dfinity.org>

--- src/codegen/compile_classical.ml
+++ src/codegen/compile_classical.ml
@@ -11787,19 +11787,22 @@ and compile_prim_invocation (env : E.t) ae p es at =
   (* The compiler will exit with an error if it encounters a related call *)
   | OtherPrim "alloc_weak_ref", [target] ->
     let msg = Diag.error_message Source.no_region "alloc_weak_ref" "classical" 
-    "Weak references are not supported in classical mode." in
+      "Weak references are not supported in classical mode."
+    in
     Diag.print_messages [msg];
     exit 0
 
   | OtherPrim "weak_get", [weak_ref] ->
     let msg = Diag.error_message Source.no_region "weak_get" "classical" 
-    "Weak references are not supported in classical mode." in
+      "Weak references are not supported in classical mode." 
+    in
     Diag.print_messages [msg];
     exit 0
 
   | OtherPrim "weak_ref_is_live", [weak_ref] ->
     let msg = Diag.error_message Source.no_region "weak_ref_is_live" "classical" 
-    "Weak references are not supported in classical mode." in
+      "Weak references are not supported in classical mode." 
+    in
     Diag.print_messages [msg];
     exit 0
 
