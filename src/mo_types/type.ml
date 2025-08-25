d47441e73eb01235374108aaae660b1ba961153a
Kamil Listopad <kamil.listopad@dfinity.org>
2025-08-25 12:45:34 +0200

    feat: Infer invariant when bound is isolated (no proper super/subtypes except Top/Bottom) (#5359)

--- src/mo_types/type.ml
+++ src/mo_types/type.ml
@@ -1479,6 +1479,51 @@ and singleton_field co tf = singleton_typ co tf.typ
 
 and singleton t : bool = singleton_typ (ref S.empty) t
 
+(** A type is isolated if it has no proper supertypes nor proper subtypes (ignoring top [Any] and bottom [Non]). *)
+let rec isolated_typ co = function
+  | Con (c, ts) as t ->
+    (match Cons.kind c with
+    | Abs (_, bound) ->
+      (* Type parameters are isolated when unbounded. *)
+      bound = Any
+    | Def (tbs, def) ->
+      (* Cyclic definitions are isolated until proven otherwise. *)
+      S.mem t !co || begin
+        co := S.add t !co;
+        isolated_typ co (reduce tbs def ts)
+      end
+    )
+  | Mut _ -> true
+  | Named (_, t) -> isolated_typ co t
+  | Prim
+    ( Bool
+    | Nat8
+    | Nat16
+    | Nat32
+    | Nat64
+    | Int8
+    | Int16
+    | Int32
+    | Int64
+    | Float
+    | Char
+    | Text
+    | Blob
+    | Error
+    | Principal
+    | Region
+    (* All except Nat, Int, Null as they have proper super/subtypes: Nat <: Int, ?T <: Null *)
+    ) -> true
+  | Array t
+  | Async (_, _, t)
+  | Weak t -> isolated_typ co t
+  | Tup ts -> List.for_all (isolated_typ co) ts
+  | Func (_, _, _, ts1, ts2) ->
+    List.for_all (isolated_typ co) ts1 &&
+    List.for_all (isolated_typ co) ts2
+  | _ -> false
+
+and isolated t = isolated_typ (ref S.empty) t
 
 (* Least upper bound and greatest lower bound *)
 
