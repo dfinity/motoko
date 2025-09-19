0450fe85ebb3b9f60dbbf29f0fc0524521954dfb
Bartosz Przydatek <bartosz.przydatek@dfinity.org>
2025-09-19 17:32:26 +0200

    fix: disable wasmtime cache

--- test/wasm-components-test-project/3_run_motoko_component.sh
+++ test/wasm-components-test-project/3_run_motoko_component.sh
@@ -3,5 +3,5 @@
 ROOT_DIR=$(dirname "$(realpath $0)")
 cd $ROOT_DIR || exit
 echo --- Running target/motoko-composed.wasm on wasmtime ...
-wasmtime run target/motoko-composed.wasm
+wasmtime run -C cache=n target/motoko-composed.wasm
 echo --- Run done.
