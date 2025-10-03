902afd6398189a6f97ed96274cd825af2faa5e9e
Kamil Listopad <listopadkamil@gmail.com>
2025-10-03 13:56:34 +0200

    fix

--- src/mo_frontend/typing.ml
+++ src/mo_frontend/typing.ml
@@ -1458,7 +1458,7 @@ let resolve_hole env at hole_sort typ =
          List.partition (fun (candidate : hole_candidate) -> is_matching_lab candidate.id)
      in
      Error (List.map suggestion_of_candidate lib_terms,
-            List.map suggestion_of_candidate explicit_terms)
+            List.map (fun candidate -> candidate.desc) explicit_terms)
   | terms -> begin
      match disambiguate_resolutions terms with
      | Some term -> Ok term
