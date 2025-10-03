ef1c7813675295ee4c3e4eb43a24afdb23fd5072
Claudio Russo <claudio@dfinity.org>
2025-10-03 12:05:22 +0100

    suggest renamings (in same source file only); add quotes

--- src/mo_frontend/typing.ml
+++ src/mo_frontend/typing.ml
@@ -132,6 +132,8 @@ let recover_with (x : 'a) (f : 'b -> 'a) (y : 'b) = try f y with Recover -> x
 let recover_opt f y = recover_with None (fun y -> Some (f y)) y
 let recover f y = recover_with () f y
 
+let quote s = "`"^s^"`"
+
 let display_lab = Lib.Format.display T.pp_lab
 
 let display_typ = Lib.Format.display T.pp_typ
@@ -1310,6 +1312,7 @@ type hole_candidate =
     desc: string;
     typ : T.typ;
     id : T.lab;
+    region : Source.region;
   }
 
 (* All candidates are subtypes of the required type. The "greatest" of
@@ -1362,14 +1365,14 @@ let resolve_hole env at hole_sort typ =
   let has_matching_field_typ = function
     | T.{ lab; typ = Typ c; _ } -> None
     | T.{ lab; typ = Mut t; _ } -> None
-    | T.{ lab = lab1; typ = typ1; _ } ->
+    | T.{ lab = lab1; typ = typ1; src } ->
        if is_matching_typ typ1
-       then Some (lab1, typ1)
+       then Some (lab1, typ1, src.T.region)
        else None
   in
   let find_candidate_fields (module_name, fs) =
     List.filter_map has_matching_field_typ fs |>
-      List.map (fun (lab, typ)->
+      List.map (fun (lab, typ, region)->
           let path =
             { it = DotE(
                 { it = VarE {it = module_name; at = no_region; note = Const};
@@ -1381,10 +1384,10 @@ let resolve_hole env at hole_sort typ =
               at = Source.no_region;
               note = empty_typ_note; }
           in
-          ({ path; desc = module_name^"."^ lab; typ; id=lab } : hole_candidate))
+          ({ path; desc = quote (module_name^"."^ lab); typ; id=lab; region } : hole_candidate))
   in
   let find_candidate_id = function
-    (id, (t, _, _, _)) ->
+    (id, (t, region, _, _)) ->
     if is_matching_typ t
     then
       let path =
@@ -1392,7 +1395,7 @@ let resolve_hole env at hole_sort typ =
           at = Source.no_region;
           note = empty_typ_note }
       in
-      Some { path; desc = id; typ = t; id }
+      Some { path; desc = quote id; typ = t; id; region }
     else None
   in
   let (eligible_ids, explicit_ids) =
@@ -1417,6 +1420,21 @@ let resolve_hole env at hole_sort typ =
        (eligible_ids @ eligible_fields,
         explicit_ids @ explicit_fields)
   in
+  let renaming_hints env =
+    List.iter (fun candidate ->
+        match hole_sort with
+        | Anon _ -> ()
+        | Named id ->
+        if (candidate.region.left.file = at.left.file) then
+          info env candidate.region
+            "Consider renaming `%s` to `%s` so it can serve as an implicit argument `%s` in this call\n%s%s"
+            candidate.id
+            id
+            id
+            (Source.string_of_region at)
+            (match Source.read_region at with Some s -> ": " ^ s | None -> "."))
+      explicit_terms
+  in
   match eligible_terms with
   | [term] -> Ok term
   | [] ->
@@ -1432,14 +1450,15 @@ let resolve_hole env at hole_sort typ =
          List.partition (fun (candidate : hole_candidate) -> is_matching_lab candidate.id)
      in
      Error (List.map (fun candidate -> candidate.desc) lib_terms,
-            List.map (fun candidate -> candidate.desc) explicit_terms)
+            List.map (fun candidate -> candidate.desc) explicit_terms,
+            renaming_hints)
   | terms -> begin
      match disambiguate_resolutions terms with
      | Some term -> Ok term
      | None ->
        let terms = List.map (fun term -> term.desc) terms in
        error env at "M0231" "ambiguous implicit argument %s of type%a.\nThe ambiguous implicit candidates are: %s%s."
-         (match hole_sort with Named n -> "named " ^ n | Anon i -> "at argument position " ^ Int.to_string i)
+         (match hole_sort with Named n -> "named " ^ quote n | Anon i -> "at argument position " ^ Int.to_string i)
          display_typ typ
          (String.concat ", " terms)
          (if explicit_terms = [] then ""
@@ -2184,18 +2203,20 @@ and check_exp' env0 t exp : T.typ =
       e := path;
       check_exp env t path;
       t
-    | Error (import_suggestions, explicit_suggestions) ->
+    | Error (import_suggestions, explicit_suggestions, renaming_hints) ->
       let import_sug =
-         if import_suggestions = [] then
+        if import_suggestions = [] then
+           let desc = match s with Named id -> " named " ^ quote id | _ -> "" in
            Format.sprintf
-             "\nHint: If you're trying to use an implicit argument you need to have a matching declaration%s in scope."
-             (match s with Named n -> " named " ^ n | _ -> "")
+             "\nHint: If you're trying to omit an implicit argument%s you need to have a matching declaration%s in scope."
+             desc desc
          else Format.sprintf "\nHint: Did you mean to import %s?" (String.concat " or " import_suggestions)
       in
       let explicit_sug =
          if explicit_suggestions = [] then ""
          else Format.sprintf "\nHint: Did you mean to explicitly use %s?" (String.concat " or " explicit_suggestions)
       in
+      renaming_hints env;
       error env exp.at "M0230" "Cannot determine implicit argument %s of type%a%s%s"
         (desc s)
         display_typ t
@@ -2557,7 +2578,7 @@ and infer_call env exp1 inst (parenthesized, ref_exp2) at t_expect_opt =
     let ts = match t_arg with
       | T.Tup ts -> ts
       | t -> [t] in
-    let e' = match insert_holes exp2.at ts es with
+    let e' = match insert_holes at ts es with
       | [e] -> e.it
       | es -> TupE es in
     { exp2 with it = e'}
