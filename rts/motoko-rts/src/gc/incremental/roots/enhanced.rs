f5b11286fce8d369e0e47e4d50babab07b9a086d
Alex Uta <alexandru.uta@gmail.com>
2025-10-08 18:12:49 +0200

    feat:  Deduplicate tagged blobs (#5512)
    This PR adds (tagged) blob deduplication support. The main issue it
    solves is that all external calls to a motoko canister go through candid
    deserialization and blobs passed as arguments end up as fresh blobs on
    the motoko heap. Calling multiple times with the same blob as argument
    creates multiple copies of the same blob.
    
    To achieve deduplication, this PR does the following:
    * in `internals.mo`, it creates a fixed-size hash-table which solves
    collisions via chaining.
    * sets up a thin RTS interface to set/get the hash-table allocated in
    `internals.mo` to be tracked by the RTS layer such that the table is not
    garbage collected and it survives upgrades.
    * to achieve deduplication, the hash table stores weak references
    pointing to the actual objects; once objects are garbage collected, the
    weak references will point to null.
    * a thin client interface (in `prim.mo`) to walk the hash table and
    check which deduplicated blobs are alive/dead and prune the dead ones if
    neeed.

--- rts/motoko-rts/src/gc/incremental/roots/enhanced.rs
+++ rts/motoko-rts/src/gc/incremental/roots/enhanced.rs
@@ -29,6 +29,16 @@ pub unsafe fn visit_roots<C, V: Fn(&mut C, *mut Value)>(
             visit_field(context, location);
         }
     }
+    #[cfg(feature = "ic")]
+    {
+        // Always visit the dedup table as well.
+        // Otherwise the dedup table will be garbage collected.
+        use crate::persistence::get_dedup_table_ptr;
+        let dedup_table = get_dedup_table_ptr();
+        if dedup_table.is_non_null_ptr() {
+            visit_field(context, dedup_table);
+        }
+    }
 }
 
 #[cfg(feature = "ic")]
@@ -67,6 +77,7 @@ pub unsafe fn initialize_static_variables<M: crate::memory::Memory>(mem: &mut M,
     for index in 0..amount {
         array.initialize(index, NULL_POINTER, mem);
     }
+
     let location = addr_of_mut!(STATIC_VARIABLES) as *mut Value;
     write_with_barrier(mem, location, variables);
 }
