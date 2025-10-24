52625d0350bb1532d197072bd6c37ecd681283ad
Kamil Listopad <listopadkamil@gmail.com>
2025-10-24 13:39:43 +0200

    revert parsing to minimal changes

--- src/mo_frontend/parsing.ml
+++ src/mo_frontend/parsing.ml
@@ -12,8 +12,6 @@ module E =
     (Parser.MenhirInterpreter)
     (Error_reporting)
 
-module S = Source.Region_set
-
 (* Define a printer for explanations. We treat an explanation as if it
    were just an item: that is, we ignore the position information that
    is provided in the explanation. Indeed, this information is hard to
@@ -88,54 +86,14 @@ let slice_lexeme lexbuf i1 i2 =
 module I = Parser.MenhirInterpreter
 
 (* For debug *)
-module Debug = struct
-  module Printer = MenhirRecoveryLib.MakePrinter (
-    struct
-      module I = Parser.MenhirInterpreter
-      let print s = Printf.eprintf "%s" s
-      let print_symbol s = print (Printers.string_of_symbol s)
-      let print_element = None
-      let print_token t = print (Source_token.string_of_parser_token t)
-    end)
-
-  let print_checkpoint = function
-    | I.InputNeeded env ->
-      Printf.eprintf "InputNeeded";
-      Printer.print_env env
-    | I.Shifting (env1, env2, _) ->
-      Printf.eprintf "Shifting";
-      Printer.print_env env1;
-      Printf.eprintf " -> ";
-      Printer.print_env env2
-    | I.HandlingError env ->
-      Printf.eprintf "HandlingError";
-      Printer.print_env env
-    | I.AboutToReduce (env, _) ->
-      Printf.eprintf "AboutToReduce";
-      Printer.print_env env
-    | I.Accepted _ ->
-      Printf.eprintf "Accepted"
-    | I.Rejected ->
-      Printf.eprintf "Rejected"
-
-  let string_of_production (prod : I.production) =
-    Printf.sprintf "%s -> %s"
-      (Printers.string_of_symbol (I.lhs prod))
-      (String.concat " . " (List.map Printers.string_of_symbol (I.rhs prod)))
-
-  let inspect_state_items st =
-    let symbol = I.incoming_symbol st in
-    Printer.print_symbol (I.X symbol);
-    Printf.eprintf "\n";
-    I.items st |> List.iter (fun (prod, _dot) ->
-      Printf.eprintf "  %s\n" (string_of_production prod))
-
-  let rec inspect_env (env : 'a I.env) =
-    match I.top env, I.pop env with
-    | Some (I.Element (st, _, _, _)), Some env' ->
-      inspect_state_items st; inspect_env env'
-    | _ -> Printf.eprintf "inspect_env done\n"
-end
+(* module RecoveryTracer = MenhirRecoveryLib.MakePrinter ( *)
+(*   struct *)
+(*     module I = Parser.MenhirInterpreter *)
+(*     let print s = Printf.eprintf "%s" s *)
+(*     let print_symbol s = print (Printers.string_of_symbol s) *)
+(*     let print_element = None *)
+(*     let print_token t = print (Source_token.string_of_parser_token t) *)
+(*   end) *)
 
 module RecoveryTracer = MenhirRecoveryLib.DummyPrinter (I)
 
@@ -161,16 +119,15 @@ end
 
 module R = MenhirRecoveryLib.Make (Parser.MenhirInterpreter) (RecoveryConfig) (RecoveryTracer)
 
-let region_of_pos (start, end_) =
-  Source.{left = Lexer.convert_pos start; right = Lexer.convert_pos end_}
-
-let handle_generic_error error_detail msg_store ((startp, _) as pos) lexeme inputneeded_cp =
-  let at = region_of_pos pos in
+let handle_error lexbuf error_detail message_store (start, end_) explanations =
+  let at =
+        Source.{left = Lexer.convert_pos start; right = Lexer.convert_pos end_}
+  in
+  let lexeme = slice_lexeme lexbuf start end_ in
   let token =
     if lexeme = "" then "end of input" else
       "token '" ^ String.escaped lexeme ^ "'"
   in
-  let explanations = E.investigate startp inputneeded_cp in
   let msg =
     match error_detail with
     | 1 ->
@@ -192,45 +149,7 @@ let handle_generic_error error_detail msg_store ((startp, _) as pos) lexeme inpu
     | _ ->
       Printf.sprintf "unexpected %s" token
   in
-  Diag.add_msg msg_store (Diag.error_message at "M0001" "syntax" msg)
-
-let is_lcurly (type a) (symbol : a I.symbol) = match symbol with
-  | I.T (I.T_LCURLY) -> true
-  | _ -> false
-
-(** Find closest '{' on the stack and check if it comes from the 'block' production. *)
-let rec inside_block (env : 'a I.env) =
-  match I.top env, I.pop env with
-  | Some (I.Element (st, _, start_pos, end_pos)), Some env' ->
-    if is_lcurly (I.incoming_symbol st) then
-      match I.items st with
-      | (prod, _dot) :: _ when I.lhs prod = I.X (I.N I.N_block) -> Some (start_pos, end_pos)
-      | _ -> None
-    else
-      inside_block env'
-  | _ -> None
-
-(** Try adding a custom error instead of the generic one. *)
-let try_add_custom_error env lexeme reported msg_store =
-  match lexeme with
-  | "=" | "with" ->
-    (match inside_block env with
-    | Some block_pos ->
-      let at = region_of_pos block_pos in
-      if not (S.mem at !reported) then begin
-        reported := S.add at !reported;
-        Diag.add_msg msg_store (Diag.error_message at "M0001" "syntax"
-          (Printf.sprintf "expected block but got record; wrap the record in braces: { { … } }"))
-      end;
-      true
-    | None -> false)
-  | _ -> false
-
-let handle_error env lexbuf reported msg_store inputneeded_cp error_detail = 
-  let (startp, endp) as positions = I.positions env in
-  let lexeme = slice_lexeme lexbuf startp endp in
-  (* if not (try_add_custom_error env lexeme reported msg_store) then *)
-    handle_generic_error error_detail msg_store positions lexeme inputneeded_cp
+  Diag.add_msg message_store (Diag.error_message at "M0001" "syntax" msg)
 
 (* We drive the parser in the usual way, but records the last [InputNeeded]
    checkpoint. If a syntax error is detected, we go back to this checkpoint
@@ -240,19 +159,19 @@ let parse ?(recovery = false) mode error_detail start lexer lexbuf =
   Diag.with_message_store ~allow_errors:recovery (fun m ->
     Parser_lib.msg_store := Some m;
     Parser_lib.mode := Some mode;
-    (* Avoid repeated custom errors for the same position.
-      A single custom error can replace multiple generic 'unexpected token' errors at different positions. *)
-    let reported = ref S.empty in
     let save_error (inputneeded_cp : 'a I.checkpoint) (fail_cp : 'a I.checkpoint) : unit =
     (* The parser signals a syntax error. Note the position of the
          problematic token, which is useful. Then, go back to the
          last [InputNeeded] checkpoint and investigate. *)
       match fail_cp with
       | I.HandlingError env ->
-        handle_error env lexbuf reported m inputneeded_cp error_detail
+        let (startp, _) as positions = I.positions env in
+        let explanations = E.investigate startp inputneeded_cp in
+        handle_error lexbuf error_detail m positions explanations
       | _ -> assert false
     in
     let fail cp = None in
-    let succ e = Some e in
+    let succ e =  Some e in
     R.loop_handle_recover succ fail save_error lexer start
   )
+
