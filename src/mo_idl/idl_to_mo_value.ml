b2afd3d9a26ebe68e2c33ed4979b3b4ad83065c0
Gabor Greif <gabor@dfinity.org>
2025-10-28 23:03:49 +0100

    simplify

--- src/mo_idl/idl_to_mo_value.ml
+++ src/mo_idl/idl_to_mo_value.ml
@@ -83,18 +83,18 @@ let rec args vs = function
   | ts when List.(compare_lengths vs.it ts < 0 && for_all null (Lib.List.drop (length vs.it) ts)) ->
     let vs' = vs.it @ Lib.List.replicate { vs with it = NullV } List.(length ts - length vs.it) in
     args {vs with it = vs'} ts
-  | T.[Obj (Object, _) as t] as ts when apart t (List.hd vs.it).it -> args {vs with it = [enrich t (List.hd vs.it)]} ts
+  | ts when List.(exists (fun (t, v) -> apart t v.it) (combine ts vs.it)) -> args {vs with it = List.map2 enrich ts vs.it} ts
   | ts -> parens_comma (List.map2 value vs.it ts)
 and null t = t = T.(Prim Null)
-and [@warning "-8"] apart T.(Obj (Object, tfs)) = function
-  | RecordV vfs ->
-    let defaultable = diff tfs (List.map (fun {it; _} -> Idl_to_mo.check_label (fst it)) vfs) in
-    defaultable <> [] && List.for_all (fun {T.typ; _} -> null typ) defaultable
+and apart t v = match t, v with
+  | T.(Obj (Object, tfs)), RecordV vfs ->
+       let defaultable = diff tfs (List.map (fun {it; _} -> Idl_to_mo.check_label (fst it)) vfs) in
+       defaultable <> [] && List.for_all (fun {T.typ; _} -> null typ) defaultable
   | _ -> false
-and diff tfs vls = List.filter (fun T.{lab; _} -> not (List.mem lab vls)) tfs
 and enrich t v = match t, v.it with
   | T.(Obj (Object, tfs)), RecordV vfs ->
     let defaultable = diff tfs (List.map (fun {it; _} -> Idl_to_mo.check_label (fst it)) vfs) in
     let defaulted = List.map (fun T.{lab; _} -> { v with it = { v with it = Id (Idllib.Escape.unescape_hash lab) }, { v with it = NullV } }) defaultable in
     { v with it = RecordV (vfs @ defaulted) }
   | _ -> v
+and diff tfs vls = List.filter (fun T.{lab; _} -> not (List.mem lab vls)) tfs
