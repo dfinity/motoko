52b9dea4a4dfec6acc1ba95ceb71296a1308e2a6
Gabor Greif <gabor@dfinity.org>
2025-10-28 22:30:42 +0100

    works: use `unescape_hash`

--- src/mo_idl/idl_to_mo_value.ml
+++ src/mo_idl/idl_to_mo_value.ml
@@ -95,6 +95,6 @@ and diff tfs vls = List.filter (fun T.{lab; _} -> not (List.mem lab vls)) tfs
 and enrich t v = match t, v.it with
   | T.(Obj (Object, tfs)), RecordV vfs ->
     let defaultable = diff tfs (List.map (fun {it; _} -> Idl_to_mo.check_label (fst it)) vfs) in
-    let defaulted = List.map (fun T.{lab; _} -> { v with it = { v with it = Unnamed (Lib.Uint32.of_int32 2l) }, { v with it = NullV } }) defaultable in
+    let defaulted = List.map (fun T.{lab; _} -> { v with it = { v with it = Id (Idllib.Escape.unescape_hash lab) }, { v with it = NullV } }) defaultable in
     { v with it = RecordV (vfs @ defaulted) }
   | _ -> v
