61039d5668ebf6643d2c704ef85e85789fe2e53d
Kamil Listopad <listopadkamil@gmail.com>
2025-11-03 15:33:29 +0100

    fix, rework message, gather more missing fields not just one

--- src/mo_frontend/typing.ml
+++ src/mo_frontend/typing.ml
@@ -2411,25 +2411,23 @@ and check_exp' env0 t exp : T.typ =
   | TupE exps, T.Tup ts when List.length exps = List.length ts ->
     List.iter2 (check_exp env) ts exps;
     t
-  | ObjE (exp_bases, exp_fields), T.Obj(T.Object, fts) -> (* TODO: infer bases? Default does a decent job. *)
+  | ObjE (exp_bases, exp_fields), T.Obj(T.Object, fts) ->
     let t' = infer_check_bases_fields env fts exp.at exp_bases exp_fields in
-    check_inferred env0 env t t' exp
-    (* check_ids env "object" "field"
-      (List.map (fun (ef : exp_field) -> ef.it.id) exp_fields);
-    List.iter (fun ef -> check_exp_field env ef fts) exp_fields; *)
-    (* if List.for_all (fun ft ->
-      if not (List.exists (fun (ef : exp_field) -> ft.T.lab = ef.it.id.it) exp_fields)
-      then begin
-      local_error env exp.at "M0151"
-        "object literal is missing field %s from expected type%a"
-        ft.T.lab
-        display_typ_expand t;
-        false
-      end else true
-    ) fts
-    then detect_lost_fields env t e;
-    t
-    *)
+    let fields = match T.promote t' with
+      | T.Obj(T.Object, fts) -> fts
+      | _ -> []
+    in
+    let missing_fields = List.filter (fun ft ->
+      not (List.exists (fun ft2 -> ft.T.lab = ft2.T.lab) fields)) fts
+      |> List.map (fun ft -> Printf.sprintf "'%s'" ft.T.lab)
+    in
+    begin match missing_fields with
+    | [] -> check_inferred env0 env t t' exp
+    | fts ->
+      let s = if List.length fts = 1 then "" else "s" in
+      local_error env exp.at "M0151" "missing field%s %s from expected type%a" s (String.concat ", " fts) display_typ_expand t;
+      t'
+    end
   | OptE exp1, _ when T.is_opt t ->
     check_exp env (T.as_opt t) exp1;
     t
