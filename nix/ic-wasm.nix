7c321b0e9ae6e73d916b10af344526e34b6c586f
Gabor Greif <gabor@dfinity.org>
2025-11-26 01:18:04 +0100

    WIP: avoiding the `-std=c++17` issue (see description)
    need to run the unwrapped compiler, with the correct sysyem includes

--- nix/ic-wasm.nix
+++ nix/ic-wasm.nix
@@ -6,4 +6,6 @@ pkgs: pkgs.rustPlatform-stable.buildRustPackage rec {
     lockFile = "${src}/Cargo.lock";
   };
   doCheck = false;
+  CXX_aarch64-apple-darwin = "${pkgs.llvmPackages_19.clang-unwrapped}/bin/clang++";
+  CXXFLAGS_aarch64-apple-darwin = "-isystem ${pkgs.llvmPackages_19.libcxx.dev}/include/c++/v1 -isystem ${pkgs.llvmPackages_19.clang}/resource-root/include";
 }
