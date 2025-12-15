00bb4120bf0b2eaa3c05b1945336e1788cd759e1
Gabor Greif <gabor@dfinity.org>
2025-12-15 12:26:11 +0100

    chore: bump `rustc-nightly` (#5730)
    No version jump but a fresher `rustc-1.89.0-nightly-2025-06-19`.

--- nix/rts.nix
+++ nix/rts.nix
@@ -13,7 +13,7 @@ let
   vendorRustStdDeps = "${cargoVendorTools}/bin/vendor-rust-std-deps";
 
   # SHA256 of Rust std deps
-  rustStdDepsHash = "sha256-lfgB6wxBaFkt0EGJnvYTs8m2fDra3Tpdnvqlnj8KI3Y=";
+  rustStdDepsHash = "sha256-kEj5g2v39atLfxuY8dmZ+9qdcJVNE8dwkaOghiLBhj0=";
 
   # Vendor directory for Rust std deps
   rustStdDeps = pkgs.stdenvNoCC.mkDerivation {
